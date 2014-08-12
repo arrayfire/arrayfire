866321bc83f6fa983b35b241390b9b9c6bacd8ed
Umar Arshad <umar@arrayfire.com>
2014-08-12 13:29:44 -0400

    forgot GetTestData.cmale

---
+++ common/GetTestData.cmake
@@ -0,0 +1,16 @@
+INCLUDE(ExternalProject)
+
+# Set default ExternalProject root directory
+set_directory_properties(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/data)
+
+ExternalProject_Add(arrayfire_data
+    GIT_REPOSITORY git@mule:area51/arrayfire_data
+    DOWNLOAD_DIR ${CMAKE_BINARY_DIRECTORY}
+    CONFIGURE_COMMAND ""
+    INSTALL_COMMAND ""
+    BUILD_COMMAND ""
+    LOG_DOWNLOAD 0
+    LOG_UPDATE 0
+    LOG_CONFIGURE 0
+    LOG_BUILD 0
+    )
