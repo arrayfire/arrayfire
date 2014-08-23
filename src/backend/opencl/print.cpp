25d2fbc859dd0d341b7eaa8d2d349f111f0d0ba8
Pradeep <pradeep@arrayfire.com>
2014-08-23 14:23:35 -0400

    af_print implementation for opencl backend

--- src/backend/opencl/print.cpp
+++ src/backend/opencl/print.cpp
@@ -3,14 +3,44 @@
 #include <cassert>
 #include <print.hpp>
 #include <Array.hpp>
+#include <copy.hpp>
 
 namespace opencl
 {
-    template<typename T>
-    void
-    printer(ostream &out, const cl::Buffer &ptr, const Array<T> &info, unsigned dim)
+    using std::ostream;
+
+    ostream&
+    operator<<(ostream &out, const cfloat& var)
     {
-        assert("NOT IMPLEMENTED" && 1 != 1);
+        out << var.s[0] << " " << var.s[1] << "i";
+        return out;
+    }
+
+    ostream&
+    operator<<(ostream &out, const cdouble& var)
+    {
+        out << var.s[0] << " " << var.s[1] << "i";
+        return out;
+    }
+
+    template<typename T>
+    void printer(ostream &out, const T* ptr, const Array<T> &info, unsigned dim)
+    {
+        dim_type stride =   info.strides()[dim];
+        dim_type d      =   info.dims()[dim];
+
+        if(dim == 0) {
+            for(dim_type i = 0, j = 0; i < d; i++, j+=stride) {
+                out << ptr[j] << "\t";
+            }
+            out << std::endl;
+        }
+        else {
+            for(int i = 0; i < d; i++) {
+                printer(out, ptr, info, dim - 1);
+                ptr += stride;
+            }
+        }
     }
 
     template<typename T>
@@ -21,7 +51,10 @@ namespace opencl
         out << "Dim:" << arr.dims();
         out << "Offset: " << arr.offsets();
         out << "Stride: " << arr.strides();
-        printer(out, arr.get(), arr, arr.ndims() - 1);
+        T * dataPtr = new T[arr.elements()];
+        copyData(dataPtr,arr);
+        printer(out, dataPtr, arr, arr.ndims() - 1);
+        delete[] dataPtr;
         return out;
     }
 
