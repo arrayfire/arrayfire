31b40e8b861c37739cc2790f68745cd1e770e546
Shehzan Mohammed <shehzan@arrayfire.com>
2014-08-26 18:56:21 -0400

    Added CUDA backend for resize

---
+++ src/backend/cuda/kernel/resize.hpp
@@ -0,0 +1,159 @@
+#include <stdio.h>
+
+#define divup(a, b) ((a+b-1)/b)
+
+namespace cuda
+{
+    namespace kernel
+    {
+        typedef struct
+        {
+            dim_type dim[4];
+        } dims_t;
+
+        // Kernel Launch Config Values
+        static const unsigned TX = 16;
+        static const unsigned TY = 16;
+
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // nearest-neighbor resampling
+        template<class T>
+        __host__ __device__
+        void resize_n_(      T* d_out, dim_type odim0, dim_type odim1,
+                       const T* d_in,  dim_type idim0, dim_type idim1,
+                       const dims_t ostrides, const dims_t istrides,
+                       const unsigned blockIdx_x, const float xf, const float yf)
+        {
+            int const ox = threadIdx.x + blockIdx_x * blockDim.x;
+            int const oy = threadIdx.y + blockIdx.y * blockDim.y;
+
+            int ix = round(ox * xf);
+            int iy = round(oy * yf);
+
+            if (ox >= odim0 || oy >= odim1) { return; }
+            if (ix >= idim0) { ix = idim0 - 1; }
+            if (iy >= idim1) { iy = idim1 - 1; }
+
+            d_out[ox + oy * ostrides.dim[1]] = d_in[ix + iy * istrides.dim[1]];
+        }
+
+        template<class T>
+        __global__
+        void resize_n(      T* d_out, dim_type odim0, dim_type odim1,
+                      const T* d_in,  dim_type idim0, dim_type idim1,
+                      const dims_t ostrides, const dims_t istrides,
+                      const unsigned b0, const dim_type batch,
+                      const float xf, const float yf)
+        {
+            unsigned id = blockIdx.x / b0;
+            // channel adjustment
+            int i_off = id * istrides.dim[2];
+            int o_off = id * ostrides.dim[2];
+            unsigned blockIdx_x =  blockIdx.x - id * b0;
+
+            // core
+            resize_n_(d_out+o_off, odim0, odim1,
+                      d_in +i_off, idim0, idim1,
+                      ostrides, istrides,
+                      blockIdx_x, xf, yf);
+        }
+        ////////////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // bilinear resampling
+        template<class T>
+        __host__ __device__
+        void resize_b_(      T* d_out, dim_type odim0, dim_type odim1,
+                       const T* d_in,  dim_type idim0, dim_type idim1,
+                       const dims_t ostrides, const dims_t istrides,
+                       const unsigned blockIdx_x, const float xf_, const float yf_)
+        {
+            int const ox = threadIdx.x + blockIdx_x * blockDim.x;
+            int const oy = threadIdx.y + blockIdx.y * blockDim.y;
+
+            float xf = ox * xf_;
+            float yf = oy * yf_;
+
+            int ix   = floorf(xf);
+            int iy   = floorf(yf);
+
+            if (ox >= odim0 || oy >= odim1) { return; }
+            if (ix >= idim0) { ix = idim0 - 1; }
+            if (iy >= idim1) { iy = idim1 - 1; }
+
+            float b = xf - ix;
+            float a = yf - iy;
+
+            const int ix2 = ix + 1 <  idim0 ? ix + 1 : ix;
+            const int iy2 = iy + 1 <  idim1 ? iy + 1 : iy;
+
+            const T p1 = d_in[ix  + istrides.dim[1] * iy ];
+            const T p2 = d_in[ix  + istrides.dim[1] * iy2];
+            const T p3 = d_in[ix2 + istrides.dim[1] * iy ] ;
+            const T p4 = d_in[ix2 + istrides.dim[1] * iy2];
+
+            T out = (1.0f-a) * (1.0f-b) * p1 +
+                    (a)      * (1.0f-b) * p2 +
+                    (1.0f-a) * (b)      * p3 +
+                    (a)      * (b)      * p4;
+
+            d_out[ox + oy * ostrides.dim[1]] = out;
+        }
+
+        template<class T>
+        __global__
+        void resize_b(      T* d_out, dim_type odim0, dim_type odim1,
+                      const T* d_in,  dim_type idim0, dim_type idim1,
+                      const dims_t ostrides, const dims_t istrides,
+                      const unsigned b0, const dim_type batch,
+                      const float xf, const float yf)
+        {
+            unsigned id = blockIdx.x / b0;
+            // channel adjustment
+            int i_off = id * istrides.dim[2];
+            int o_off = id * ostrides.dim[2];
+            unsigned blockIdx_x =  blockIdx.x - id * b0;
+
+            // core
+            resize_b_(d_out+o_off, odim0, odim1,
+                      d_in +i_off, idim0, idim1,
+                      ostrides, istrides,
+                      blockIdx_x, xf, yf);
+        }
+        ////////////////////////////////////////////////////////////////////////////////
+
+        ///////////////////////////////////////////////////////////////////////////
+        // Wrapper functions
+        ///////////////////////////////////////////////////////////////////////////
+        template <class T>
+        void resize(T *out, const dim_type odim0, const dim_type odim1,
+              const T *in, const dim_type idim0, const dim_type idim1,
+              const dim_type channels, const dim_type *ostrides, const dim_type *istrides,
+              const af_interp_type method)
+        {
+            dim3 threads(TX, TY, 1);
+            dim3 blocks(divup(odim0, threads.x), divup(odim1, threads.y));
+            unsigned blocksPerMatX = blocks.x;
+
+            if (channels > 1) { blocks.x *= channels; }
+            float xf = (float)idim0 / (float)odim0;
+            float yf = (float)idim1 / (float)odim1;
+
+            dims_t _ostrides = {{ostrides[0], ostrides[1], ostrides[2], ostrides[3]}};
+            dims_t _istrides = {{istrides[0], istrides[1], istrides[2], istrides[3]}};
+
+            if(method == AF_INTERP_NEAREST)
+                resize_n<<<blocks, threads>>>(out, odim0, odim1,
+                                              in,  idim0, idim1,
+                                              _ostrides, _istrides,
+                                              blocksPerMatX, channels, xf, yf);
+            else
+                resize_b<<<blocks, threads>>>(out, odim0, odim1,
+                                              in,  idim0, idim1,
+                                              _ostrides, _istrides,
+                                              blocksPerMatX, channels, xf, yf);
+        }
+
+    }
+}
--- src/backend/cuda/resize.cu
+++ src/backend/cuda/resize.cu
@@ -2,6 +2,7 @@
 #include <af/dim4.hpp>
 #include <Array.hpp>
 #include <resize.hpp>
+#include <kernel/resize.hpp>
 #include <stdexcept>
 
 namespace cuda
@@ -10,9 +11,19 @@ namespace cuda
     Array<T>* resize(const Array<T> &in, const dim_type odim0, const dim_type odim1,
                      const af_interp_type method)
     {
-        throw std::runtime_error("Resize not supported in CUDA yet");
-        Array<T> *outArray;
-        return outArray;
+        const af::dim4 iDims = in.dims();
+        af::dim4 oDims(odim0, odim1, iDims[2], iDims[3]);
+
+        if(iDims.elements() == 0 || oDims.elements() == 0) {
+            throw std::runtime_error("Elements is 0");
+        }
+
+        Array<T> *out = createEmptyArray<T>(oDims);
+
+        kernel::resize<T>(out->get(), oDims[0], oDims[1], in.get(), iDims[0], iDims[1], iDims[2],
+                          out->strides().get(), in.strides().get(), method);
+
+        return out;
     }
 
 
