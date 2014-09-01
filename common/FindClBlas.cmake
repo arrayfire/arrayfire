ec9b3842e0b9098de87c580335ce286ad08547ac
Umar Arshad <umar@arrayfire.com>
2014-09-01 14:00:31 -0400

    Cleanup CMake files(i.e. remove messages)

--- common/FindClBlas.cmake
+++ common/FindClBlas.cmake
@@ -5,30 +5,30 @@
 # * CLMATH_BLAS_LIBRARY_DIR     Location of the clBlas libraries
 # * CLMATH_BLAS_LIBRARY         List of libraries
 
-# Set default ExternalProject root directory
+# Look a directory above for the clBlas folder
+# TODO: Make this more robust
 FIND_PATH( CLMATH_BLAS_PACKAGE_DIR
     NAMES   lib64 include bin
-    PATHS   ${CMAKE_SOURCE_DIR}/../clBLAS/build/package
-            ${CMAKE_SOURCE_DIR}/../../clBLAS/build/package)
+    PATHS   ${CMAKE_SOURCE_DIR}/../clBlas/build/package
+            ${CMAKE_SOURCE_DIR}/../../clBlas/build/package)
 
 FIND_PATH( CLMATH_BLAS_INCLUDE_DIR
-    NAMES clBLAS.h
-    PATHS   ${CMAKE_SOURCE_DIR}/../clBLAS/build/package/include
-            ${CMAKE_SOURCE_DIR}/../../clBLAS/build/package/include
-            ${CLMATH_BLAS_PACKAGE_DIR}/include
+    NAMES   clBLAS.h
+    HINTS   ${CMAKE_SOURCE_DIR}/../clBlas/build/package/include
+            ${CMAKE_SOURCE_DIR}/../../clBlas/build/package/include
+    PATHS   ${CLMATH_BLAS_PACKAGE_DIR}/include
             ${CLMATH_BLAS_PACKAGE_DIR}/package/include)
 
-        MESSAGE(FINDING libclBLAS${CMAKE_SHARED_LIBRARY_SUFFIX})
 FIND_PATH( CLMATH_BLAS_LIBRARY_DIR
     NAMES   libclBLAS${CMAKE_SHARED_LIBRARY_SUFFIX}
-    PATHS   ${CMAKE_SOURCE_DIR}/../clBLAS/build/package/lib64
-            ${CMAKE_SOURCE_DIR}/../../clBLAS/build/package/lib64
-            ${CLMATH_BLAS_PACKAGE_DIR}/lib64
+    HINTS   ${CMAKE_SOURCE_DIR}/../clBlas/build/package/lib64
+            ${CMAKE_SOURCE_DIR}/../../clBlas/build/package/lib64
+    PATHS   ${CLMATH_BLAS_PACKAGE_DIR}/lib64
             ${CLMATH_BLAS_PACKAGE_DIR}/package/lib64)
 
 FIND_LIBRARY(CLMATH_BLAS_LIBRARY
     NAMES   clBLAS
-    PATHS   ${CMAKE_SOURCE_DIR}/../clBLAS/build/package/lib64
-            ${CMAKE_SOURCE_DIR}/../../clBLAS/build/package/lib64
-            ${CLMATH_BLAS_PACKAGE_DIR}/lib64
+    HINTS   ${CMAKE_SOURCE_DIR}/../clBlas/build/package/lib64
+            ${CMAKE_SOURCE_DIR}/../../clBlas/build/package/lib64
+    PATHS   ${CLMATH_BLAS_PACKAGE_DIR}/lib64
             ${CLMATH_BLAS_PACKAGE_DIR}/package/lib64)
