38df112900eb658c0fccc4ab5b97a6bed3c55560
Pradeep <pradeep@arrayfire.com>
2014-09-03 11:15:59 -0400

    bilateral opencl backend implementation

--- src/backend/opencl/bilateral.cpp
+++ src/backend/opencl/bilateral.cpp
@@ -1,8 +1,10 @@
+#define __CL_ENABLE_EXCEPTIONS
 #include <af/dim4.hpp>
 #include <af/defines.h>
 #include <ArrayInfo.hpp>
 #include <Array.hpp>
 #include <bilateral.hpp>
+#include <kernel/bilateral.hpp>
 #include <stdexcept>
 
 using af::dim4;
@@ -13,8 +15,26 @@ namespace opencl
 template<typename T, bool isColor>
 Array<T> * bilateral(const Array<T> &in, const float &s_sigma, const float &c_sigma)
 {
-    Array<T> *out = nullptr;
-    throw std::runtime_error("bilateral not supported in opencl");
+    const dim4 dims     = in.dims();
+    const dim4 istrides = in.strides();
+
+    Array<T>* out       = createEmptyArray<T>(dims);
+    const dim4 ostrides = out->strides();
+
+    kernel::KernelParams params;
+    params.offset = in.getOffset();
+    for(dim_type i=0; i<4; ++i) {
+        params.idims[i]    = dims[i];
+        params.istrides[i] = istrides[i];
+        params.ostrides[i] = ostrides[i];
+    }
+
+    try {
+        kernel::bilateral<T, isColor>(out->get(), in.get(), params, s_sigma, c_sigma);
+    } catch(cl::Error error) {
+        throw std::runtime_error(std::string("@opencl/bilateral: ").append(error.what()));
+    }
+
     return out;
 }
 
