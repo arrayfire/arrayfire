63670b025e324ceca61ed63f55acf40c99182521
Pradeep <pradeep@arrayfire.com>
2014-09-04 13:33:51 -0400

    histogram cuda backend

--- src/backend/cuda/histogram.cu
+++ src/backend/cuda/histogram.cu
@@ -3,7 +3,8 @@
 #include <ArrayInfo.hpp>
 #include <Array.hpp>
 #include <histogram.hpp>
-#include <cassert>
+#include <kernel/histogram.hpp>
+#include <stdexcept>
 
 using af::dim4;
 
@@ -13,19 +14,60 @@ namespace cuda
 template<typename inType, typename outType>
 Array<outType> * histogram(const Array<inType> &in, const unsigned &nbins, const double &minval, const double &maxval)
 {
-    Array<outType>* out  = 0;
-    assert("histogram not implemented yet in cuda backend" && out!=0);
+
+    if (nbins>kernel::MAX_BINS)
+        throw std::runtime_error("@histogram: maximum bins exceeded.");
+
+    const dim4 dims     = in.dims();
+    const dim4 istrides = in.strides();
+    dim4 outDims        = dim4(nbins, 1, dims[2], dims[3]);
+    Array<outType>* out = createValueArray<outType>(outDims, outType(0));
+    const dim4 ostrides = out->strides();
+
+    // create an array to hold min and max values for
+    // batch operation handling, this will reduce
+    // number of concurrent reads to one single memory location
+    cfloat* h_minmax = new cfloat[dims[2]];
+
+    for(dim_type k=0; k<dims[2]; ++k) {
+        h_minmax[k].x = minval;
+        h_minmax[k].y = maxval;
+    }
+
+    dim4 minmax_dims(dims[2]*2);
+    Array<cfloat>* minmax = createDataArray<cfloat>(minmax_dims, h_minmax);
+
+    // cleanup the host memory used
+    delete[] h_minmax;
+
+    kernel::hist_param_t<inType, outType> params;
+
+    params.d_dst    = out->get();
+    params.d_src    = in.get();
+    params.d_minmax = minmax->get();
+
+    for(dim_type i=0; i<4; ++i) {
+        params.idims[i]    = dims[i];
+        params.istrides[i] = istrides[i];
+        params.ostrides[i] = ostrides[i];
+    }
+
+    kernel::histogram(params, nbins);
+
+    // destroy the minmax array
+    destroyArray(*minmax);
+
     return out;
 }
 
 #define INSTANTIATE(in_t,out_t)\
 template Array<out_t> * histogram(const Array<in_t> &in, const unsigned &nbins, const double &minval, const double &maxval);
 
-INSTANTIATE(float,uint)
-INSTANTIATE(double,uint)
-INSTANTIATE(char,uint)
-INSTANTIATE(int,uint)
-INSTANTIATE(uint,uint)
-INSTANTIATE(uchar,uint)
+INSTANTIATE(float , uint)
+INSTANTIATE(double, uint)
+INSTANTIATE(char  , uint)
+INSTANTIATE(int   , uint)
+INSTANTIATE(uint  , uint)
+INSTANTIATE(uchar , uint)
 
 }
---
+++ src/backend/cuda/kernel/histogram.hpp
@@ -0,0 +1,95 @@
+#include <af/defines.h>
+#include <backend.hpp>
+#include "../helper.hpp"
+
+namespace cuda
+{
+
+namespace kernel
+{
+
+static const unsigned MAX_BINS  = 4000;
+static const dim_type THREADS_X =  256;
+static const dim_type THRD_LOAD =   16;
+
+template<typename inType, typename outType>
+struct hist_param_t {
+    outType *            d_dst;
+    const inType *       d_src;
+    const float2 *    d_minmax;
+    dim_type          idims[4];
+    dim_type       istrides[4];
+    dim_type       ostrides[4];
+};
+
+__forceinline__ __device__ dim_type minimum(dim_type a, dim_type b)
+{
+  return (a < b ? a : b);
+}
+
+template<typename inType, typename outType>
+static __global__
+void histogramKernel(const hist_param_t<inType, outType> params,
+                     dim_type len, dim_type nbins, dim_type blk_x)
+{
+    SharedMemory<outType> shared;
+    outType * shrdMem = shared.getPointer();
+
+    // offset minmax array to account for batch ops
+    const float2 * d_minmax = params.d_minmax + blockIdx.y;
+
+    // offset input and output to account for batch ops
+    const inType *in  = params.d_src + blockIdx.y * params.istrides[2];
+    outType * out     = params.d_dst + blockIdx.y * params.ostrides[2];
+
+    int start = blockIdx.x * THRD_LOAD * blockDim.x + threadIdx.x;
+    int end   = minimum((start + THRD_LOAD * blockDim.x), len);
+
+    __shared__ float min;
+    __shared__ float step;
+
+    if (threadIdx.x == 0) {
+        float2 minmax = *d_minmax;
+        step          = (minmax.y-minmax.x) / (float)nbins;
+    }
+
+    for (int i = threadIdx.x; i < nbins; i += blockDim.x)
+        shrdMem[i] = 0;
+    __syncthreads();
+
+    for (int row = start; row < end; row += blockDim.x) {
+        int bin = (int)((in[row] - min) / step);
+        bin     = (bin < 0)      ? 0         : bin;
+        bin     = (bin >= nbins) ? (nbins-1) : bin;
+        atomicAdd((shrdMem + bin), 1);
+    }
+    __syncthreads();
+
+    for (int i = threadIdx.x; i < nbins; i += blockDim.x) {
+        atomicAdd((out + i), shrdMem[i]);
+    }
+}
+
+template<typename inType, typename outType>
+void histogram(const hist_param_t<inType, outType> &params, dim_type nbins)
+{
+    dim3 threads(kernel::THREADS_X, 1);
+
+    dim_type numElements= params.idims[0]*params.idims[1];
+
+    dim_type blk_x = divup(numElements, THRD_LOAD*THREADS_X);
+
+    dim_type batchCount = params.idims[2];
+
+    dim3 blocks(blk_x, batchCount);
+
+    dim_type smem_size = nbins * sizeof(outType);
+
+    histogramKernel<inType, outType>
+                 <<<blocks, threads, smem_size>>>
+                 (params, numElements, nbins, blk_x);
+}
+
+}
+
+}
