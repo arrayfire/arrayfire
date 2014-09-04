fac89b9f3260ec5a23d81180f9b5132a8d9ccd73
Shehzan Mohammed <shehzan@arrayfire.com>
2014-09-04 12:32:47 -0400

    Added base_type to traits

--- include/af/traits.hpp
+++ include/af/traits.hpp
@@ -15,48 +15,56 @@ template<typename T> struct dtype_traits;
 template<>
 struct dtype_traits<float> {
     enum { af_type = f32 };
+    typedef float base_type;
     static const char* getName() { return "float"; }
 };
 
 template<>
 struct dtype_traits<af_cfloat> {
     enum { af_type = c32 };
+    typedef float base_type;
     static const char* getName() { return "std::complex<float>"; }
 };
 
 template<>
 struct dtype_traits<double> {
     enum { af_type = f64 };
+    typedef double base_type;
     static const char* getName() { return "double"; }
 };
 
 template<>
 struct dtype_traits<af_cdouble> {
     enum { af_type = c64 };
+    typedef double base_type;
     static const char* getName() { return "std::complex<double>"; }
 };
 
 template<>
 struct dtype_traits<char> {
     enum { af_type = b8 };
+    typedef char base_type;
     static const char* getName() { return "char"; }
 };
 
 template<>
 struct dtype_traits<int> {
     enum { af_type = s32 };
+    typedef int base_type;
     static const char* getName() { return "int"; }
 };
 
 template<>
 struct dtype_traits<unsigned> {
     enum { af_type = u32 };
+    typedef unsigned base_type;
     static const char* getName() { return "uint"; }
 };
 
 template<>
 struct dtype_traits<unsigned char> {
     enum { af_type = u8 };
+    typedef unsigned char base_type;
     static const char* getName() { return "uchar"; }
 };
 
