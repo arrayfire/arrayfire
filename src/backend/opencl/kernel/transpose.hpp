844303e5785d9d1b3a255181fead764c5932c53d
Pradeep <pradeep@arrayfire.com>
2014-09-11 17:08:13 -0400

    style changes in ocl transpose

--- src/backend/opencl/kernel/transpose.hpp
+++ src/backend/opencl/kernel/transpose.hpp
@@ -18,61 +18,63 @@ using std::string;
 
 namespace opencl
 {
+
 namespace kernel
 {
 
-    static const dim_type TILE_DIM  = 32;
-    static const dim_type THREADS_X = TILE_DIM;
-    static const dim_type THREADS_Y = (256 / TILE_DIM);
+static const dim_type TILE_DIM  = 32;
+static const dim_type THREADS_X = TILE_DIM;
+static const dim_type THREADS_Y = (256 / TILE_DIM);
 
-    template<typename T>
-    void transpose(Param out, const Param in)
-    {
-        try {
-            static std::once_flag compileFlags[DeviceManager::MAX_DEVICES];
-            static Program            trsProgs[DeviceManager::MAX_DEVICES];
-            static Kernel           trsKernels[DeviceManager::MAX_DEVICES];
+template<typename T>
+void transpose(Param out, const Param in)
+{
+    try {
+        static std::once_flag compileFlags[DeviceManager::MAX_DEVICES];
+        static Program            trsProgs[DeviceManager::MAX_DEVICES];
+        static Kernel           trsKernels[DeviceManager::MAX_DEVICES];
 
-            int device = getActiveDeviceId();
+        int device = getActiveDeviceId();
 
-            std::call_once( compileFlags[device], [device] () {
+        std::call_once( compileFlags[device], [device] () {
 
-                    std::ostringstream options;
-                    options << " -D T=" << dtype_traits<T>::getName()
-                            << " -D TILE_DIM=" << TILE_DIM;
+                std::ostringstream options;
+                options << " -D T=" << dtype_traits<T>::getName()
+                        << " -D TILE_DIM=" << TILE_DIM;
 
-                    buildProgram(trsProgs[device],
-                                 transpose_cl,
-                                 transpose_cl_len,
-                                 options.str());
+                buildProgram(trsProgs[device],
+                             transpose_cl,
+                             transpose_cl_len,
+                             options.str());
 
-                    trsKernels[device] = Kernel(trsProgs[device], "transpose");
-                });
-
-
-            NDRange local(THREADS_X, THREADS_Y);
-
-            dim_type blk_x = divup(in.info.dims[0], TILE_DIM);
-            dim_type blk_y = divup(in.info.dims[1], TILE_DIM);
-
-            // launch batch * blk_x blocks along x dimension
-            NDRange global(blk_x * TILE_DIM * in.info.dims[2], blk_y * TILE_DIM);
-
-            auto transposeOp = make_kernel<Buffer, KParam,
-                                           Buffer, KParam,
-                                           dim_type> (trsKernels[device]);
+                trsKernels[device] = Kernel(trsProgs[device], "transpose");
+            });
 
 
-            transposeOp(EnqueueArgs(getQueue(), global, local),
-                        out.data, out.info, in.data, in.info, blk_x);
+        NDRange local(THREADS_X, THREADS_Y);
 
-            CL_FINISH(getQueue());
+        dim_type blk_x = divup(in.info.dims[0], TILE_DIM);
+        dim_type blk_y = divup(in.info.dims[1], TILE_DIM);
 
-        } catch (cl::Error err) {
-            SHOW_CL_ERROR(err);
-            throw;
-        }
+        // launch batch * blk_x blocks along x dimension
+        NDRange global(blk_x * TILE_DIM * in.info.dims[2], blk_y * TILE_DIM);
+
+        auto transposeOp = make_kernel<Buffer, KParam,
+                                       Buffer, KParam,
+                                       dim_type> (trsKernels[device]);
+
+
+        transposeOp(EnqueueArgs(getQueue(), global, local),
+                    out.data, out.info, in.data, in.info, blk_x);
+
+        CL_FINISH(getQueue());
+
+    } catch (cl::Error err) {
+        SHOW_CL_ERROR(err);
+        throw;
     }
+}
 
 }
+
 }
--- src/backend/opencl/transpose.cpp
+++ src/backend/opencl/transpose.cpp
@@ -8,26 +8,26 @@ using af::dim4;
 namespace opencl
 {
 
-    template<typename T>
-    Array<T> * transpose(const Array<T> &in)
-    {
-        const dim4 inDims   = in.dims();
-        dim4 outDims  = dim4(inDims[1],inDims[0],inDims[2],inDims[3]);
-        Array<T>* out  = createEmptyArray<T>(outDims);
-        kernel::transpose<T>(*out, in);
-        return out;
-    }
+template<typename T>
+Array<T> * transpose(const Array<T> &in)
+{
+    const dim4 inDims   = in.dims();
+    dim4 outDims  = dim4(inDims[1],inDims[0],inDims[2],inDims[3]);
+    Array<T>* out  = createEmptyArray<T>(outDims);
+    kernel::transpose<T>(*out, in);
+    return out;
+}
 
 #define INSTANTIATE(T)\
     template Array<T> * transpose(const Array<T> &in);
 
-    INSTANTIATE(float)
-    INSTANTIATE(cfloat)
-    INSTANTIATE(double)
-    INSTANTIATE(cdouble)
-    INSTANTIATE(char)
-    INSTANTIATE(int)
-    INSTANTIATE(uint)
-    INSTANTIATE(uchar)
+INSTANTIATE(float)
+INSTANTIATE(cfloat)
+INSTANTIATE(double)
+INSTANTIATE(cdouble)
+INSTANTIATE(char)
+INSTANTIATE(int)
+INSTANTIATE(uint)
+INSTANTIATE(uchar)
 
 }
