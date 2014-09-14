42ee357c902e0f1f5450dc13402294f4ec921b5b
Pavan Yalamanchili <contact@pavanky.com>
2014-09-14 06:30:32 -0400

    Cleaning up a couple of lines

--- src/backend/opencl/err_opencl.hpp
+++ src/backend/opencl/err_opencl.hpp
@@ -5,13 +5,13 @@
         throw SupportError(__func__, __LINE__, "OPENCL");   \
     } while(0)
 
-#define CL_TO_AF_ERROR(ERR) do {                    \
-        char opencl_err_msg[1024];                  \
-        snprintf(opencl_err_msg,                    \
-                 sizeof(opencl_err_msg),            \
-                 "OpenCL Error%s when calling %s",  \
-                 getErrorMessage(ERR.err()),        \
-                 ERR.what());                       \
-        AF_ERROR(opencl_err_msg,                    \
-                 AF_ERR_INTERNAL);                  \
+#define CL_TO_AF_ERROR(ERR) do {                        \
+        char opencl_err_msg[1024];                      \
+        snprintf(opencl_err_msg,                        \
+                 sizeof(opencl_err_msg),                \
+                 "OpenCL Error: %s when calling %s",    \
+                 getErrorMessage(ERR.err()),            \
+                 ERR.what());                           \
+        AF_ERROR(opencl_err_msg,                        \
+                 AF_ERR_INTERNAL);                      \
     } while(0)
