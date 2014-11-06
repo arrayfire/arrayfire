1f724df073cd8ba9501aca0aae9ef699bde900ff
Pradeep <pradeep@arrayfire.com>
2014-11-06 01:54:38 -0500

    windows fixes for opencl backend

--- src/backend/opencl/kernel/names.hpp
+++ src/backend/opencl/kernel/names.hpp
@@ -11,9 +11,9 @@
 #include <ops.hpp>
 template<af_op_t T> static const char *binOpName() { return "ADD_OP"; }
 
-template<> const char *binOpName<af_add_t>() { return "ADD_OP"; }
-template<> const char *binOpName<af_and_t>() { return "AND_OP"; }
-template<> const char *binOpName<af_or_t >() { return "OR_OP" ; }
-template<> const char *binOpName<af_min_t>() { return "MIN_OP"; }
-template<> const char *binOpName<af_max_t>() { return "MAX_OP"; }
-template<> const char *binOpName<af_notzero_t>() { return "NOTZERO_OP"; }
+template<> STATIC_ const char *binOpName<af_add_t>() { return "ADD_OP"; }
+template<> STATIC_ const char *binOpName<af_and_t>() { return "AND_OP"; }
+template<> STATIC_ const char *binOpName<af_or_t >() { return "OR_OP" ; }
+template<> STATIC_ const char *binOpName<af_min_t>() { return "MIN_OP"; }
+template<> STATIC_ const char *binOpName<af_max_t>() { return "MAX_OP"; }
+template<> STATIC_ const char *binOpName<af_notzero_t>() { return "NOTZERO_OP"; }
--- src/backend/opencl/types.hpp
+++ src/backend/opencl/types.hpp
@@ -16,6 +16,7 @@
 
 namespace opencl
 {
+
     typedef cl_float2   cfloat;
     typedef cl_double2 cdouble;
     typedef cl_uchar     uchar;
@@ -25,13 +26,6 @@ namespace opencl
     template<> struct           is_complex<cfloat>  { static const bool value = true;   };
     template<> struct           is_complex<cdouble> { static const bool value = true;   };
 
-    template<typename T > static const char *shortname(bool caps) { return caps ? "X" : "x"; }
-    template<> const char *shortname<float   >(bool caps) { return caps ? "S" : "s"; }
-    template<> const char *shortname<double  >(bool caps) { return caps ? "D" : "d"; }
-    template<> const char *shortname<cfloat  >(bool caps) { return caps ? "C" : "c"; }
-    template<> const char *shortname<cdouble >(bool caps) { return caps ? "Z" : "z"; }
-    template<> const char *shortname<int     >(bool caps) { return caps ? "I" : "i"; }
-    template<> const char *shortname<uint    >(bool caps) { return caps ? "U" : "u"; }
-    template<> const char *shortname<char    >(bool caps) { return caps ? "J" : "j"; }
-    template<> const char *shortname<uchar   >(bool caps) { return caps ? "V" : "v"; }
+	template<typename T > const char *shortname(bool caps);
+
 }
