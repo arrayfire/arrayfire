e47a855c2dce6a3a8dbcba3b3333ad57d572090e
Shehzan Mohammed <shehzan@arrayfire.com>
2014-12-06 11:13:40 -0500

    Split sort_by_key instantiation into multiple files
    * Compilation being killed due to timeout on tegra. Required splitting

--- src/backend/cuda/sort_by_key_impl.hpp
+++ src/backend/cuda/sort_by_key_impl.hpp
@@ -29,27 +29,4 @@ namespace cuda
         default: AF_ERROR("Not Supported", AF_ERR_NOT_SUPPORTED);
         }
     }
-
-#define INSTANTIATE(Tk, Tv)                                             \
-    template void                                                       \
-    sort_by_key<Tk, Tv, true>(Array<Tk> &okey, Array<Tv> &oval,         \
-                              const Array<Tk> &ikey, const Array<Tv> &ival, const uint dim); \
-    template void                                                       \
-    sort_by_key<Tk, Tv,false>(Array<Tk> &okey, Array<Tv> &oval,         \
-                              const Array<Tk> &ikey, const Array<Tv> &ival, const uint dim); \
-
-#define INSTANTIATE1(Tk)       \
-    INSTANTIATE(Tk, float)     \
-    INSTANTIATE(Tk, double)    \
-    INSTANTIATE(Tk, int)       \
-    INSTANTIATE(Tk, uint)      \
-    INSTANTIATE(Tk, char)      \
-    INSTANTIATE(Tk, uchar)     \
-
-    INSTANTIATE1(float)
-    INSTANTIATE1(double)
-    INSTANTIATE1(int)
-    INSTANTIATE1(uint)
-    INSTANTIATE1(char)
-    INSTANTIATE1(uchar)
 }
