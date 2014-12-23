c2462cfd5c999a2bfc34d85ef997a29956a1442c
Pradeep <pradeep@arrayfire.com>
2014-12-23 18:10:32 -0500

    bugfix: fixes complex types for mean on cuda/opencl backend

--- src/api/c/mean.cpp
+++ src/api/c/mean.cpp
@@ -15,13 +15,34 @@
 #include <backend.hpp>
 #include <reduce.hpp>
 #include <arith.hpp>
-#include <print.hpp>
+#include <math.hpp>
+
+using namespace detail;
+
+template<typename T> static T division(T val, double elem) { return val / elem; }
+
+cfloat division(cfloat val, double elem)
+{
+    float resReal = real(val) / elem;
+    float resImag = imag(val) / elem;
+    cfloat res = (cfloat){resReal, resImag};
+    return res;
+}
+
+cdouble division(cdouble val, double elem)
+{
+    float resReal = real(val) / elem;
+    float resImag = imag(val) / elem;
+    cdouble res = (cdouble){resReal, resImag};
+    return res;
+}
 
 template<typename inType, typename outType>
 static outType mean(const af_array &in)
 {
     const Array<inType> &input = getArray<inType>(in);
-    return reduce_all<af_add_t, inType, outType>(input)/(outType)input.elements();
+    outType retVal = reduce_all<af_add_t, inType, outType>(input);
+    return division(retVal, input.elements());
 }
 
 template<typename inType, typename outType>
@@ -34,7 +55,7 @@ static af_array mean(const af_array &in, dim_type dim)
 
     dim4 oDims = output->dims();
 
-    Array<outType> *cnstArray = createValueArray<outType>(oDims, iDims[dim]);
+    Array<outType> *cnstArray = createValueArray<outType>(oDims, scalar<outType>(iDims[dim]));
     Array<outType> *retVal    = detail::arithOp<outType, af_div_t>(*output, *cnstArray);
 
     destroyArray<outType>(*cnstArray);
@@ -66,27 +87,27 @@ af_err af_mean(af_array *out, const af_array in, dim_type dim)
     return AF_SUCCESS;
 }
 
-af_err af_mean_all(double *real, double *imag, const af_array in)
+af_err af_mean_all(double *realVal, double *imagVal, const af_array in)
 {
     try {
         ArrayInfo info = getInfo(in);
         af_dtype type = info.getType();
         switch(type) {
-            case f64: *real = mean<double, double>(in); break;
-            case f32: *real = mean<float ,  float>(in); break;
-            case s32: *real = mean<int   ,    int>(in); break;
-            case u32: *real = mean<uint  ,   uint>(in); break;
-            case  u8: *real = mean<uchar ,   uint>(in); break;
-            case  b8: *real = mean<char  ,    int>(in); break;
+            case f64: *realVal = mean<double, double>(in); break;
+            case f32: *realVal = mean<float ,  float>(in); break;
+            case s32: *realVal = mean<int   ,    int>(in); break;
+            case u32: *realVal = mean<uint  ,   uint>(in); break;
+            case  u8: *realVal = mean<uchar ,   uint>(in); break;
+            case  b8: *realVal = mean<char  ,    int>(in); break;
             case c32: {
-                std::complex<float> tmp = mean<cfloat,cfloat>(in);
-                *real = tmp.real();
-                *imag = tmp.imag();
+                cfloat tmp = mean<cfloat,cfloat>(in);
+                *realVal = real(tmp);
+                *imagVal = imag(tmp);
                 } break;
             case c64: {
-                std::complex<double> tmp = mean<cdouble,cdouble>(in);
-                *real = tmp.real();
-                *imag = tmp.imag();
+                cdouble tmp = mean<cdouble,cdouble>(in);
+                *realVal = real(tmp);
+                *imagVal = imag(tmp);
                 } break;
             default : TYPE_ERROR(1, type);
         }
