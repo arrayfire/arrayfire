3c12121c8c809c4df0225e328f016c340f0c690a
Pavan Yalamanchili <pavan@arrayfire.com>
2015-01-03 17:07:41 -0500

    Adding proper error checking macros to src/api/c/index.cpp

--- src/api/c/index.cpp
+++ src/api/c/index.cpp
@@ -40,28 +40,26 @@ static void indexArray(af_array &dest, const af_array &src, const unsigned ndims
 
 af_err af_index(af_array *result, const af_array in, const unsigned ndims, const af_seq* index)
 {
-    //TODO: Check ndims agains max_dims
-    af_err ret = AF_ERR_INTERNAL;
     af_array out;
     try {
         if(result)  { out = *result; }
 
-        switch(getInfo(in).getType()) {
-            case f32:    indexArray<float>   (out, in, ndims, index);  break;
-            case c32:    indexArray<cfloat>  (out, in, ndims, index);  break;
-            case f64:    indexArray<double>  (out, in, ndims, index);  break;
-            case c64:    indexArray<cdouble> (out, in, ndims, index);  break;
-            case b8:     indexArray<char>    (out, in, ndims, index);  break;
-            case s32:    indexArray<int>     (out, in, ndims, index);  break;
-            case u32:    indexArray<unsigned>(out, in, ndims, index);  break;
-            case u8:     indexArray<uchar>   (out, in, ndims, index);  break;
-            default:    ret = AF_ERR_NOT_SUPPORTED;                    break;
+        af_dtype in_type = getInfo(in).getType();
+
+        switch(in_type) {
+        case f32:    indexArray<float>   (out, in, ndims, index);  break;
+        case c32:    indexArray<cfloat>  (out, in, ndims, index);  break;
+        case f64:    indexArray<double>  (out, in, ndims, index);  break;
+        case c64:    indexArray<cdouble> (out, in, ndims, index);  break;
+        case b8:     indexArray<char>    (out, in, ndims, index);  break;
+        case s32:    indexArray<int>     (out, in, ndims, index);  break;
+        case u32:    indexArray<unsigned>(out, in, ndims, index);  break;
+        case u8:     indexArray<uchar>   (out, in, ndims, index);  break;
+        default:    TYPE_ERROR(1, in_type);
         }
-        if(ret !=AF_ERR_NOT_SUPPORTED)
-            ret = AF_SUCCESS;
     }
     CATCHALL
 
     swap(*result, out);
-    return ret;
+    return AF_SUCCESS;
 }
