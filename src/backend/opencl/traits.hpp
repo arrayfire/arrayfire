f385a1a9765639bad6f8a7c639fd92d1c72a1f3c
Pavan Yalamanchili <pavan@arrayfire.com>
2015-01-26 13:44:52 -0500

    Compile fix for windows

--- src/backend/opencl/traits.hpp
+++ src/backend/opencl/traits.hpp
@@ -26,11 +26,6 @@ struct dtype_traits<cl_double2> {
     static const char* getName() { return "double2"; }
 };
 
-template<>
-struct dtype_traits<size_t> {
-    static const char* getName() { return "size_t"; };
-};
-
 template<typename T> static bool iscplx() { return false; }
 template<> STATIC_ bool iscplx<cl_float2>() { return true; }
 template<> STATIC_ bool iscplx<cl_double2>() { return true; }
