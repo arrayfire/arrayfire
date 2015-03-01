f76d51e98d85c0d4fe5641f04ecb94900c42f96d
Pradeep <pradeep@arrayfire.com>
2015-03-01 17:03:47 -0500

    FEATURE: colorspace function
    This is a convience function that provides a unified interface
    for the following color space conversions
    * RGB to Grayscale
    * Grayscale to RGB
    * RGB to HSV
    * HSV to RGB

---
+++ src/api/c/colorspace.cpp
@@ -0,0 +1,27 @@
+/*******************************************************
+ * Copyright (c) 2014, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#include <af/defines.h>
+#include <af/image.h>
+#include <err_common.hpp>
+
+af_err af_colorspace(af_array *out, const af_array image, af_cspace_t to, af_cspace_t from)
+{
+    bool hsv2rgb  = (from==AF_HSV  && to==AF_RGB );
+    bool rgb2hsv  = (from==AF_RGB  && to==AF_HSV );
+    bool gray2rgb = (from==AF_GRAY && to==AF_RGB );
+    bool rgb2gray = (from==AF_RGB  && to==AF_GRAY);
+
+    ARG_ASSERT(2, (hsv2rgb || rgb2hsv || gray2rgb || rgb2gray));
+
+    if (hsv2rgb)  return  af_hsv2rgb(out, image);
+    if (rgb2hsv)  return  af_rgb2hsv(out, image);
+    if (gray2rgb) return af_gray2rgb(out, image, 1.0f, 1.0f, 1.0f);
+    if (rgb2gray) return af_rgb2gray(out, image, 0.2126f, 0.7152f, 0.0722f);
+}
