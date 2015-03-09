1d47b4875a591f934790ddbc2066573dea885392
Umar Arshad <umar@arrayfire.com>
2015-03-09 00:16:59 -0400

    Make tests using libstdc++ for clang builds on OSX
    Use libstdc++ for tests and gtest when building with clang on OSX
    when building CUDA.

--- CMakeModules/build_gtest.cmake
+++ CMakeModules/build_gtest.cmake
@@ -27,34 +27,44 @@ set(prefix "${CMAKE_BINARY_DIR}/third_party/gtest")
 # the binary dir must be know before creating the external project in order
 # to pass the byproducts
 set(binary_dir "${prefix}/src/googletest-build")
+set(stdlib_binary_dir "${prefix}/src/googletest-build-stdlib")
 
 set(GTEST_LIBRARIES gtest gtest_main)
+set(GTEST_LIBRARIES_STDLIB gtest_stdlib gtest_main_stdlib)
 
 set(byproducts)
+set(byproducts_libstdcpp)
 foreach(lib ${GTEST_LIBRARIES})
+    message(${lib})
     set(${lib}_location
         ${binary_dir}/${CMAKE_CFG_INTDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib}${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(${lib}_location_libstdcpp
+        ${stdlib_binary_dir}/${CMAKE_CFG_INTDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib}${CMAKE_STATIC_LIBRARY_SUFFIX})
     list(APPEND byproducts ${${lib}_location})
+    list(APPEND byproducts_libstdcpp ${${lib}_location_libstdcpp})
 endforeach()
+SET(CMAKE_CXX_FLAGS_STD "${CMAKE_CXX_FLAGS} -stdlib=libstdc++")
 
+FUNCTION(GTEST_BUILD BUILD_NAME BUILD_TYPE BUILD_BINARY_DIR BUILD_BYPRODUCTS)
 # Add gtest
 ExternalProject_Add(
-    googletest
+    ${BUILD_NAME}
     # URL http://googletest.googlecode.com/files/gtest-1.7.0.zip
     # URL_MD5 2d6ec8ccdf5c46b05ba54a9fd1d130d7
     SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../test/gtest"
     PREFIX ${prefix}
-    BINARY_DIR ${binary_dir}
+    BINARY_DIR ${BUILD_BINARY_DIR}
     TIMEOUT 10
-    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-               -Dgtest_force_shared_crt=ON
+    CMAKE_ARGS -Dgtest_force_shared_crt=ON
                -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+               -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
                -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+               -DCMAKE_CXX_FLAGS_LIBSTDCPP=${CMAKE_CXX_FLAGS_STD}
                -DCMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS_DEBUG}
                -DCMAKE_CXX_FLAGS_MINSIZEREL=${CMAKE_CXX_FLAGS_MINSIZEREL}
                -DCMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS_RELEASE}
                -DCMAKE_CXX_FLAGS_RELWITHDEBINFO=${CMAKE_CXX_FLAGS_RELWITHDEBINFO}
-    BUILD_BYPRODUCTS ${byproducts}
+    BUILD_BYPRODUCTS ${BUILD_BYPRODUCTS}
     # Disable install step
     INSTALL_COMMAND ""
     # Wrap download, configure and build steps in a script to log output
@@ -62,11 +72,29 @@ ExternalProject_Add(
     LOG_UPDATE 0
     LOG_CONFIGURE 0
     LOG_BUILD 0)
+ENDFUNCTION(GTEST_BUILD)
+
+GTEST_BUILD(googletest              ${CMAKE_BUILD_TYPE} ${binary_dir} ${byproducts})
+
+IF("${APPLE}")
+    IF(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+        GTEST_BUILD(googletest_libstdcpp    LibStdCpp           ${stdlib_binary_dir} ${byproducts_libstdcpp})
+    ENDIF(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+ENDIF("${APPLE}")
+
 
 foreach(lib ${GTEST_LIBRARIES})
     add_library(${lib} IMPORTED STATIC)
     add_dependencies(${lib} googletest)
     set_target_properties(${lib} PROPERTIES IMPORTED_LOCATION ${${lib}_location})
+
+    IF("${APPLE}")
+        IF(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+            add_library(${lib}_stdlib IMPORTED STATIC)
+            add_dependencies(${lib}_stdlib googletest_libstdcpp)
+            set_target_properties(${lib}_stdlib PROPERTIES IMPORTED_LOCATION ${${lib}_location_libstdcpp})
+        ENDIF(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+    ENDIF("${APPLE}")
 endforeach()
 
 # Specify include dir
