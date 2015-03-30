89124e96fccdd4a879e8dbc5b27c92e73a3049cb
Shehzan Mohammed <shehzan@arrayfire.com>
2015-03-30 16:06:26 -0400

    Formatting and case in cmake files

--- CMakeModules/build_boost_compute.cmake
+++ CMakeModules/build_boost_compute.cmake
@@ -1,46 +1,46 @@
-set(VER 415e7a0a21b7fc2cb5a6aaa7ea14a4f32e884631)
-set(URL https://github.com/kylelutz/compute/archive/${VER}.tar.gz)
-set(MD5 733daf88c39c7337c51cd5adb0efb51b)
+SET(VER 415e7a0a21b7fc2cb5a6aaa7ea14a4f32e884631)
+SET(URL https://github.com/kylelutz/compute/archive/${VER}.tar.gz)
+SET(MD5 733daf88c39c7337c51cd5adb0efb51b)
 
-set(thirdPartyDir "${CMAKE_BINARY_DIR}/third_party")
-set(srcDir "${thirdPartyDir}/compute-${VER}")
-set(archive ${srcDir}.tar.gz)
-set(inflated ${srcDir}-inflated)
+SET(thirdPartyDir "${CMAKE_BINARY_DIR}/third_party")
+SET(srcDir "${thirdPartyDir}/compute-${VER}")
+SET(archive ${srcDir}.tar.gz)
+SET(inflated ${srcDir}-inflated)
 
 # the config to be used in the code
-set(BoostCompute_INCLUDE_DIRS "${srcDir}/include")
+SET(boostcompute_include_dirs "${srcDir}/include")
 
 # do we have to do it again?
-set(doExtraction ON)
-if(EXISTS "${inflated}")
-  file(READ "${inflated}" extractedMD5)
-  if("${extractedMD5}" STREQUAL "${MD5}")
-    # nope, everything looks fine
-    return()
-  endif()
-endif()
+SET(doExtraction ON)
+IF(EXISTS "${inflated}")
+    FILE(READ "${inflated}" extractedMD5)
+    IF("${extractedMD5}" STREQUAL "${MD5}")
+        # nope, everything looks fine
+        return()
+    ENDIF()
+ENDIF()
 
 # lets get and extract boost compute
 
-message(STATUS "BoostCompute...")
-if(EXISTS "${archive}")
-  file(MD5 "${archive}" md5)
-  if(NOT "${md5}" STREQUAL "${MD5}")
-    message("  wrong check sum ${md5}, redownloading")
-    file(REMOVE "${archive}")
-  endif()
-endif()
+MESSAGE(STATUS "BoostCompute...")
+IF(EXISTS "${archive}")
+    FILE(MD5 "${archive}" md5)
+    IF(NOT "${md5}" STREQUAL "${MD5}")
+        MESSAGE("  wrong check sum ${md5}, redownloading")
+        FILE(REMOVE "${archive}")
+    ENDIF()
+ENDIF()
 
-if(NOT EXISTS "${archive}")
-  message(STATUS "  getting ${URL}")
-  file(DOWNLOAD "${URL}" ${archive}
-    STATUS rv
-    SHOW_PROGRESS)
-endif()
+IF(NOT EXISTS "${archive}")
+    MESSAGE(STATUS "  getting ${URL}")
+    FILE(DOWNLOAD "${URL}" ${archive}
+        STATUS rv
+        SHOW_PROGRESS)
+ENDIF()
 
-message(STATUS "  validating ${archive}")
-file(MD5 "${archive}" md5)
-if(NOT "${md5}" STREQUAL "${MD5}")
+MESSAGE(STATUS "  validating ${archive}")
+FILE(MD5 "${archive}" md5)
+IF(NOT "${md5}" STREQUAL "${MD5}")
     MESSAGE(WARNING "${archive}: Invalid check sum ${md5}. Expected was ${MD5}")
     IF("${md5}" STREQUAL "d41d8cd98f00b204e9800998ecf8427e")
         MESSAGE(STATUS "Trying wget ${URL}")
@@ -51,20 +51,20 @@ if(NOT "${md5}" STREQUAL "${MD5}")
         ENDIF(NOT "${md5_}" STREQUAL "${MD5}")
         MESSAGE(STATUS "wget successful")
     ENDIF("${md5}" STREQUAL "d41d8cd98f00b204e9800998ecf8427e")
-endif()
+ENDIF()
 
-if(IS_DIRECTORY ${srcDir})
-  message(STATUS "  cleaning ${cleaning}")
-  file(REMOVE_RECURSE ${srcDir})
-endif()
+IF(IS_DIRECTORY ${srcDir})
+    MESSAGE(STATUS "  cleaning ${cleaning}")
+    FILE(REMOVE_RECURSE ${srcDir})
+ENDIF()
 
-message(STATUS "  extracting ${archive}")
-file(MAKE_DIRECTORY ${srcDir})
-execute_process(COMMAND ${CMAKE_COMMAND} -E tar xfz ${archive}
-  WORKING_DIRECTORY ${thirdPartyDir}
-  RESULT_VARIABLE rv)
-if(NOT rv EQUAL 0)
-  message(FATAL_ERROR "'${archive}' extraction failed")
-endif()
+MESSAGE(STATUS "  extracting ${archive}")
+FILE(MAKE_DIRECTORY ${srcDir})
+EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E tar xfz ${archive}
+    WORKING_DIRECTORY ${thirdPartyDir}
+    RESULT_VARIABLE rv)
+IF(NOT rv EQUAL 0)
+    MESSAGE(FATAL_ERROR "'${archive}' extraction failed")
+ENDIF()
 
-file(WRITE ${inflated} "${MD5}")
+FILE(WRITE ${inflated} "${MD5}")
--- CMakeModules/build_clBLAS.cmake
+++ CMakeModules/build_clBLAS.cmake
@@ -1,22 +1,22 @@
-include(ExternalProject)
+INCLUDE(ExternalProject)
 
-set(prefix ${CMAKE_BINARY_DIR}/third_party/clBLAS)
-set(clBLAS_location ${prefix}/lib/import/${CMAKE_STATIC_LIBRARY_PREFIX}clBLAS${CMAKE_STATIC_LIBRARY_SUFFIX})
-if(CMAKE_VERSION VERSION_LESS 3.2 AND CMAKE_GENERATOR MATCHES "Ninja")
-  message(WARNING "Building clBLAS with Ninja has known issues with CMake older than 3.2")
-  set(byproducts)
-else()
-  set(byproducts BYPRODUCTS ${clBLAS_location})
-endif()
+SET(prefix ${CMAKE_BINARY_DIR}/third_party/clBLAS)
+SET(clBLAS_location ${prefix}/lib/import/${CMAKE_STATIC_LIBRARY_PREFIX}clBLAS${CMAKE_STATIC_LIBRARY_SUFFIX})
+IF(CMAKE_VERSION VERSION_LESS 3.2 AND CMAKE_GENERATOR MATCHES "Ninja")
+    message(WARNING "Building clBLAS with Ninja has known issues with CMake older than 3.2")
+    SET(byproducts)
+ELSE()
+    SET(byproducts BYPRODUCTS ${clBLAS_location})
+ENDIF()
 
 ExternalProject_Add(
-  clBLAS-external
-  GIT_REPOSITORY https://github.com/arrayfire/clBLAS.git
-  GIT_TAG 47662a6ac1186c756508109d7fef8827efab4504
-  PREFIX "${prefix}"
-  INSTALL_DIR "${prefix}"
-  UPDATE_COMMAND ""
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -Wno-dev "-G${CMAKE_GENERATOR}" <SOURCE_DIR>/src
+    clBLAS-external
+    GIT_REPOSITORY https://github.com/arrayfire/clBLAS.git
+    GIT_TAG 47662a6ac1186c756508109d7fef8827efab4504
+    PREFIX "${prefix}"
+    INSTALL_DIR "${prefix}"
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -Wno-dev "-G${CMAKE_GENERATOR}" <SOURCE_DIR>/src
     -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
     "-DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS} -w -fPIC"
     -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
@@ -28,12 +28,12 @@ ExternalProject_Add(
     -DBUILD_TEST:BOOL=OFF
     -DBUILD_KTEST:BOOL=OFF
     -DSUFFIX_LIB:STRING=
-  )
+    )
 
 ExternalProject_Get_Property(clBLAS-external install_dir)
-add_library(clBLAS IMPORTED STATIC)
-set_target_properties(clBLAS PROPERTIES IMPORTED_LOCATION ${clBLAS_location})
-add_dependencies(clBLAS clBLAS-external)
-set(CLBLAS_INCLUDE_DIRS ${install_dir}/include)
-set(CLBLAS_LIBRARIES clBLAS)
-set(CLBLAS_FOUND ON)
+ADD_LIBRARY(clBLAS IMPORTED STATIC)
+SET_TARGET_PROPERTIES(clBLAS PROPERTIES IMPORTED_LOCATION ${clBLAS_location})
+ADD_DEPENDENCIES(clBLAS clBLAS-external)
+SET(CLBLAS_INCLUDE_DIRS ${install_dir}/include)
+SET(CLBLAS_LIBRARIES clBLAS)
+SET(CLBLAS_FOUND ON)
