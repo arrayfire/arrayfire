1387fd4ae874fe4422dc23a576f0155984c1cc2e
Pavan Yalamanchili <pavan@arrayfire.com>
2015-04-28 15:24:04 -0400

    FEAT: LU decomposition for OpenCL backend

--- src/backend/opencl/lu.cpp
+++ src/backend/opencl/lu.cpp
@@ -11,20 +11,76 @@
 #include <err_common.hpp>
 
 #if defined(WITH_OPENCL_LINEAR_ALGEBRA)
+#include <magma/magma.h>
+#include <kernel/lu_split.hpp>
+#include <copy.hpp>
 
 namespace opencl
 {
 
+Array<int> convertPivot(int *ipiv, int num)
+{
+    int *out = new int[num];
+
+    for (int i = 0; i < num; i++) {
+        out[i] = i;
+    }
+
+    for(int j = 0; j < num; j++) {
+        // 1 indexed in pivot
+        std::swap(out[j], out[ipiv[j] - 1]);
+    }
+
+    Array<int> res = createHostDataArray(dim4(num), out);
+    delete[] out;
+    return res;
+}
+
 template<typename T>
 void lu(Array<T> &lower, Array<T> &upper, Array<int> &pivot, const Array<T> &in)
 {
-    AF_ERROR("Linear Algebra is disabled on OpenCL", AF_ERR_NOT_CONFIGURED);
+    dim4 iDims = in.dims();
+    int M = iDims[0];
+    int N = iDims[1];
+    int MN = std::min(M, N);
+    int *ipiv = new int[MN];
+
+    Array<T> in_copy = copyArray<T>(in);
+
+    cl::Buffer *in_buf = in_copy.get();
+    int info = 0;
+    magma_getrf_gpu<T>(M, N, (*in_buf)(), in.getOffset(), in.strides()[1],
+                       ipiv, getQueue()(), &info);
+
+    // SPLIT into lower and upper
+    dim4 ldims(M, MN);
+    dim4 udims(MN, N);
+    lower = createEmptyArray<T>(ldims);
+    upper = createEmptyArray<T>(udims);
+
+    kernel::lu_split<T>(lower, upper, in_copy);
+
+    pivot = convertPivot(ipiv, MN);
+    delete[] ipiv;
 }
 
 template<typename T>
 Array<int> lu_inplace(Array<T> &in, const bool convert_pivot)
 {
-    AF_ERROR("Linear Algebra is disabled on OpenCL", AF_ERR_NOT_CONFIGURED);
+    dim4 iDims = in.dims();
+    int M = iDims[0];
+    int N = iDims[1];
+    int MN = std::min(M, N);
+    int *ipiv = new int[MN];
+
+    cl::Buffer *in_buf = in.get();
+    int info = 0;
+    magma_getrf_gpu<T>(M, N, (*in_buf)(), in.getOffset(), in.strides()[1],
+                       ipiv, getQueue()(), &info);
+
+    Array<int> pivot = convertPivot(ipiv, MN);
+    delete[] ipiv;
+    return pivot;
 }
 
 #define INSTANTIATE_LU(T)                                                                           \
@@ -67,4 +123,3 @@ INSTANTIATE_LU(cdouble)
 }
 
 #endif
-
