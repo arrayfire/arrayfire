0ef9f845b59d8d037edb1c7b712b6a5c00e5aa42
Pavan Yalamanchili <pavan@arrayfire.com>
2015-04-30 15:45:57 -0400

    TEST: Adding tests for cholesky for large matrix sizes

--- test/cholesky.cpp
+++ test/cholesky.cpp
@@ -27,39 +27,84 @@ using af::cdouble;
 
 ///////////////////////////////// CPP ////////////////////////////////////
 //
-TEST(Cholesky, CPP)
+// TEST(Cholesky, CPP)
+// {
+//     if (noDoubleTests<float>()) return;
+
+//     int resultIdx = 0;
+
+//     vector<af::dim4> numDims;
+//     vector<vector<float> > in;
+//     vector<vector<float> > tests;
+//     readTests<float, float, float>(string(TEST_DIR"/lapack/cholesky.test"),numDims,in,tests);
+
+//     af::dim4 idims = numDims[0];
+//     af::array input(idims, &(in[0].front()));
+//     int info;
+//     af::array output = af::cholesky(input, &info, false);
+
+//     af::dim4 odims = output.dims();
+
+//     // Get result
+//     float* outData = new float[tests[resultIdx].size()];
+//     output.host((void*)outData);
+
+//     // Compare result
+//     for (int y = 0; y < odims[1]; ++y) {
+//         for (int x = 0; x < odims[0]; ++x) {
+//             // Test only lower half
+//             if( x >= y) {
+//                 int elIter = y * odims[0] + x;
+//                 ASSERT_NEAR(tests[resultIdx][elIter], outData[elIter], 0.001) << "at: " << elIter << std::endl;
+//             }
+//         }
+//     }
+
+//     // Delete
+//     delete[] outData;
+// }
+
+template<typename T>
+void choleskyTester(const int n, double eps, bool is_upper)
 {
-    if (noDoubleTests<float>()) return;
+    if (noDoubleTests<T>()) return;
 
-    int resultIdx = 0;
+    af::dtype ty = (af::dtype)af::dtype_traits<T>::af_type;
 
-    vector<af::dim4> numDims;
-    vector<vector<float> > in;
-    vector<vector<float> > tests;
-    readTests<float, float, float>(string(TEST_DIR"/lapack/cholesky.test"),numDims,in,tests);
+    // Prepare positive definite matrix
+    af::array a = af::randu(n, n, ty);
+    af::array b = 5 * n * af::identity(n, n, ty);
+    af::array in = matmul(a.H(), a) + b;
 
-    af::dim4 idims = numDims[0];
-    af::array input(idims, &(in[0].front()));
-    int info;
-    af::array output = af::cholesky(input, &info, false);
+    int info = 0;
+    af::array out = cholesky(in, &info, is_upper);
 
-    af::dim4 odims = output.dims();
+    af::array re = is_upper ? matmul(out.H(), out) : matmul(out, out.H());
 
-    // Get result
-    float* outData = new float[tests[resultIdx].size()];
-    output.host((void*)outData);
-
-    // Compare result
-    for (int y = 0; y < odims[1]; ++y) {
-        for (int x = 0; x < odims[0]; ++x) {
-            // Test only lower half
-            if( x >= y) {
-                int elIter = y * odims[0] + x;
-                ASSERT_NEAR(tests[resultIdx][elIter], outData[elIter], 0.001) << "at: " << elIter << std::endl;
-            }
-        }
-    }
-
-    // Delete
-    delete[] outData;
+    ASSERT_NEAR(0, af::max<double>(af::abs(real(in - re))), eps);
+    ASSERT_NEAR(0, af::max<double>(af::abs(imag(in - re))), eps);
 }
+
+#define CHOLESKY_BIG_TESTS(T, eps)              \
+    TEST(Cholesky, T##Upper)                    \
+    {                                           \
+        choleskyTester<T>( 500, eps, true );    \
+    }                                           \
+    TEST(Cholesky, T##Lower)                    \
+    {                                           \
+        choleskyTester<T>(1000, eps, false);    \
+    }                                           \
+    TEST(Cholesky, T##UpperMultiple)            \
+    {                                           \
+        choleskyTester<T>(1024, eps, true );    \
+    }                                           \
+    TEST(Cholesky, T##LowerMultiple)            \
+    {                                           \
+        choleskyTester<T>( 512, eps, false);    \
+    }                                           \
+
+
+CHOLESKY_BIG_TESTS(float, 0.05)
+CHOLESKY_BIG_TESTS(double, 1E-8)
+CHOLESKY_BIG_TESTS(cfloat, 0.05)
+CHOLESKY_BIG_TESTS(cdouble, 1E-8)
