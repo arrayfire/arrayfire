20697d0e7a86fbe2e8101037102ce81ba0b5c976
Shehzan Mohammed <shehzan@arrayfire.com>
2015-05-13 12:44:50 -0400

    Added options for static FreeImage in CMAKE

--- CMakeModules/FindFreeImage.cmake
+++ CMakeModules/FindFreeImage.cmake
@@ -7,6 +7,8 @@
 # FREEIMAGE_LIBRARY
 #
 
+OPTION(FREEIMAGE_STATIC "Use Static FreeImage Lib" OFF)
+
 FIND_PATH( FREEIMAGE_INCLUDE_PATH
         NAMES FreeImage.h
         HINTS ${PROJECT_SOURCE_DIR}/extern/FreeImage
@@ -16,6 +18,21 @@ FIND_PATH( FREEIMAGE_INCLUDE_PATH
         /sw/include
         /opt/local/include
         DOC "The directory where FreeImage.h resides")
+IF(FREEIMAGE_STATIC)
+  SET(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
+  FIND_LIBRARY( FREEIMAGE_LIBRARY
+  NAMES FreeImageLIB FreeImage freeimage
+        HINTS ${PROJECT_SOURCE_DIR}/FreeImage
+        PATHS
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        /sw/lib
+        /opt/local/lib
+        DOC "The FreeImage library")
+  ADD_DEFINITIONS(-DFREEIMAGE_LIB)
+ELSE(FREEIMAGE_STATIC)
 FIND_LIBRARY( FREEIMAGE_LIBRARY
         NAMES FreeImage freeimage
         HINTS ${PROJECT_SOURCE_DIR}/FreeImage
@@ -27,13 +44,14 @@ FIND_LIBRARY( FREEIMAGE_LIBRARY
         /sw/lib
         /opt/local/lib
         DOC "The FreeImage library")
+ENDIF(FREEIMAGE_STATIC)
 
 SET(FREEIMAGE_INCLUDE_DIRS ${FREEIMAGE_INCLUDE_PATH})
 SET(FREEIMAGE_LIBRARIES ${FREEIMAGE_LIBRARY})
 
 MARK_AS_ADVANCED(
     FREEIMAGE_LIBRARY
-        FREEIMAGE_INCLUDE_PATH)
+    FREEIMAGE_INCLUDE_PATH)
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(FREEIMAGE DEFAULT_MSG
     FREEIMAGE_INCLUDE_PATH FREEIMAGE_LIBRARY)
