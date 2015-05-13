8283d618f4ea6618495f4e2fc90c8e25af11ce94
Shehzan Mohammed <shehzan@arrayfire.com>
2015-05-13 13:55:48 -0400

    Updated FindFreeImage.cmake to handle switch of lib type

--- CMakeModules/FindFreeImage.cmake
+++ CMakeModules/FindFreeImage.cmake
@@ -10,44 +10,48 @@
 OPTION(FREEIMAGE_STATIC "Use Static FreeImage Lib" OFF)
 
 FIND_PATH( FREEIMAGE_INCLUDE_PATH
-        NAMES FreeImage.h
-        HINTS ${PROJECT_SOURCE_DIR}/extern/FreeImage
-        PATHS
-        /usr/include
-        /usr/local/include
-        /sw/include
-        /opt/local/include
-        DOC "The directory where FreeImage.h resides")
-IF(FREEIMAGE_STATIC)
-  SET(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
-  FIND_LIBRARY( FREEIMAGE_LIBRARY
-  NAMES FreeImageLIB FreeImage freeimage
-        HINTS ${PROJECT_SOURCE_DIR}/FreeImage
-        PATHS
-        /usr/lib64
-        /usr/lib
-        /usr/local/lib64
-        /usr/local/lib
-        /sw/lib
-        /opt/local/lib
-        DOC "The FreeImage library")
-  ADD_DEFINITIONS(-DFREEIMAGE_LIB)
-ELSE(FREEIMAGE_STATIC)
-FIND_LIBRARY( FREEIMAGE_LIBRARY
-        NAMES FreeImage freeimage
-        HINTS ${PROJECT_SOURCE_DIR}/FreeImage
-        PATHS
-        /usr/lib64
-        /usr/lib
-        /usr/local/lib64
-        /usr/local/lib
-        /sw/lib
-        /opt/local/lib
-        DOC "The FreeImage library")
-ENDIF(FREEIMAGE_STATIC)
+    NAMES FreeImage.h
+    HINTS ${PROJECT_SOURCE_DIR}/extern/FreeImage
+    PATHS
+    /usr/include
+    /usr/local/include
+    /sw/include
+    /opt/local/include
+    DOC "The directory where FreeImage.h resides")
 
-SET(FREEIMAGE_INCLUDE_DIRS ${FREEIMAGE_INCLUDE_PATH})
-SET(FREEIMAGE_LIBRARIES ${FREEIMAGE_LIBRARY})
+FIND_LIBRARY( FREEIMAGE_DYNAMIC_LIBRARY
+    NAMES FreeImage freeimage
+    HINTS ${PROJECT_SOURCE_DIR}/FreeImage
+    PATHS
+    /usr/lib64
+    /usr/lib
+    /usr/local/lib64
+    /usr/local/lib
+    /sw/lib
+    /opt/local/lib
+    DOC "The FreeImage library")
+
+SET(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
+FIND_LIBRARY( FREEIMAGE_STATIC_LIBRARY
+    NAMES FreeImageLIB FreeImage freeimage
+    HINTS ${PROJECT_SOURCE_DIR}/FreeImage
+    PATHS
+    /usr/lib64
+    /usr/lib
+    /usr/local/lib64
+    /usr/local/lib
+    /sw/lib
+    /opt/local/lib
+    DOC "The FreeImage library")
+
+IF(FREEIMAGE_STATIC)
+  MESSAGE(STATUS "Using Static FreeImage Lib")
+  ADD_DEFINITIONS(-DFREEIMAGE_LIB)
+  SET(FREEIMAGE_LIBRARY ${FREEIMAGE_STATIC_LIBRARY})
+ELSE(FREEIMAGE_STATIC)
+  MESSAGE(STATUS "Using Dynamic FreeImage Lib")
+  SET(FREEIMAGE_LIBRARY ${FREEIMAGE_DYNAMIC_LIBRARY})
+ENDIF(FREEIMAGE_STATIC)
 
 MARK_AS_ADVANCED(
     FREEIMAGE_LIBRARY
