9613d854c58fbe7f68bd192acfa7204a81155d5b
Pavan Yalamanchili <pavan@arrayfire.com>
2015-05-17 23:33:53 -0400

    TEST: Adding more tests for iir filter

--- test/iir.cpp
+++ test/iir.cpp
@@ -143,3 +143,49 @@ TYPED_TEST(filter, iirA0MatMat)
 {
     iirA0Test<TypeParam>(5000, 10, 50, 10);
 }
+
+template<typename T>
+void iirTest(const char *testFile)
+{
+    if (noDoubleTests<T>()) return;
+    vector<af::dim4> inDims;
+
+    vector<vector<T> > inputs;
+    vector<vector<T> > outputs;
+    readTests<T, T, float> (testFile, inDims, inputs, outputs);
+
+    try {
+        af::array a = af::array(inDims[0], &inputs[0][0]);
+        af::array b = af::array(inDims[1], &inputs[1][0]);
+        af::array x = af::array(inDims[2], &inputs[2][0]);
+
+        af::array y = af::iir(b, a, x);
+        std::vector<T> gold = outputs[0];
+        ASSERT_EQ(gold.size(), (size_t)y.elements());
+
+        std::vector<T> out(y.elements());
+        y.host(&out[0]);
+
+        for(size_t i = 0; i < gold.size(); i++) {
+            ASSERT_NEAR(get_real(out[i]), get_real(gold[i]), 0.01) << "at: " << i;
+        }
+
+    } catch (af::exception &ex) {
+        FAIL() << ex.what();
+    }
+}
+
+TYPED_TEST(filter, iirVecVec)
+{
+    iirTest<TypeParam>(TEST_DIR"/iir/iir_vv.test");
+}
+
+TYPED_TEST(filter, iirVecMat)
+{
+    iirTest<TypeParam>(TEST_DIR"/iir/iir_vm.test");
+}
+
+TYPED_TEST(filter, iirMatMat)
+{
+    iirTest<TypeParam>(TEST_DIR"/iir/iir_mm.test");
+}
