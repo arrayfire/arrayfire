dbbf18a05811080a743a52ae537c02869bdb7e5a
Pavan Yalamanchili <pavan@arrayfire.com>
2015-05-25 02:39:11 -0400

    STYLE: Fixing the way debug info is displayed when building opencl programs

--- src/backend/opencl/program.hpp
+++ src/backend/opencl/program.hpp
@@ -20,6 +20,31 @@ using cl::EnqueueArgs;
 using cl::NDRange;
 using std::string;
 
+#define SHOW_DEBUG_BUILD_INFO(PROG) do {                                \
+        cl_uint numDevices = PROG.getInfo<CL_PROGRAM_NUM_DEVICES>();    \
+        for (unsigned int i = 0; i<numDevices; ++i) {                   \
+            std::cout << PROG.getBuildInfo<CL_PROGRAM_BUILD_LOG>(       \
+                PROG.getInfo<CL_PROGRAM_DEVICES>()[i]) << std::endl;    \
+                                                                        \
+            std::cout << PROG.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(   \
+                PROG.getInfo<CL_PROGRAM_DEVICES>()[i]) << std::endl;    \
+        }                                                               \
+    } while(0)                                                          \
+
+
+#if defined(NDEBUG)
+
+#define SHOW_BUILD_INFO(PROG) do {                                  \
+        const char *info = getenv("AF_OPENCL_SHOW_BUILD_INFO");     \
+        if (info != nullptr && std::strncmp(info,"0", 1) != 0) {    \
+            SHOW_DEBUG_BUILD_INFO(prog);                            \
+        }                                                           \
+    } while(0)
+
+#else
+#define SHOW_BUILD_INFO(PROG) SHOW_DEBUG_BUILD_INFO(PROG)
+#endif
+
 namespace opencl
 {
     void buildProgram(cl::Program &prog,
