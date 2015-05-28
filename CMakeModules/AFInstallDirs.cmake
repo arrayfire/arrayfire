cac53d8da1bb8599be8f00a266efdab41743fa75
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-05-28 18:35:53 +0100

    make paths overridable

--- CMakeModules/AFInstallDirs.cmake
+++ CMakeModules/AFInstallDirs.cmake
@@ -2,11 +2,39 @@
 # Sets ArrayFire installation paths.
 #
 
-# These paths are all relative to the project installation prefix.
-set(AF_INSTALL_BIN_DIR bin CACHE PATH "Installation path for executables")
-set(AF_INSTALL_LIB_DIR lib CACHE PATH "Installation path for libraries")
-set(AF_INSTALL_INC_DIR include CACHE PATH "Installation path for headers")
-set(AF_INSTALL_DATA_DIR share/ArrayFire CACHE PATH "Installation path for data files")
-set(AF_INSTALL_DOC_DIR ${AF_INSTALL_DATA_DIR}/doc CACHE PATH "Installation path for documentation")
-set(AF_INSTALL_MAN_DIR ${AF_INSTALL_DATA_DIR}/man CACHE PATH "Installation path for man pages")
-set(AF_INSTALL_CMAKE_DIR ${AF_INSTALL_DATA_DIR}/cmake CACHE PATH "Installation path for CMake modules")
+# NOTE: These paths are all relative to the project installation prefix.
+
+# Executables
+if(NOT DEFINED AF_INSTALL_BIN_DIR)
+  set(AF_INSTALL_BIN_DIR "bin" CACHE PATH "Installation path for executables")
+endif()
+
+# Libraries
+if(NOT DEFINED AF_INSTALL_LIB_DIR)
+  set(AF_INSTALL_LIB_DIR "lib" CACHE PATH "Installation path for libraries")
+endif()
+
+# Header files
+if(NOT DEFINED AF_INSTALL_INC_DIR)
+  set(AF_INSTALL_INC_DIR "include" CACHE PATH "Installation path for headers")
+endif()
+
+# Data files
+if(NOT DEFINED AF_INSTALL_DATA_DIR)
+  set(AF_INSTALL_DATA_DIR "share/ArrayFire" CACHE PATH "Installation path for data files")
+endif()
+
+# Documentation
+if(NOT DEFINED AF_INSTALL_DOC_DIR)
+  set(AF_INSTALL_DOC_DIR "${AF_INSTALL_DATA_DIR}/doc" CACHE PATH "Installation path for documentation")
+endif()
+
+# Man pages
+if(NOT DEFINED AF_INSTALL_MAN_DIR)
+  set(AF_INSTALL_MAN_DIR "${AF_INSTALL_DATA_DIR}/man" CACHE PATH "Installation path for man pages")
+endif()
+
+# CMake files
+if(NOT DEFINED AF_INSTALL_CMAKE_DIR)
+  set(AF_INSTALL_CMAKE_DIR "${AF_INSTALL_DATA_DIR}/cmake" CACHE PATH "Installation path for CMake files")
+endif()
