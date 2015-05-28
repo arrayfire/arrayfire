8c2156a6b79c414d16722e597ada88e53537bb68
Pavan Yalamanchili <pavan@arrayfire.com>
2015-05-27 21:00:56 -0400

    BUGFIX: Convolve in OpenCL now work when filters are sub arrays

--- src/backend/opencl/kernel/convolve/conv3.cpp
+++ src/backend/opencl/kernel/convolve/conv3.cpp
@@ -20,12 +20,13 @@ void conv3(conv_kparam_t& p, Param& out, const Param& sig, const Param& filt)
 {
     size_t se_size = filt.info.dims[0] * filt.info.dims[1] * filt.info.dims[2] * sizeof(aT);
     p.impulse = bufferAlloc(se_size);
+    int f0Off = filt.info.offset;
 
     for (int b3=0; b3<filt.info.dims[3]; ++b3) {
         int f3Off = b3 * filt.info.strides[3];
         // FIXME: if the filter array is strided, direct copy of symbols
         // might cause issues
-        getQueue().enqueueCopyBuffer(*filt.data, *p.impulse, f3Off*sizeof(aT), 0, se_size);
+        getQueue().enqueueCopyBuffer(*filt.data, *p.impulse, (f0Off + f3Off)*sizeof(aT), 0, se_size);
 
         p.o[2] = (p.outHasNoOffset ? 0 : b3);
         p.s[2] = (p.inHasNoOffset ? 0 : b3);
