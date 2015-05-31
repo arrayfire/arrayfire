8fa76dc03bc87d837472f047de19b9521d5a004d
Pavan Yalamanchili <pavan@arrayfire.com>
2015-05-31 19:29:51 -0400

    Adding a specialziation for array.device<cl_mem>() in af/opencl.h

--- include/af/opencl.h
+++ include/af/opencl.h
@@ -211,4 +211,14 @@ namespace afcl
     */
 }
 
+namespace af {
+    template<> AFAPI cl_mem *array::device() const
+    {
+        cl_mem *mem = new cl_mem;
+        af_err err = af_get_device_ptr((void **)mem, get());
+        if (err != AF_SUCCESS) throw af::exception("Failed to get cl_mem from array object");
+        return mem;
+    }
+}
+
 #endif
