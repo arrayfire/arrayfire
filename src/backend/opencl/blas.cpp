29d0df71bf867d96a6f5764a1be1172c3c1463df
Umar Arshad <umar@arrayfire.com>
2015-06-26 11:07:08 -0400

    Revert "BUILD: Fix redefinition warning in blas."
    This reverts commit cf58d66c9dc13e00afa678a8b51e00144b583507.

--- src/backend/opencl/blas.cpp
+++ src/backend/opencl/blas.cpp
@@ -72,8 +72,6 @@ BLAS_FUNC(dot, float,       S)
 BLAS_FUNC(dot, double,      D)
 
 #undef BLAS_FUNC_DEF
-#undef BLAS_FUNC
-
 #define BLAS_FUNC(NAME, TYPE, PREFIX, SUFFIX)					\
 template<>                                                              \
 struct NAME##_func<TYPE>                                                \
