61ea09115415e724f8c9d594958613c54c12ee92
pradeep <pradeep@arrayfire.com>
2015-08-04 19:59:14 -0400

    FEAT: Summed Area Tables (sat, af_sat) a.k.a integral images

---
+++ src/api/cpp/sat.cpp
@@ -0,0 +1,24 @@
+/*******************************************************
+ * Copyright (c) 2014, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#include <af/image.h>
+#include <af/array.h>
+#include "error.hpp"
+
+namespace af
+{
+
+array sat(const array& in)
+{
+    af_array out = 0;
+    AF_THROW(af_sat(&out, in.get()));
+    return array(out);
+}
+
+}
