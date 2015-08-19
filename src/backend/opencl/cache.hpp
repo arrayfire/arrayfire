365afb4290d6d7763dcb58cc36b8411e04b733c7
Pavan Yalamanchili <pavan@arrayfire.com>
2015-08-19 02:48:50 -0400

    Moving the kernel cache map to a centralized location

---
+++ src/backend/opencl/cache.hpp
@@ -0,0 +1,25 @@
+/*******************************************************
+ * Copyright (c) 2015, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#pragma once
+#include <program.hpp>
+
+namespace opencl
+{
+    using cl::Kernel;
+    using cl::Program;
+
+    typedef struct {
+        Program* prog;
+        Kernel* ker;
+    } kc_entry_t;
+
+    typedef std::map<string, kc_entry_t> kc_t;
+    static kc_t kernelCaches[DeviceManager::MAX_DEVICES];
+}
