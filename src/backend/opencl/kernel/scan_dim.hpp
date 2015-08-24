5c0da49f748b94a263a2dd7d2f7c31f71881b2c2
Pavan Yalamanchili <pavan@arrayfire.com>
2015-08-24 06:46:54 -0400

    Templated options are now runtime compile options for opencl scan

--- src/backend/opencl/kernel/scan_dim.hpp
+++ src/backend/opencl/kernel/scan_dim.hpp
@@ -19,6 +19,7 @@
 #include <Param.hpp>
 #include <debug_opencl.hpp>
 #include <type_util.hpp>
+#include <cache.hpp>
 #include "names.hpp"
 #include "config.hpp"
 
@@ -34,64 +35,77 @@ namespace opencl
 {
 namespace kernel
 {
-    template<typename Ti, typename To, af_op_t op, int dim, bool isFinalPass, uint threads_y>
-    static Kernel* get_scan_dim_kernels(int kerIdx)
+    template<typename Ti, typename To, af_op_t op>
+    static Kernel get_scan_dim_kernels(int kerIdx, int dim, bool isFinalPass, uint threads_y)
     {
-        try {
-            static std::once_flag compileFlags[DeviceManager::MAX_DEVICES];
-            static std::map<int, Program*> scanProgs;
-            static std::map<int, Kernel*>  scanKerns;
-            static std::map<int, Kernel*>  bcastKerns;
+        std::string ref_name =
+            std::string("scan_") +
+            std::to_string(dim) +
+            std::string("_") +
+            std::to_string(isFinalPass) +
+            std::string("_") +
+            std::string(dtype_traits<Ti>::getName()) +
+            std::string("_") +
+            std::string(dtype_traits<To>::getName()) +
+            std::string("_") +
+            std::to_string(op) +
+            std::string("_") +
+            std::to_string(threads_y);
 
-            int device= getActiveDeviceId();
+        int device = getActiveDeviceId();
+        kc_t::iterator idx = kernelCaches[device].find(ref_name);
 
-            std::call_once(compileFlags[device], [device] () {
+        kc_entry_t entry;
+        if (idx == kernelCaches[device].end()) {
 
-                    Binary<To, op> scan;
-                    ToNum<To> toNum;
+            Binary<To, op> scan;
+            ToNum<To> toNum;
 
-                    std::ostringstream options;
-                    options << " -D To=" << dtype_traits<To>::getName()
-                            << " -D Ti=" << dtype_traits<Ti>::getName()
-                            << " -D T=To"
-                            << " -D dim=" << dim
-                            << " -D DIMY=" << threads_y
-                            << " -D THREADS_X=" << THREADS_X
-                            << " -D init=" << toNum(scan.init())
-                            << " -D " << binOpName<op>()
-                            << " -D CPLX=" << af::iscplx<Ti>()
-                            << " -D isFinalPass=" << (int)(isFinalPass);
-                    if (std::is_same<Ti, double>::value ||
-                        std::is_same<Ti, cdouble>::value) {
-                        options << " -D USE_DOUBLE";
-                    }
+            std::ostringstream options;
+            options << " -D To=" << dtype_traits<To>::getName()
+                    << " -D Ti=" << dtype_traits<Ti>::getName()
+                    << " -D T=To"
+                    << " -D dim=" << dim
+                    << " -D DIMY=" << threads_y
+                    << " -D THREADS_X=" << THREADS_X
+                    << " -D init=" << toNum(scan.init())
+                    << " -D " << binOpName<op>()
+                    << " -D CPLX=" << af::iscplx<Ti>()
+                    << " -D isFinalPass=" << (int)(isFinalPass);
+            if (std::is_same<Ti, double>::value ||
+                std::is_same<Ti, cdouble>::value) {
+                options << " -D USE_DOUBLE";
+            }
 
-                    const char *ker_strs[] = {ops_cl, scan_dim_cl};
-                    const int   ker_lens[] = {ops_cl_len, scan_dim_cl_len};
-                    cl::Program prog;
-                    buildProgram(prog, 2, ker_strs, ker_lens, options.str());
-                    scanProgs[device] = new Program(prog);
+            const char *ker_strs[] = {ops_cl, scan_dim_cl};
+            const int   ker_lens[] = {ops_cl_len, scan_dim_cl_len};
+            cl::Program prog;
+            buildProgram(prog, 2, ker_strs, ker_lens, options.str());
 
-                    scanKerns[device] = new Kernel(*scanProgs[device],  "scan_dim_kernel");
-                    bcastKerns[device] = new Kernel(*scanProgs[device],  "bcast_dim_kernel");
+            entry.prog = new Program(prog);
+            entry.ker = new Kernel[2];
 
-                });
+            entry.ker[0] = Kernel(*entry.prog, "scan_dim_kernel");
+            entry.ker[1] = Kernel(*entry.prog, "bcast_dim_kernel");
 
-            return (kerIdx == 0) ? scanKerns[device] : bcastKerns[device];
-        } catch (cl::Error err) {
-            CL_TO_AF_ERROR(err);
-            throw;
+            kernelCaches[device][ref_name] = entry;
+
+        } else {
+            entry = idx->second;
         }
+
+        return entry.ker[kerIdx];
     }
 
-    template<typename Ti, typename To, af_op_t op, int dim, bool isFinalPass, uint threads_y>
+    template<typename Ti, typename To, af_op_t op>
     static void scan_dim_launcher(Param &out,
                                   Param &tmp,
                                   const Param &in,
+                                  int dim, bool isFinalPass, uint threads_y,
                                   const uint groups_all[4])
     {
         try {
-            Kernel* ker = get_scan_dim_kernels<Ti, To, op, dim, isFinalPass, threads_y>(0);
+            Kernel ker = get_scan_dim_kernels<Ti, To, op>(0, dim, isFinalPass, threads_y);
 
             NDRange local(THREADS_X, threads_y);
             NDRange global(groups_all[0] * groups_all[2] * local[0],
@@ -103,7 +117,7 @@ namespace kernel
                                       Buffer, KParam,
                                       Buffer, KParam,
                                       uint, uint,
-                                      uint, uint>(*ker);
+                                      uint, uint>(ker);
 
 
             scanOp(EnqueueArgs(getQueue(), global, local),
@@ -117,13 +131,14 @@ namespace kernel
         }
     }
 
-    template<typename Ti, typename To, af_op_t op, int dim, bool isFinalPass, uint threads_y>
+    template<typename Ti, typename To, af_op_t op>
     static void bcast_dim_launcher(Param &out,
                                    Param &tmp,
+                                   int dim, bool isFinalPass, uint threads_y,
                                    const uint groups_all[4])
     {
         try {
-            Kernel* ker = get_scan_dim_kernels<Ti, To, op, dim, isFinalPass, threads_y>(1);
+            Kernel ker = get_scan_dim_kernels<Ti, To, op>(1, dim, isFinalPass, threads_y);
 
             NDRange local(THREADS_X, threads_y);
             NDRange global(groups_all[0] * groups_all[2] * local[0],
@@ -134,7 +149,7 @@ namespace kernel
             auto bcastOp = make_kernel<Buffer, KParam,
                                        Buffer, KParam,
                                        uint, uint,
-                                       uint, uint>(*ker);
+                                       uint, uint>(ker);
 
             bcastOp(EnqueueArgs(getQueue(), global, local),
                     *out.data, out.info, *tmp.data, tmp.info,
@@ -147,57 +162,8 @@ namespace kernel
         }
     }
 
-
-    template<typename Ti, typename To, af_op_t op, int dim, bool isFinalPass>
-    static void scan_dim_fn(Param &out,
-                            Param &tmp,
-                            const Param &in,
-                            const uint threads_y,
-                            const uint groups_all[4])
-    {
-
-        switch (threads_y) {
-        case 8:
-            (scan_dim_launcher<Ti, To, op, dim, isFinalPass, 8>)(
-                out, tmp, in, groups_all); break;
-        case 4:
-            (scan_dim_launcher<Ti, To, op, dim, isFinalPass, 4>)(
-                out, tmp, in, groups_all); break;
-        case 2:
-            (scan_dim_launcher<Ti, To, op, dim, isFinalPass, 2>)(
-                out, tmp, in, groups_all); break;
-        case 1:
-            (scan_dim_launcher<Ti, To, op, dim, isFinalPass, 1>)(
-                out, tmp, in, groups_all); break;
-        }
-
-    }
-
-    template<typename Ti, typename To, af_op_t op, int dim, bool isFinalPass>
-    static void bcast_dim_fn(Param &out,
-                             Param &tmp,
-                             const uint threads_y,
-                             const uint groups_all[4])
-    {
-
-        switch (threads_y) {
-        case 8:
-            (bcast_dim_launcher<Ti, To, op, dim, isFinalPass, 8>)(
-                out, tmp, groups_all); break;
-        case 4:
-            (bcast_dim_launcher<Ti, To, op, dim, isFinalPass, 4>)(
-                out, tmp, groups_all); break;
-        case 2:
-            (bcast_dim_launcher<Ti, To, op, dim, isFinalPass, 2>)(
-                out, tmp, groups_all); break;
-        case 1:
-            (bcast_dim_launcher<Ti, To, op, dim, isFinalPass, 1>)(
-                out, tmp, groups_all); break;
-        }
-    }
-
-    template<typename Ti, typename To, af_op_t op, int dim>
-    static void scan_dim(Param &out, const Param &in)
+    template<typename Ti, typename To, af_op_t op>
+    static void scan_dim(Param &out, const Param &in, int dim)
     {
         try {
             uint threads_y = std::min(THREADS_Y, nextpow2(out.info.dims[dim]));
@@ -212,9 +178,10 @@ namespace kernel
 
             if (groups_all[dim] == 1) {
 
-                scan_dim_fn<Ti, To, op, dim, true>(out, out, in,
-                                                   threads_y,
-                                                   groups_all);
+                scan_dim_launcher<Ti, To, op>(out, out, in,
+                                              dim, true,
+                                              threads_y,
+                                              groups_all);
             } else {
 
                 Param tmp = out;
@@ -229,27 +196,31 @@ namespace kernel
                 // FIXME: Do I need to free this ?
                 tmp.data = bufferAlloc(tmp_elements * sizeof(To));
 
-                scan_dim_fn<Ti, To, op, dim, false>(out, tmp, in,
-                                                    threads_y,
-                                                    groups_all);
+                scan_dim_launcher<Ti, To, op>(out, tmp, in,
+                                              dim, false,
+                                              threads_y,
+                                              groups_all);
 
                 int gdim = groups_all[dim];
                 groups_all[dim] = 1;
 
                 if (op == af_notzero_t) {
-                    scan_dim_fn<To, To, af_add_t, dim, true>(tmp, tmp, tmp,
-                                                             threads_y,
-                                                             groups_all);
+                    scan_dim_launcher<To, To, af_add_t>(tmp, tmp, tmp,
+                                                        dim, true,
+                                                        threads_y,
+                                                        groups_all);
                 } else {
-                    scan_dim_fn<To, To,       op, dim, true>(tmp, tmp, tmp,
-                                                             threads_y,
-                                                             groups_all);
+                    scan_dim_launcher<To, To,       op>(tmp, tmp, tmp,
+                                                        dim, true,
+                                                        threads_y,
+                                                        groups_all);
                 }
 
                 groups_all[dim] = gdim;
-                bcast_dim_fn<To, To, op, dim, true>(out, tmp,
-                                                    threads_y,
-                                                    groups_all);
+                bcast_dim_launcher<To, To, op>(out, tmp,
+                                               dim, true,
+                                               threads_y,
+                                               groups_all);
                 bufferFree(tmp.data);
             }
         } catch (cl::Error err) {
