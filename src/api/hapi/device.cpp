a83b0955dc944ee8b0c688f47860ea6d1e1fba6e
Pradeep <pradeep@arrayfire.com>
2015-08-31 18:41:37 -0400

    Fixed typo in data, device & index wrapper source files

--- src/api/hapi/device.cpp
+++ src/api/hapi/device.cpp
@@ -21,98 +21,98 @@ af_err af_info()
     return CALL_NO_PARAMS();
 }
 
-AFAPI af_err af_init()
+af_err af_init()
 {
     return CALL_NO_PARAMS();
 }
 
-AFAPI af_err af_device_info(char* d_name, char* d_platform, char *d_toolkit, char* d_compute)
+af_err af_device_info(char* d_name, char* d_platform, char *d_toolkit, char* d_compute)
 {
     return CALL(d_name, d_platform, d_toolkit, d_compute);
 }
 
-AFAPI af_err af_get_device_count(int *num_of_devices)
+af_err af_get_device_count(int *num_of_devices)
 {
     return CALL(num_of_devices);
 }
 
-AFAPI af_err af_get_dbl_support(bool* available, const int device)
+af_err af_get_dbl_support(bool* available, const int device)
 {
     return CALL(available, device);
 }
 
-AFAPI af_err af_set_device(const int device)
+af_err af_set_device(const int device)
 {
     return CALL(device);
 }
 
-AFAPI af_err af_get_device(int *device)
+af_err af_get_device(int *device)
 {
     return CALL(device);
 }
 
-AFAPI af_err af_sync(const int device)
+af_err af_sync(const int device)
 {
     return CALL(device);
 }
 
-AFAPI af_err af_alloc_device(void **ptr, const dim_t bytes)
+af_err af_alloc_device(void **ptr, const dim_t bytes)
 {
     return CALL(ptr, bytes);
 }
 
-AFAPI af_err af_alloc_pinned(void **ptr, const dim_t bytes)
+af_err af_alloc_pinned(void **ptr, const dim_t bytes)
 {
     return CALL(ptr, bytes);
 }
 
-AFAPI af_err af_free_device(void *ptr)
+af_err af_free_device(void *ptr)
 {
     return CALL(ptr);
 }
 
-AFAPI af_err af_free_pinned(void *ptr)
+af_err af_free_pinned(void *ptr)
 {
     return CALL(ptr);
 }
 
-AFAPI af_err af_device_array(af_array *arr, const void *data, const unsigned ndims, const dim_t * const dims, const af_dtype type)
+af_err af_device_array(af_array *arr, const void *data, const unsigned ndims, const dim_t * const dims, const af_dtype type)
 {
     return CALL(arr, data, ndims, dims, type);
 }
 
-AFAPI af_err af_device_mem_info(size_t *alloc_bytes, size_t *alloc_buffers,
+af_err af_device_mem_info(size_t *alloc_bytes, size_t *alloc_buffers,
         size_t *lock_bytes, size_t *lock_buffers)
 {
     return CALL(alloc_bytes, alloc_buffers, lock_bytes, lock_buffers);
 }
 
-AFAPI af_err af_device_gc()
+af_err af_device_gc()
 {
     return CALL_NO_PARAMS();
 }
 
-AFAPI af_err af_set_mem_step_size(const size_t step_bytes)
+af_err af_set_mem_step_size(const size_t step_bytes)
 {
     return CALL(step_bytes);
 }
 
-AFAPI af_err af_get_mem_step_size(size_t *step_bytes)
+af_err af_get_mem_step_size(size_t *step_bytes)
 {
     return CALL(step_bytes);
 }
 
-AFAPI af_err af_lock_device_ptr(const af_array arr)
+af_err af_lock_device_ptr(const af_array arr)
 {
     return CALL(arr);
 }
 
-AFAPI af_err af_unlock_device_ptr(const af_array arr)
+af_err af_unlock_device_ptr(const af_array arr)
 {
     return CALL(arr);
 }
 
-AFAPI af_err af_get_device_ptr(void **ptr, const af_array arr)
+af_err af_get_device_ptr(void **ptr, const af_array arr)
 {
     return CALL(ptr, arr);
 }
--- src/api/hapi/index.cpp
+++ src/api/hapi/index.cpp
@@ -11,21 +11,21 @@
 #include <af/index.h>
 #include "symbol_manager.hpp"
 
-AFAPI af_err af_index(  af_array *out,
+af_err af_index(  af_array *out,
         const af_array in,
         const unsigned ndims, const af_seq* const index)
 {
     return CALL(out, in, ndims, index);
 }
 
-AFAPI af_err af_lookup( af_array *out,
+af_err af_lookup( af_array *out,
         const af_array in, const af_array indices,
         const unsigned dim)
 {
     return CALL(out, in, indices, dim);
 }
 
-AFAPI af_err af_assign_seq( af_array *out,
+af_err af_assign_seq( af_array *out,
         const af_array lhs,
         const unsigned ndims, const af_seq* const indices,
         const af_array rhs)
@@ -33,14 +33,14 @@ AFAPI af_err af_assign_seq( af_array *out,
     return CALL(out, lhs, ndims, indices, rhs);
 }
 
-AFAPI af_err af_index_gen(  af_array *out,
+af_err af_index_gen(  af_array *out,
         const af_array in,
         const dim_t ndims, const af_index_t* indices)
 {
     return CALL(out, in, ndims, indices);
 }
 
-AFAPI af_err af_assign_gen( af_array *out,
+af_err af_assign_gen( af_array *out,
         const af_array lhs,
         const dim_t ndims, const af_index_t* indices,
         const af_array rhs)
