c19058f9fbc4a6c38dd9640951314ccf6b58d0f4
pradeep <pradeep@arrayfire.com>
2015-09-29 18:51:10 -0400

    statistics functions fixes
    Earlier, some of the stats functions where casting the input
    array to float/double always. After this commit, this is done
     only in few cases where there is no alternative.

--- src/api/c/mean.cpp
+++ src/api/c/mean.cpp
@@ -22,26 +22,26 @@
 
 using namespace detail;
 
-template<typename T>
-static T mean(const af_array &in)
+template<typename Ti, typename To>
+static To mean(const af_array &in)
 {
     /* following function is defined in stats.h */
-    return mean<T>(castArray<T>(in)); /* defined in stats.h */
+    return mean<Ti, To>(getArray<Ti>(in)); /* defined in stats.h */
 }
 
 template<typename T>
 static T mean(const af_array &in, const af_array &weights)
 {
-    typedef typename baseOutType<T>::type bType;
+    typedef typename baseOutType<T>::type Tw;
     /* following function is defined in stats.h */
-    return mean<T, bType>(castArray<T>(in), castArray<bType>(weights));
+    return mean<T, Tw>(castArray<T>(in), castArray<Tw>(weights));
 }
 
-template<typename T>
+template<typename Ti, typename To>
 static af_array mean(const af_array &in, const dim_t dim)
 {
     /* following function is defined in stats.h */
-    return getHandle<T>(mean<T>(castArray<T>(in), dim));
+    return getHandle<To>(mean<Ti, To>(getArray<Ti>(in), dim));
 }
 
 template<typename T>
@@ -60,18 +60,18 @@ af_err af_mean(af_array *out, const af_array in, const dim_t dim)
         ArrayInfo info = getInfo(in);
         af_dtype type = info.getType();
         switch(type) {
-            case f64: output = mean< double>(in, dim); break;
-            case f32: output = mean< float >(in, dim); break;
-            case s32: output = mean< float >(in, dim); break;
-            case u32: output = mean< float >(in, dim); break;
-            case s64: output = mean< double>(in, dim); break;
-            case u64: output = mean< double>(in, dim); break;
-            case s16: output = mean< float >(in, dim); break;
-            case u16: output = mean< float >(in, dim); break;
-            case  u8: output = mean< float >(in, dim); break;
-            case  b8: output = mean< float >(in, dim); break;
-            case c32: output = mean< cfloat>(in, dim); break;
-            case c64: output = mean<cdouble>(in, dim); break;
+            case f64: output = mean<double  ,  double>(in, dim); break;
+            case f32: output = mean<float   ,  float >(in, dim); break;
+            case s32: output = mean<int     ,  float >(in, dim); break;
+            case u32: output = mean<unsigned,  float >(in, dim); break;
+            case s64: output = mean<intl    ,  double>(in, dim); break;
+            case u64: output = mean<uintl   ,  double>(in, dim); break;
+            case s16: output = mean<short   ,  float >(in, dim); break;
+            case u16: output = mean<ushort  ,  float >(in, dim); break;
+            case  u8: output = mean<uchar   ,  float >(in, dim); break;
+            case  b8: output = mean<char    ,  float >(in, dim); break;
+            case c32: output = mean<cfloat  ,  cfloat>(in, dim); break;
+            case c64: output = mean<cdouble , cdouble>(in, dim); break;
             default : TYPE_ERROR(1, type);
         }
         std::swap(*out, output);
@@ -120,23 +120,23 @@ af_err af_mean_all(double *realVal, double *imagVal, const af_array in)
         ArrayInfo info = getInfo(in);
         af_dtype type = info.getType();
         switch(type) {
-            case f64: *realVal = mean<double>(in); break;
-            case f32: *realVal = mean< float>(in); break;
-            case s32: *realVal = mean< float>(in); break;
-            case u32: *realVal = mean< float>(in); break;
-            case s64: *realVal = mean<double>(in); break;
-            case u64: *realVal = mean<double>(in); break;
-            case s16: *realVal = mean< float>(in); break;
-            case u16: *realVal = mean< float>(in); break;
-            case  u8: *realVal = mean< float>(in); break;
-            case  b8: *realVal = mean< float>(in); break;
+            case f64: *realVal = mean<double  , double>(in); break;
+            case f32: *realVal = mean<float   , float >(in); break;
+            case s32: *realVal = mean<int     , float >(in); break;
+            case u32: *realVal = mean<unsigned, float >(in); break;
+            case s64: *realVal = mean<intl    , double>(in); break;
+            case u64: *realVal = mean<uintl   , double>(in); break;
+            case s16: *realVal = mean<short   , float >(in); break;
+            case u16: *realVal = mean<ushort  , float >(in); break;
+            case  u8: *realVal = mean<uchar   , float >(in); break;
+            case  b8: *realVal = mean<char    , float >(in); break;
             case c32: {
-                cfloat tmp = mean<cfloat>(in);
+                cfloat tmp = mean<cfloat, cfloat>(in);
                 *realVal = real(tmp);
                 *imagVal = imag(tmp);
                 } break;
             case c64: {
-                cdouble tmp = mean<cdouble>(in);
+                cdouble tmp = mean<cdouble, cdouble>(in);
                 *realVal = real(tmp);
                 *imagVal = imag(tmp);
                 } break;
