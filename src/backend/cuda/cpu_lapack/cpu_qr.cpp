37e0658559fca47cac8bcab2cf1991de74b57061
Shehzan Mohammed <shehzan@arrayfire.com>
2015-10-08 17:58:07 -0400

    Added CPU fallback for CUDA QR when CUDA older than 7
    * Moved triangle function into it's own header file

--- src/backend/cuda/cpu_lapack/cpu_qr.cpp
+++ src/backend/cuda/cpu_lapack/cpu_qr.cpp
@@ -17,8 +17,8 @@
 #include <copy.hpp>
 #include <iostream>
 #include <cassert>
-#include <triangle.hpp>
 
+#include <cpu_lapack/cpu_triangle.hpp>
 #include "lapack_helper.hpp"
 
 namespace cuda
@@ -62,38 +62,6 @@ GQR_FUNC(gqr , float  , sorgqr)
 GQR_FUNC(gqr , double , dorgqr)
 GQR_FUNC(gqr , cfloat , cungqr)
 GQR_FUNC(gqr , cdouble, zungqr)
-
-template<typename T, bool is_upper, bool is_unit_diag>
-void triangle(T *o, const T *i, const dim4 odm, const dim4 ost, const dim4 ist)
-{
-    for(dim_t ow = 0; ow < odm[3]; ow++) {
-        const dim_t oW = ow * ost[3];
-        const dim_t iW = ow * ist[3];
-
-        for(dim_t oz = 0; oz < odm[2]; oz++) {
-            const dim_t oZW = oW + oz * ost[2];
-            const dim_t iZW = iW + oz * ist[2];
-
-            for(dim_t oy = 0; oy < odm[1]; oy++) {
-                const dim_t oYZW = oZW + oy * ost[1];
-                const dim_t iYZW = iZW + oy * ist[1];
-
-                for(dim_t ox = 0; ox < odm[0]; ox++) {
-                    const dim_t oMem = oYZW + ox;
-                    const dim_t iMem = iYZW + ox;
-
-                    bool cond = is_upper ? (oy >= ox) : (oy <= ox);
-                    bool do_unit_diag = (is_unit_diag && ox == oy);
-                    if(cond) {
-                        o[oMem] = do_unit_diag ? scalar<T>(1) : i[iMem];
-                    } else {
-                        o[oMem] = scalar<T>(0);
-                    }
-                }
-            }
-        }
-    }
-}
 
 template<typename T>
 void qr(Array<T> &q, Array<T> &r, Array<T> &t, const Array<T> &in)
