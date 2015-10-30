f3dbb85478ccd322b3723ced79b46cc66701301c
Shehzan Mohammed <shehzan@arrayfire.com>
2015-10-30 13:47:43 -0400

    Add load_image_t and save_image_t to unified

--- src/api/unified/image.cpp
+++ src/api/unified/image.cpp
@@ -41,6 +41,16 @@ af_err af_delete_image_memory(void* ptr)
     return CALL(ptr);
 }
 
+af_err af_load_image_t(af_array *out, const char* filename)
+{
+    return CALL(out, filename);
+}
+
+af_err af_save_image_t(const char* filename, const af_array in)
+{
+    return CALL(filename, in);
+}
+
 af_err af_resize(af_array *out, const af_array in, const dim_t odim0, const dim_t odim1, const af_interp_type method)
 {
     return CALL(out, in, odim0, odim1, method);
