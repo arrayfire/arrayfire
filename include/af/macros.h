8c945f38832ed3227faaf72d41f215b736c0f824
Shehzan Mohammed <shehzan@arrayfire.com>
2015-12-16 15:08:29 -0500

    Added MEMINFO macro to print memory stats

--- include/af/macros.h
+++ include/af/macros.h
@@ -21,3 +21,16 @@
                  __FILE__, __LINE__, ##__VA_ARGS__);      \
         } while (0);
 #endif
+
+#define MEMINFO(msg) do {                                                               \
+    size_t abytes = 0, abuffs = 0, lbytes = 0, lbuffs = 0;                              \
+    af_err err = af_device_mem_info(&abytes, &abuffs, &lbytes, &lbuffs);                \
+    if(err == AF_SUCCESS) {                                                             \
+        printf("MemInfo at %s:%d: " msg "\n", __FILE__, __LINE__);                      \
+        printf("Allocated [ Bytes | Buffers ] = [ %ld | %ld ]\n", abytes, abuffs);      \
+        printf("In Use    [ Bytes | Buffers ] = [ %ld | %ld ]\n", lbytes, lbuffs);      \
+    } else {                                                                            \
+        fprintf(stderr, "MemInfo at %s:%d: " msg "\nAF Error %d\n",                     \
+                __FILE__, __LINE__, err);                                               \
+    }                                                                                   \
+} while(0);
