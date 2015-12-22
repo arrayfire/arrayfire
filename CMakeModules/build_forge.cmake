cd26c4e4d83145e2e3c28875ca5c34af9a81338e
Pavan Yalamanchili <pavan@arrayfire.com>
2015-12-22 15:34:35 -0500

    Updating forge tag to reduce the path lengths

--- CMakeModules/build_forge.cmake
+++ CMakeModules/build_forge.cmake
@@ -22,7 +22,7 @@ ENDIF()
 ExternalProject_Add(
     forge-ext
     GIT_REPOSITORY https://github.com/arrayfire/forge.git
-    GIT_TAG af3.2.1
+    GIT_TAG 84f82e8f54746d75ddf66eeef0e4368881da1f6e
     PREFIX "${prefix}"
     INSTALL_DIR "${prefix}"
     UPDATE_COMMAND ""
