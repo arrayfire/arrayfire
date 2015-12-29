ff8326722228fd6663f32e5f8a6b19ce834677fc
Shehzan Mohammed <shehzan@arrayfire.com>
2015-12-29 13:29:30 -0500

    __AF_FILENAME__ returns path from src

--- src/backend/defines.hpp
+++ src/backend/defines.hpp
@@ -9,19 +9,31 @@
 
 #pragma once
 
-#include <string.h>
+#include <string>
+
+inline std::string
+clipFilePath(std::string path, std::string str)
+{
+    std::string::size_type pos = path.rfind(str);
+    if(pos == std::string::npos) {
+        return path;
+    } else {
+        return path.substr(pos);
+    }
+}
+
 #if defined(_WIN32) || defined(_MSC_VER)
     #define __PRETTY_FUNCTION__ __FUNCSIG__
     #if _MSC_VER < 1900
         #define snprintf sprintf_s
     #endif
     #define STATIC_ static
-    #define __AF_FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+    #define __AF_FILENAME__ (clipFilePath(__FILE__, "src\\").c_str())
 #else
     //#ifndef __PRETTY_FUNCTION__
     //    #define __PRETTY_FUNCTION__ __func__ // __PRETTY_FUNCTION__ Fallback
     //#endif
     #define STATIC_ inline
-    #define __AF_FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+    #define __AF_FILENAME__ (clipFilePath(__FILE__, "src/").c_str())
 #endif
 
