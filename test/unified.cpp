05471d19543af8f9f4500be5e56c3cb18c341901
Shehzan Mohammed <shehzan@arrayfire.com>
2015-12-29 14:34:43 -0500

    TEST add a test for unified api

---
+++ test/unified.cpp
@@ -0,0 +1,68 @@
+/*******************************************************
+ * Copyright (c) 2014, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#include <gtest/gtest.h>
+#include <arrayfire.h>
+#include <af/data.h>
+#include <af/dim4.hpp>
+#include <af/traits.hpp>
+#include <string>
+#include <vector>
+#include <testHelpers.hpp>
+
+#include <af/device.h>
+
+using std::string;
+using std::vector;
+
+template<typename T>
+void testFunction()
+{
+    af_info();
+
+    af_array outArray = 0;
+    dim_t dims[] = {32, 32};
+    ASSERT_EQ(AF_SUCCESS, af_randu(&outArray, 2, dims, (af_dtype) af::dtype_traits<T>::af_type));
+    // cleanup
+    if(outArray != 0) ASSERT_EQ(AF_SUCCESS, af_release_array(outArray));
+}
+
+void unifiedTest()
+{
+    int backends = af::getAvailableBackends();
+
+    bool cpu    = backends & AF_BACKEND_CPU;
+    bool cuda   = backends & AF_BACKEND_CUDA;
+    bool opencl = backends & AF_BACKEND_OPENCL;
+
+    if(cpu) {
+        printf("Running CPU Backend...\n");
+        af::setBackend(AF_BACKEND_CPU);
+        testFunction<float>();
+    }
+
+    if(cuda) {
+        printf("Running CUDA Backend...\n");
+        af::setBackend(AF_BACKEND_CUDA);
+        testFunction<float>();
+    }
+
+    if(opencl) {
+        printf("Running OpenCL Backend...\n");
+        af::setBackend(AF_BACKEND_OPENCL);
+        testFunction<float>();
+    }
+
+    af::setBackend(AF_BACKEND_DEFAULT);
+}
+
+TEST(UNIFIED_TEST, Basic)
+{
+    unifiedTest();
+}
