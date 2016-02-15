199ea82b15314a3be3e28675185395b972a13ff0
Pavan Yalamanchili <pavan@arrayfire.com>
2016-02-14 21:46:48 -0500

    Moving af_get_version to version.cpp

--- src/api/c/version.cpp
+++ src/api/c/version.cpp
@@ -10,6 +10,15 @@
 #include <af/util.h>
 #include <version.hpp>
 
+af_err af_get_version(int *major, int *minor, int *patch)
+{
+    *major = AF_VERSION_MAJOR;
+    *minor = AF_VERSION_MINOR;
+    *patch = AF_VERSION_PATCH;
+
+    return AF_SUCCESS;
+}
+
 const char *af_get_revision()
 {
     return AF_REVISION;
