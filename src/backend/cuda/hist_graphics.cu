e08d31021dcf690e148678bc0f6452a622285859
Shehzan Mohammed <shehzan@arrayfire.com>
2016-05-12 17:48:20 -0400

    FEAT Add fallback to CUDA OpenGL Interop
    This comes into play when devices are not graphics capable but OpenGL is
    available.
    
    On Windows, when all devices are in TCC mode or not connected to display, CUDA
    throws an error
    `CUDA Error (63): OS call failed or operation not supported on this OS`.
    
    On Linux, if all devices are in TCC mode, the graphics part will still run as
    long as OpenGL is available.

--- src/backend/cuda/hist_graphics.cu
+++ src/backend/cuda/hist_graphics.cu
@@ -21,23 +21,35 @@ namespace cuda
 template<typename T>
 void copy_histogram(const Array<T> &data, const fg::Histogram* hist)
 {
-    const T *d_P = data.get();
+    if(InteropManager::checkGraphicsInteropCapability()) {
+        const T *d_P = data.get();
 
-    InteropManager& intrpMngr = InteropManager::getInstance();
+        InteropManager& intrpMngr = InteropManager::getInstance();
 
-    cudaGraphicsResource *cudaVBOResource = intrpMngr.getBufferResource(hist);
-    // Map resource. Copy data to VBO. Unmap resource.
-    size_t num_bytes = hist->size();
-    T* d_vbo = NULL;
-    cudaGraphicsMapResources(1, &cudaVBOResource, 0);
-    cudaGraphicsResourceGetMappedPointer((void **)&d_vbo, &num_bytes, cudaVBOResource);
-    cudaMemcpyAsync(d_vbo, d_P, num_bytes, cudaMemcpyDeviceToDevice,
-                    cuda::getStream(cuda::getActiveDeviceId()));
-    cudaGraphicsUnmapResources(1, &cudaVBOResource, 0);
+        cudaGraphicsResource *cudaVBOResource = intrpMngr.getBufferResource(hist);
+        // Map resource. Copy data to VBO. Unmap resource.
+        size_t num_bytes = hist->size();
+        T* d_vbo = NULL;
+        cudaGraphicsMapResources(1, &cudaVBOResource, 0);
+        cudaGraphicsResourceGetMappedPointer((void **)&d_vbo, &num_bytes, cudaVBOResource);
+        cudaMemcpyAsync(d_vbo, d_P, num_bytes, cudaMemcpyDeviceToDevice,
+                        cuda::getStream(cuda::getActiveDeviceId()));
+        cudaGraphicsUnmapResources(1, &cudaVBOResource, 0);
 
-    CheckGL("After cuda resource copy");
+        CheckGL("After cuda resource copy");
 
-    POST_LAUNCH_CHECK();
+        POST_LAUNCH_CHECK();
+    } else {
+        CheckGL("Begin CUDA fallback-resource copy");
+        glBindBuffer(GL_ARRAY_BUFFER, hist->vbo());
+        GLubyte* ptr = (GLubyte*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+        if (ptr) {
+            CUDA_CHECK(cudaMemcpy(ptr, data.get(), hist->size(), cudaMemcpyDeviceToHost));
+            glUnmapBuffer(GL_ARRAY_BUFFER);
+        }
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        CheckGL("End CUDA fallback-resource copy");
+    }
 }
 
 #define INSTANTIATE(T)  \
