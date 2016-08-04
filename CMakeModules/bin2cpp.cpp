5b5582e81a8c9a4dab465414c0d18dc5c4bba4fe
Umar Arshad <umar@arrayfire.com>
2016-08-04 19:50:46 -0400

    Updating bin2cpp with latest updates

--- CMakeModules/bin2cpp.cpp
+++ CMakeModules/bin2cpp.cpp
@@ -13,7 +13,6 @@
 using namespace std;
 typedef map<string, string> opt_t;
 
-static
 void print_usage() {
     cout << R"delimiter(BIN2CPP
 Converts files from a binary file to C++ headers. It is similar to bin2c and
@@ -34,23 +33,21 @@ Command:
 ./bin2cpp --file blah.txt --namespace blah detail --formatted --name blah_var
 
 Will produce:
-#pragma once
 #include <cstddef>
 namespace blah {
-    namespace detail {
-        static const char blah_var[] = {
-            0x2f,    0x2f,    0x20,    0x62,    0x6c,    0x61,    0x68,    0x2e,    0x74,    0x78,
-            0x74,    0xa,    0x62,    0x6c,    0x61,    0x68,    0x20,    0x62,    0x6c,    0x61,
-            0x68,    0x20,    0x62,    0x6c,    0x61,    0x68,    0xa,    };
-        static const size_t blah_var_len = 27;
-    }
+	namespace detail {
+		static const char blah_var[] = {
+			0x2f,	0x2f,	0x20,	0x62,	0x6c,	0x61,	0x68,	0x2e,	0x74,	0x78,
+			0x74,	0xa,	0x62,	0x6c,	0x61,	0x68,	0x20,	0x62,	0x6c,	0x61,
+			0x68,	0x20,	0x62,	0x6c,	0x61,	0x68,	0xa,	};
+		static const size_t blah_var_len = 27;
+	}
 })delimiter";
         exit(0);
 }
 
 static bool formatted;
 
-static
 void add_tabs(const int level ){
     if(formatted) {
         for(int i =0; i < level; i++) {
@@ -59,7 +56,6 @@ void add_tabs(const int level ){
     }
 }
 
-static
 opt_t
 parse_options(const vector<string>& args) {
     opt_t options;
@@ -69,7 +65,7 @@ parse_options(const vector<string>& args) {
     options["--file"]       = "";
     options["--output"]     = "";
     options["--namespace"]  = "";
-    options["--eof"]        = "";
+    options["--eof"]        = "0";
 
     //Parse Arguments
     string curr_opt;
@@ -113,7 +109,6 @@ parse_options(const vector<string>& args) {
 
 int main(int argc, const char * const * const argv)
 {
-
     vector<string> args(argv, argv+argc);
 
     opt_t&& options = parse_options(args);
@@ -158,7 +153,7 @@ int main(int argc, const char * const * const argv)
     size_t char_cnt = 0;
     add_tabs(++level);
     for(char i; input.get(i);) {
-        cout << "0x" << std::hex << static_cast<int>(i) << ",\t";
+        cout << "0x" << std::hex << static_cast<int>(i & 0xff) << ",\t";
         char_cnt++;
         if(!(char_cnt % 10)) {
             cout << endl;
