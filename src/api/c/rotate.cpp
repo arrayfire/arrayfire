a4c2d0526a7c7e38ccf4909aeab5c069a4e8c4af
Pavan Yalamanchili <pavan@arrayfire.com>
2016-09-05 19:05:03 -0400

    FEAT: Enabling additional interpolation types
    - Enabling cubic support for rotate and transform
    - resize fallsback to use scale
    
    Resize is not using common interp.cl because of compilation
    issues that arose from using too much constant memory when
    compiling FAST in CUDA backend.

--- src/api/c/rotate.cpp
+++ src/api/c/rotate.cpp
@@ -47,6 +47,9 @@ af_err af_rotate(af_array *out, const af_array in, const float theta,
 
         ARG_ASSERT(3, method == AF_INTERP_NEAREST  ||
                       method == AF_INTERP_BILINEAR ||
+                      method == AF_INTERP_BILINEAR_COSINE ||
+                      method == AF_INTERP_BICUBIC ||
+                      method == AF_INTERP_BICUBIC_SPLINE ||
                       method == AF_INTERP_LOWER);
 
         if(idims.elements() == 0) {
