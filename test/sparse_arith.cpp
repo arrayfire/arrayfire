a4a3871129f0b477b7d8bdd1d28542561c9c1837
Shehzan Mohammed <shehzan@arrayfire.com>
2016-12-30 12:23:06 -0500

    Update sparse arith tests for sparse arrays returned from mul and div

--- test/sparse_arith.cpp
+++ test/sparse_arith.cpp
@@ -112,6 +112,41 @@ struct arith_op<af_div_t>
     }
 };
 
+template<typename T>
+void sparseCompare(af::array A, af::array B, const double eps)
+{
+// This macro is used to check if either value is finite and then call assert
+// If neither value is finite, then they can be assumed to be equal to either inf or nan
+#define ASSERT_FINITE_EQ(V1, V2)                                                            \
+    if(std::isfinite(V1) || std::isfinite(V2)) ASSERT_NEAR(V1, V2, eps) << "at : " << i;    \
+
+    af::array AValues = sparseGetValues(A);
+    af::array ARowIdx = sparseGetRowIdx(A);
+    af::array AColIdx = sparseGetColIdx(A);
+
+    af::array BValues = sparseGetValues(B);
+    af::array BRowIdx = sparseGetRowIdx(B);
+    af::array BColIdx = sparseGetColIdx(B);
+
+    // Verify row and col indices
+    ASSERT_EQ(0, af::max<int>(ARowIdx - BRowIdx));
+    ASSERT_EQ(0, af::max<int>(AColIdx - BColIdx));
+
+    T *ptrA = AValues.host<T>();
+    T *ptrB = BValues.host<T>();
+    for(int i = 0; i < AValues.elements(); i++) {
+        ASSERT_FINITE_EQ(real(ptrA[i]), real(ptrB[i]));
+
+        if(A.iscomplex()) {
+            ASSERT_FINITE_EQ(imag(ptrA[i]), imag(ptrB[i]));
+        }
+    }
+    af::freeHost(ptrA);
+    af::freeHost(ptrB);
+
+#undef ASSERT_FINITE_EQ
+}
+
 template<typename T, af_op_t op>
 void sparseArithTester(const int m, const int n, int factor, const double eps)
 {
@@ -154,6 +189,67 @@ void sparseArithTester(const int m, const int n, int factor, const double eps)
     ASSERT_NEAR(0, af::sum<double>(af::abs(imag(revO - revD))) / (m * n), eps);
 }
 
+// Mul
+template<typename T>
+void sparseArithTesterMul(const int m, const int n, int factor, const double eps)
+{
+    af::deviceGC();
+
+    if (noDoubleTests<T>()) return;
+
+#if 1
+    af::array A = cpu_randu<T>(af::dim4(m, n));
+    af::array B = cpu_randu<T>(af::dim4(m, n));
+#else
+    af::array A = af::randu(m, n, (af::dtype)af::dtype_traits<T>::af_type);
+    af::array B = af::randu(m, n, (af::dtype)af::dtype_traits<T>::af_type);
+#endif
+
+    A = makeSparse<T>(A, factor);
+
+    af::array RA = af::sparse(A, AF_STORAGE_CSR);
+    af::array OA = af::sparse(A, AF_STORAGE_COO);
+
+    // Forward
+    {
+        // Arith Op
+        af::array resR = arith_op<af_mul_t>()(RA, B);
+        af::array resO = arith_op<af_mul_t>()(OA, B);
+
+        // We will test this by converting the COO to CSR and CSR to COO and
+        // comparing them. In essense, we are comparing the resR and resO
+        // TODO: Make a better comparison using dense
+
+        // Check resR against conR
+        af::array conR = sparseConvertTo(resR, AF_STORAGE_CSR);
+        sparseCompare<T>(resR, conR, eps);
+
+        // Check resO against conO
+        af::array conO = sparseConvertTo(resR, AF_STORAGE_COO);
+        sparseCompare<T>(resO, conO, eps);
+    }
+
+    // Reverse
+    {
+        // Arith Op
+        af::array resR = arith_op<af_mul_t>()(B, RA);
+        af::array resO = arith_op<af_mul_t>()(B, OA);
+
+        // We will test this by converting the COO to CSR and CSR to COO and
+        // comparing them. In essense, we are comparing the resR and resO
+        // TODO: Make a better comparison using dense
+
+        // Check resR against conR
+        af::array conR = sparseConvertTo(resR, AF_STORAGE_CSR);
+        sparseCompare<T>(resR, conR, eps);
+
+        // Check resO against conO
+        af::array conO = sparseConvertTo(resR, AF_STORAGE_COO);
+        sparseCompare<T>(resO, conO, eps);
+    }
+}
+
+// Div
 template<typename T>
 void sparseArithTesterDiv(const int m, const int n, int factor, const double eps)
 {
@@ -177,7 +273,6 @@ void sparseArithTesterDiv(const int m, const int n, int factor, const double eps
     // Arith Op
     af::array resR = arith_op<af_div_t>()(RA, B);
     af::array resO = arith_op<af_div_t>()(OA, B);
-    af::array resD = arith_op<af_div_t>()( A, B);
 
     // Assert division by sparse is not allowed
     af_array out_temp = 0;
@@ -185,29 +280,17 @@ void sparseArithTesterDiv(const int m, const int n, int factor, const double eps
     ASSERT_EQ(AF_ERR_NOT_SUPPORTED, af_div(&out_temp, B.get(), OA.get(), false));
     if(out_temp != 0) af_release_array(out_temp);
 
-    T *hResR = resR.host<T>();
-    T *hResO = resO.host<T>();
-    T *hResD = resD.host<T>();
+    // We will test this by converting the COO to CSR and CSR to COO and
+    // comparing them. In essense, we are comparing the resR and resO
+    // TODO: Make a better comparison using dense
 
-// This macro is used to check if either value is finite and then call assert
-// If neither value is finite, then they can be assumed to be equal to either inf or nan
-#define ASSERT_FINITE_EQ(V1, V2)                                                            \
-    if(std::isfinite(V1) || std::isfinite(V2)) ASSERT_NEAR(V1, V2, eps) << "at : " << i;    \
+    // Check resR against conR
+    af::array conR = sparseConvertTo(resR, AF_STORAGE_CSR);
+    sparseCompare<T>(resR, conR, eps);
 
-    for(int i = 0; i < B.elements(); i++) {
-        ASSERT_FINITE_EQ(real(hResR[i]), real(hResD[i]));
-        ASSERT_FINITE_EQ(real(hResO[i]), real(hResD[i]));
-
-        if(A.iscomplex()) {
-            ASSERT_FINITE_EQ(imag(hResR[i]), imag(hResD[i]));
-            ASSERT_FINITE_EQ(imag(hResO[i]), imag(hResD[i]));
-        }
-    }
-#undef ASSERT_FINITE_EQ
-
-    af::freeHost(hResR);
-    af::freeHost(hResO);
-    af::freeHost(hResD);
+    // Check resO against conO
+    af::array conO = sparseConvertTo(resR, AF_STORAGE_COO);
+    sparseCompare<T>(resO, conO, eps);
 }
 
 #define ARITH_TESTS_OPS(T, M, N, F, EPS)                                    \
@@ -221,7 +304,7 @@ void sparseArithTesterDiv(const int m, const int n, int factor, const double eps
     }                                                                       \
     TEST(SPARSE_ARITH, T##_MUL_##M##_##N)                                   \
     {                                                                       \
-        sparseArithTester<T, af_mul_t>(M, N, F, EPS);                       \
+        sparseArithTesterMul<T>(M, N, F, EPS);                              \
     }                                                                       \
     TEST(SPARSE_ARITH, T##_DIV_##M##_##N)                                   \
     {                                                                       \
