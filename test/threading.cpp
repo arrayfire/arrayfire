53e1fd8cdb2bfe2ec37503cc42bc85ea30c8c029
pradeep <pradeep@arrayfire.com>
2017-04-23 00:06:47 +0530

    Disable Solve/Sparse multi-threaded tests for OpenCL

--- test/threading.cpp
+++ test/threading.cpp
@@ -616,6 +616,8 @@ TEST(Threading, BLAS)
             tests[testId].join();
 }
 
+#if !defined(AF_OPENCL)
+
 #define SOLVE_LU_TESTS(T, eps)                                                                          \
     tests.emplace_back(solveLUTester<T>, 1000, 100, eps, nextTargetDeviceId()%numDevices);              \
     tests.emplace_back(solveTriangleTester<T>, 1000, 100, true, eps, nextTargetDeviceId()%numDevices);  \
@@ -671,3 +673,5 @@ TEST(Threading, Sparse)
         if (tests[testId].joinable())
             tests[testId].join();
 }
+
+#endif
