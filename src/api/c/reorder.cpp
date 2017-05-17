24c0b31a11ee5a386272b45fd0d891239354e8e5
Pavan Yalamanchili <contact@pavanky.com>
2017-05-16 23:37:42 -0700

    Fix for reorder after JIT

--- src/api/c/reorder.cpp
+++ src/api/c/reorder.cpp
@@ -32,6 +32,9 @@ static inline af_array reorder(const af_array in, const af::dim4 &rdims0)
     const dim4 idims = In.dims();
     const dim4 istrides = In.strides();
 
+    // Ensure all JIT nodes are evaled
+    In.eval();
+
     af_array out;
     if (rdims[0] == 0 &&
         rdims[1] == 1 &&
