8bd49f04dadf1508b58de8fe7921ded01c27d3ff
syurkevi <stefan@arrayfire.com>
2017-07-28 02:07:15 -0400

    fix max grid limitation for colorspace conversions

--- test/ycbcr_rgb.cpp
+++ test/ycbcr_rgb.cpp
@@ -55,6 +55,41 @@ TEST(ycbcr2rgb, CPP)
     }
 }
 
+TEST(ycbcr2rgb, MaxDim)
+{
+    vector<af::dim4>      numDims;
+    vector<vector<float> >      in;
+    vector<vector<float> >   tests;
+
+    readTestsFromFile<float,float>(string(TEST_DIR "/ycbcr_rgb/ycbcr2rgb.test"), numDims, in, tests);
+
+    af::dim4 dims    = numDims[0];
+    af::array input(dims, &(in[0].front()));
+
+    const size_t largeDim = 65535 * 16 + 1;
+    unsigned int ntile = (largeDim + dims[1] - 1)/dims[1];
+    input = af::tile(input, 1, ntile);
+    af::array output = af::ycbcr2rgb(input);
+    af::dim4 outDims = output.dims();
+
+    float *outData = new float[outDims.elements()];
+    output.host((void*)outData);
+
+    vector<float> currGoldBar = tests[0];
+    for(int z=0; z<outDims[2]; ++z) {
+        for(int y=0; y<outDims[1]; ++y) {
+            for(int x=0; x<outDims[0]; ++x) {
+                int outIter  = (z*outDims[1]*outDims[0]) + (y*outDims[0]) + x;
+                int goldIter = (z*dims[1]*dims[0]) + ((y%dims[1])*dims[0]) + x;
+                ASSERT_NEAR(currGoldBar[goldIter], outData[outIter], 1.0e-3)<< "at: " << outIter << std::endl;
+            }
+        }
+    }
+
+    // cleanup
+    delete[] outData;
+}
+
 TEST(rgb2ycbcr, CPP)
 {
     vector<af::dim4>      numDims;
@@ -75,4 +110,39 @@ TEST(rgb2ycbcr, CPP)
     for (size_t elIter=0; elIter<nElems; ++elIter) {
         ASSERT_NEAR(currGoldBar[elIter], outData[elIter], 1.0e-3)<< "at: " << elIter<< std::endl;
     }
+
+}
+
+TEST(rgb2ycbcr, MaxDim)
+{
+    vector<af::dim4>      numDims;
+    vector<vector<float> >      in;
+    vector<vector<float> >   tests;
+
+    readTestsFromFile<float,float>(string(TEST_DIR "/ycbcr_rgb/rgb2ycbcr.test"), numDims, in, tests);
+
+    af::dim4 dims    = numDims[0];
+    af::array input(dims, &(in[0].front()));
+
+    const size_t largeDim = 65535 * 16 + 1;
+    unsigned int ntile = (largeDim + dims[1] - 1)/dims[1];
+    input = af::tile(input, 1, ntile);
+    af::array output = af::rgb2ycbcr(input);
+    af::dim4 outDims = output.dims();
+
+    float *outData = new float[outDims.elements()];
+    output.host((void*)outData);
+
+    vector<float> currGoldBar = tests[0];
+    for(int z=0; z<outDims[2]; ++z) {
+        for(int y=0; y<outDims[1]; ++y) {
+            for(int x=0; x<outDims[0]; ++x) {
+                int outIter  = (z*outDims[1]*outDims[0]) + (y*outDims[0]) + x;
+                int goldIter = (z*dims[1]*dims[0]) + ((y%dims[1])*dims[0]) + x;
+                ASSERT_NEAR(currGoldBar[goldIter], outData[outIter], 1.0e-3)<< "at: " << outIter << std::endl;
+            }
+        }
+    }
+
+    delete[] outData;
 }
