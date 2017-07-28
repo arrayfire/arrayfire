23365fd9e46449e699ae1cc2d0af5c7b7e719d56
syurkevi <stefan@arrayfire.com>
2017-07-28 02:07:15 -0400

    fix max grid limitation for colorspace conversions

--- test/hsv_rgb.cpp
+++ test/hsv_rgb.cpp
@@ -76,3 +76,73 @@ TEST(rgb2hsv, CPP)
         ASSERT_NEAR(currGoldBar[elIter], outData[elIter], 1.0e-3)<< "at: " << elIter<< std::endl;
     }
 }
+
+TEST(rgb2hsv, MaxDim)
+{
+    vector<af::dim4>      numDims;
+    vector<vector<float> >      in;
+    vector<vector<float> >   tests;
+
+    readTestsFromFile<float,float>(string(TEST_DIR"/hsv_rgb/rgb2hsv.test"), numDims, in, tests);
+
+    af::dim4 dims    = numDims[0];
+    af::array input(dims, &(in[0].front()));
+
+    const size_t largeDim = 65535 * 16 + 1;
+    unsigned int ntile = (largeDim + dims[1] - 1)/dims[1];
+    input = af::tile(input, 1, ntile);
+    af::array output = af::rgb2hsv(input);
+    af::dim4 outDims = output.dims();
+
+    float *outData = new float[outDims.elements()];
+    output.host((void*)outData);
+
+    vector<float> currGoldBar = tests[0];
+    for(int z=0; z<outDims[2]; ++z) {
+        for(int y=0; y<outDims[1]; ++y) {
+            for(int x=0; x<outDims[0]; ++x) {
+                int outIter  = (z*outDims[1]*outDims[0]) + (y*outDims[0]) + x;
+                int goldIter = (z*dims[1]*dims[0]) + ((y%dims[1])*dims[0]) + x;
+                ASSERT_NEAR(currGoldBar[goldIter], outData[outIter], 1.0e-3)<< "at: " << outIter << std::endl;
+            }
+        }
+    }
+
+    // cleanup
+    delete[] outData;
+}
+
+TEST(hsv2rgb, MaxDim)
+{
+    vector<af::dim4>      numDims;
+    vector<vector<float> >      in;
+    vector<vector<float> >   tests;
+
+    readTestsFromFile<float,float>(string(TEST_DIR"/hsv_rgb/hsv2rgb.test"), numDims, in, tests);
+
+    af::dim4 dims    = numDims[0];
+    af::array input(dims, &(in[0].front()));
+
+    const size_t largeDim = 65535 * 16 + 1;
+    unsigned int ntile = (largeDim + dims[1] - 1)/dims[1];
+    input = af::tile(input, 1, ntile);
+    af::array output = af::hsv2rgb(input);
+    af::dim4 outDims = output.dims();
+
+    float *outData = new float[outDims.elements()];
+    output.host((void*)outData);
+
+    vector<float> currGoldBar = tests[0];
+    for(int z=0; z<outDims[2]; ++z) {
+        for(int y=0; y<outDims[1]; ++y) {
+            for(int x=0; x<outDims[0]; ++x) {
+                int outIter  = (z*outDims[1]*outDims[0]) + (y*outDims[0]) + x;
+                int goldIter = (z*dims[1]*dims[0]) + ((y%dims[1])*dims[0]) + x;
+                ASSERT_NEAR(currGoldBar[goldIter], outData[outIter], 1.0e-3)<< "at: " << outIter << std::endl;
+            }
+        }
+    }
+
+    // cleanup
+    delete[] outData;
+}
