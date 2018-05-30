56ae40a30d5f47c533c1b0160f0ffd9819d705d4
Umar Arshad <umar@arrayfire.com>
2018-05-30 02:57:54 -0400

    Add missing copyright

--- src/backend/common/MemoryManagerImpl.hpp
+++ src/backend/common/MemoryManagerImpl.hpp
@@ -1,3 +1,11 @@
+/*******************************************************
+ * Copyright (c) 2018, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
 
 #include <common/MemoryManager.hpp>
 #include <common/Logger.hpp>
