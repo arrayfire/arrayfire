a40b1145426d1d473f14a31d84d90e3d0f59a356
Umar Arshad <umar@arrayfire.com>
2018-06-19 01:43:34 -0400

    Move retainHandle to handle.hpp. Clarify documentation
    (cherry picked from commit 2e8b66b92433f468d5250c88d37b53253bbe44fe)

--- src/backend/cpu/Array.hpp
+++ src/backend/cpu/Array.hpp
@@ -58,17 +58,22 @@ namespace cpu
     template<typename T>
     Array<T> createDeviceDataArray(const af::dim4 &size, const void *data);
 
-    // Copies data to an existing Array object from a host pointer
+    /// Copies data to an existing Array object from a host pointer
     template<typename T>
     void writeHostDataArray(Array<T> &arr, const T * const data, const size_t bytes);
 
-    // Copies data to an existing Array object from a device pointer
+    /// Copies data to an existing Array object from a device pointer
     template<typename T>
     void writeDeviceDataArray(Array<T> &arr, const void * const data, const size_t bytes);
 
-    // Create an Array object and do not assign any values to it
+    /// Create an Array object and do not assign any values to it.
+    /// \NOTE: This object should not be used to initalize an array. Use
+    ///       createEmptyArray instead
     template<typename T> Array<T> *initArray();
 
+    /// Creates an empty array of a given size. No data is initialized
+    ///
+    /// \param[in] size The dimension of the output array
     template<typename T>
     Array<T> createEmptyArray(const af::dim4 &size);
 
