c8a5526d5a3b2900215c22a38e6768133bee25ac
Umar Arshad <umar@arrayfire.com>
2018-07-16 03:12:23 -0400

    Add CTestCustom. Print info after tests. Mark sparse test SERIAL
    (cherry picked from commit 6992e9fb16505eab1e2bc70229743a54b80fc5e8)

---
+++ test/print_info.cpp
@@ -0,0 +1,26 @@
+/*******************************************************
+ * Copyright (c) 2018, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#include <arrayfire.h>
+
+using namespace af;
+
+int main(int argc, const char** argv) {
+    int backend = getAvailableBackends();
+    if (backend & AF_BACKEND_OPENCL) {
+        setBackend(AF_BACKEND_OPENCL);
+    } else if (backend & AF_BACKEND_CUDA) {
+        setBackend(AF_BACKEND_CUDA);
+    } else if (backend & AF_BACKEND_CPU) {
+        setBackend(AF_BACKEND_CPU);
+    }
+
+    info();
+    return 0;
+}
