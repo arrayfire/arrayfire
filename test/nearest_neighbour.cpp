0f102efc1dee32890d19af7d7eefce160d4ae938
syurkevi <stefan@arrayfire.com>
2018-08-16 18:04:37 -0400

    test all nearest neighbors parameters

--- test/nearest_neighbour.cpp
+++ test/nearest_neighbour.cpp
@@ -21,8 +21,10 @@ using std::string;
 using af::array;
 using af::cfloat;
 using af::cdouble;
+using af::constant;
 using af::dim4;
 using af::dtype_traits;
+using af::range;
 
 template<typename T>
 class NearestNeighbour : public ::testing::Test
@@ -270,17 +272,17 @@ TEST(KNearestNeighbourSSD, small)
     const int nfeat  = 2;
 
     float query[nquery * nfeat] = {
-        5, 5,
-        0, 0,
+        5,  5,
+        0,  0,
        10, 10,
     };
 
     float train[ntrain * nfeat] = {
-        0, 0,
+        0,   0,
         3.5, 4,
-        5, 5,
-        6, 5,
-        8, 6.5
+        5,   5,
+        6,   5,
+        8,   6.5
     };
 
     array t(nfeat, ntrain, train);
@@ -303,8 +305,152 @@ TEST(KNearestNeighbourSSD, small)
 
     vector<float> actualDistances(nquery);
     distances.host(&actualDistances[0]);
-    for (int i = 0; i < nquery; i++)
-    {
+    for (int i = 0; i < nquery; i++) {
         EXPECT_NEAR(expectedDistances[i], actualDistances[i], 1E-8);
     }
 }
+
+struct nearest_neighbors_params {
+    string testname_;
+    int    k_;
+    int    feat_dim_;
+    array  query_;
+    array  train_;
+    array  indices_;
+    array  dists_;
+
+    nearest_neighbors_params(array indices, array dists, array query, array train, int k, int feat_dim, string testname)
+    : indices_(indices), dists_(dists), query_(query), train_(train), k_(k), feat_dim_(feat_dim), testname_(testname)
+    { }
+};
+
+template<typename TestClass>
+string testNameGenerator(const ::testing::TestParamInfo<typename TestClass::ParamType> info) {
+    return info.param.testname_;
+}
+
+class NearestNeighborsTest  : public ::testing::TestWithParam<nearest_neighbors_params> { };
+class KNearestNeighborsTest : public ::testing::TestWithParam<nearest_neighbors_params> { };
+
+nearest_neighbors_params
+single_knn_data(const string testname, const int nquery, const int ntrain, const int nfeat, const int k, const int feat_dim) {
+    array indices, dists;
+    array query, train;
+    if(feat_dim == 0) {
+        query = constant(0, nfeat, nquery);
+        train = constant(1, nfeat, ntrain);
+    } else {
+        query = constant(0, nquery, nfeat);
+        train = constant(1, ntrain, nfeat);
+    }
+
+    indices = constant(0, k, nquery, u32);
+    dists   = constant(nfeat, k, nquery);
+    return nearest_neighbors_params(indices, dists, query, train, k, feat_dim, testname);
+}
+
+nearest_neighbors_params
+knn_data(const string testname, const int nquery, const int ntrain, const int nfeat, const int k, const int feat_dim) {
+    array indices, dists;
+    array query, train;
+    if(feat_dim == 0) {
+        query = constant(0, nfeat, nquery);
+        train = range(dim4(nfeat, ntrain), 1);
+    } else {
+        query = constant(0, nquery, nfeat);
+        train = range(dim4(ntrain, nfeat), 0);
+    }
+
+    indices = range(dim4(k, nquery), 0, u32);
+    dists   = range(dim4(k, nquery));
+    dists  *= dists;
+
+    return nearest_neighbors_params(indices, dists, query, train, k, feat_dim, testname);
+}
+
+vector<nearest_neighbors_params> genNNTests() {
+    return {single_knn_data("1q1t",      1,     1, 10, 1, 0),
+            single_knn_data("1q10t",     1,    10, 10, 1, 0),
+            single_knn_data("1q100t",    1,   100, 10, 1, 0),
+            single_knn_data("1q1000t",   1,  1000, 10, 1, 0),
+            single_knn_data("1q100000t", 1, 10000, 10, 1, 0),
+            single_knn_data("10q1t",        10, 1, 10, 1, 0),
+            single_knn_data("100q1t",      100, 1, 10, 1, 0),
+            single_knn_data("1000q1t",    1000, 1, 10, 1, 0),
+            single_knn_data("10000q1t",  10000, 1, 10, 1, 0),
+            single_knn_data("100000q1t", 10000, 1, 10, 1, 0),
+            single_knn_data("1q1tfl1",      10, 1,  1, 1, 0),
+            single_knn_data("1q1tfl2",      10, 1,  2, 1, 0),
+            single_knn_data("1q1tfl4",      10, 1,  4, 1, 0),
+            single_knn_data("1q1tfl8",      10, 1,  8, 1, 0),
+            single_knn_data("1q1tfl16",     10, 1, 16, 1, 0),
+            single_knn_data("1q1tfl32",     10, 1, 32, 1, 0),
+            single_knn_data("1q1tfl64",     10, 1, 64, 1, 0),
+            single_knn_data("1q1tfl128",    10, 1,128, 1, 0),
+            single_knn_data("1q1tfl256",    10, 1,256, 1, 0),
+            single_knn_data("1q1tfl10000",  10, 1,10000, 1, 0),
+            single_knn_data("10q1t1d",        10, 1, 10, 1, 1),
+            single_knn_data("100q1t1d",      100, 1, 10, 1, 1),
+            single_knn_data("1000q1t1d",    1000, 1, 10, 1, 1),
+            single_knn_data("10000q1t1d",  10000, 1, 10, 1, 1),
+            single_knn_data("100000q1t1d", 10000, 1, 10, 1, 1),
+           };
+}
+
+vector<nearest_neighbors_params> genKNNTests() {
+    return { knn_data("1q1000t1k",   1,  1000, 1,   1, 0),
+             knn_data("1q1000t2k",   1,  1000, 1,   2, 0),
+             knn_data("1q1000t4k",   1,  1000, 1,   4, 0),
+             knn_data("1q1000t8k",   1,  1000, 1,   8, 0),
+             knn_data("1q1000t16k",  1,  1000, 1,  16, 0),
+             knn_data("1q1000t32k",  1,  1000, 1,  32, 0),
+             knn_data("1q1000t64k",  1,  1000, 1,  64, 0),
+             knn_data("1q1000t128k", 1,  1000, 1, 128, 0),
+             knn_data("1q1000t256k", 1,  1000, 1, 256, 0)
+            };
+}
+
+INSTANTIATE_TEST_CASE_P(KNearestNeighborsSSD,
+                        NearestNeighborsTest,
+                        ::testing::ValuesIn(genNNTests()),
+                        testNameGenerator<NearestNeighborsTest>
+                        );
+
+INSTANTIATE_TEST_CASE_P(KNearestNeighborsSSD,
+                        KNearestNeighborsTest,
+                        ::testing::ValuesIn(genKNNTests()),
+                        testNameGenerator<KNearestNeighborsTest>
+                        );
+
+TEST_P(NearestNeighborsTest, SingleQTests) {
+    nearest_neighbors_params params = GetParam();
+
+    array query = params.query_;
+    array train = params.train_;
+
+    array indices;
+    array distances;
+    const int k = params.k_;
+    const int feat_dim = params.feat_dim_;
+    nearestNeighbour(indices, distances, query, train, feat_dim, k, AF_SSD);
+
+    ASSERT_ARRAYS_EQ(indices, params.indices_);
+    ASSERT_ARRAYS_NEAR(distances, params.dists_, 1e-5);
+}
+
+TEST_P(KNearestNeighborsTest, SingleQTests) {
+    nearest_neighbors_params params = GetParam();
+
+    array query = params.query_;
+    array train = params.train_;
+
+    array indices;
+    array distances;
+    const int k = params.k_;
+    const int feat_dim = params.feat_dim_;
+    nearestNeighbour(indices, distances, query, train, feat_dim, k, AF_SSD);
+
+    ASSERT_ARRAYS_EQ(indices, params.indices_);
+    ASSERT_ARRAYS_NEAR(distances, params.dists_, 1e-5);
+}
+
