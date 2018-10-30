7f770cf587f4188caf676a515cf34e0634af7bce
Umar Arshad <umar@arrayfire.com>
2018-10-29 20:44:45 -0400

    Mark more tests as serial tests. Update CDash drop site
    (cherry picked from commit 6fb213988c5f210e140626b442425d7c49149fe1)

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "ArrayFire")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "67.207.87.39")
+set(CTEST_DROP_SITE "ci.arrayfire.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=ArrayFire")
 set(CTEST_DROP_SITE_CDASH TRUE)
