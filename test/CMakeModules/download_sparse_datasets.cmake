c7f4852d0d0d1024a8584a1e46979ddff0dbe2b1
pradeep <pradeep@arrayfire.com>
2019-02-15 11:05:55 +0530

    Correctly turn off mtx tests in cmake

--- test/CMakeModules/download_sparse_datasets.cmake
+++ test/CMakeModules/download_sparse_datasets.cmake
@@ -8,45 +8,48 @@
 set(URL "https://sparse.tamu.edu")
 
 function(download_mtx name group)
-  set(file_name "${group}/${name}.tar.gz")
-  if (NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/matrixmarket/${file_name}")
-    file(DOWNLOAD
-        "${URL}/MM/${file_name}"
-        ${CMAKE_CURRENT_BINARY_DIR}/matrixmarket/${file_name}
-        INACTIVITY_TIMEOUT 600
-        SHOW_PROGRESS
-        STATUS out_status
-        TLS_VERIFY ON
-        )
-    list(GET out_status 0 error_code)
-    list(GET out_status 1 error_string)
-    if (${error_code} EQUAL 0)
-      message("Downloaded ${name} file from sparse.tamu.edu")
-      file(MAKE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/data/matrixmarket/${group}")
-      execute_process(
-        COMMAND ${CMAKE_COMMAND} -E tar xzf "${CMAKE_CURRENT_BINARY_DIR}/matrixmarket/${file_name}"
-        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/data/matrixmarket/${group}"
-        )
-      message("Extracted mtx files to test data directory")
-    else ()
-      if (${error_code} EQUAL 503)
-          message(FATAL_ERROR "${URL} service unavailable")
-      elseif (${error_code} EQUAL 504)
-          message(FATAL_ERROR "Request to ${URL} timedout")
-      elseif (${error_code} EQUAL 521)
-        # CLOUDFLARE error code
-        message(FATAL_ERROR "Request to ${URL} has been refused")
-      elseif (${error_code} EQUAL 523)
-        # CLOUDFLARE error code
-        message(FATAL_ERROR "${URL} is unreachable")
+  if(AF_TEST_WITH_MTX_FILES)
+    set(file_name "${group}/${name}.tar.gz")
+    if (NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/matrixmarket/${file_name}")
+      file(DOWNLOAD
+          "${URL}/MM/${file_name}"
+          ${CMAKE_CURRENT_BINARY_DIR}/matrixmarket/${file_name}
+          INACTIVITY_TIMEOUT 600
+          SHOW_PROGRESS
+          STATUS out_status
+          TLS_VERIFY ON
+          )
+      list(GET out_status 0 error_code)
+      list(GET out_status 1 error_string)
+      if (${error_code} EQUAL 0)
+        message("Downloaded ${name} file from sparse.tamu.edu")
+        file(MAKE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/data/matrixmarket/${group}")
+        execute_process(
+          COMMAND ${CMAKE_COMMAND} -E tar xzf "${CMAKE_CURRENT_BINARY_DIR}/matrixmarket/${file_name}"
+          WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/data/matrixmarket/${group}"
+          )
+        message("Extracted mtx files to test data directory")
       else ()
-        message("Failed to download ${name} file from sparse.tamu.edu")
-        message("Failure message: ${error_string}")
-      endif ()
-      file(REMOVE "${CMAKE_CURRENT_BINARY_DIR}/matrixmarket/${file_name}")
+        if (${error_code} EQUAL 503)
+            message(FATAL_ERROR "${URL} service unavailable")
+        elseif (${error_code} EQUAL 504)
+            message(FATAL_ERROR "Request to ${URL} timedout")
+        elseif (${error_code} EQUAL 521)
+          # CLOUDFLARE error code
+          message(FATAL_ERROR "Request to ${URL} has been refused")
+        elseif (${error_code} EQUAL 523)
+          # CLOUDFLARE error code
+          message(FATAL_ERROR "${URL} is unreachable")
+        else ()
+          message("Failed to download ${name} file from sparse.tamu.edu")
+          message("Failure message: ${error_string}")
+        endif ()
+        file(REMOVE "${CMAKE_CURRENT_BINARY_DIR}/matrixmarket/${file_name}")
 
-      #Force test with mtx files to be turned since one of the downloads failed
-      set(AF_TEST_WITH_MTX_FILES OFF)
+        set(AF_TEST_WITH_MTX_FILES OFF CACHE BOOL
+            "Download and run tests on large matrices form sparse.tamu.edu"
+            FORCE)
+      endif ()
     endif ()
   endif ()
 endfunction()
