05953277cb9f9e41b677511c370142c50efa7623
pradeep <pradeep@arrayfire.com>
2019-02-26 09:37:17 +0530

    Avoid using make_shared for VS(_MSC_VER > 1914) in CUDA backend
    std::make_shared creates a temporary object that utilizes alignment greater
    than alignof(std::max_align_t) for the type cdouble. When extended
    alignment is used without enabling it explicitly, type_traits header
    from VC++ throws a static assertion preventing successful build. Note
    that this happens on VS updates beyond 15.7 i.e. _MSC_VER > 1914.

--- src/backend/cuda/scalar.hpp
+++ src/backend/cuda/scalar.hpp
@@ -17,8 +17,19 @@ namespace cuda {
 
 template<typename T>
 Array<T> createScalarNode(const dim4 &size, const T val) {
+#if _MSC_VER > 1914
+    // FIXME(pradeep) - Needed only in CUDA backend, didn't notice any
+    // issues in other backends.
+    // Either this gaurd or we need to enable extended alignment
+    // by defining _ENABLE_EXTENDED_ALIGNED_STORAGE before <type_traits>
+    // header is included
+    using ScalarNode = common::ScalarNode<T>;
+    using ScalarNodePtr = std::shared_ptr<ScalarNode>;
+    return createNodeArray<T>(size, ScalarNodePtr(new ScalarNode(val)));
+#else
     return createNodeArray<T>(size,
                               std::make_shared<common::ScalarNode<T>>(val));
+#endif
 }
 
 }  // namespace cuda
