2804a72f071d677f36d47f548fe72fe814c315c3
Umar Arshad <umar@arrayfire.com>
2019-04-26 17:59:28 -0400

    Remove default implementation of unaryName struct

--- src/backend/cpu/unary.hpp
+++ src/backend/cpu/unary.hpp
@@ -7,6 +7,7 @@
  * http://arrayfire.com/licenses/BSD-3-Clause
  ********************************************************/
 
+#pragma once
 #include <Array.hpp>
 #include <err_cpu.hpp>
 #include <jit/UnaryNode.hpp>
