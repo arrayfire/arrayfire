f22a937ae56aea8f9bff7f4a2ebbcc42e5fef79a
Umar Arshad <umar@arrayfire.com>
2019-06-03 18:41:47 -0400

    Update coverage exclude files to remove glad

--- CMakeModules/CTestCustom.cmake
+++ CMakeModules/CTestCustom.cmake
@@ -1,17 +1,18 @@
 
 
-set(CTEST_CUSTOM_ERROR_POST_CONTEXT 20)
-set(CTEST_CUSTOM_ERROR_PRE_CONTEXT 20)
+set(CTEST_CUSTOM_ERROR_POST_CONTEXT 30)
+set(CTEST_CUSTOM_ERROR_PRE_CONTEXT 30)
 set(CTEST_CUSTOM_POST_TEST ./test/print_info)
 
 list(APPEND CTEST_CUSTOM_COVERAGE_EXCLUDE
-	"test/gtest/*"
+  "test/gtest/*"
 
   # All external and third_party libraries
-	"extern/spdlog/*"
-	"src/backend/cpu/threads/*"
-	"src/backend/cuda/cub/*"
-	"cl2.hpp"
+  "extern/*"
+  "test/mmio/*"
+  "src/backend/cpu/threads/*"
+  "src/backend/cuda/cub/*"
+  "cl2.hpp"
 
   # Remove bin2cpp from coverage
-	"CMakeModules/*")
+  "CMakeModules/*")
