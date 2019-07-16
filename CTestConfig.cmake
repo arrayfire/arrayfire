e73488d0d597c06173478f78e1c64ff0e0a47dfc
Umar Arshad <umar@arrayfire.com>
2019-07-15 20:32:53 -0400

    Update CDash drop site to use https

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,7 +7,7 @@
 set(CTEST_PROJECT_NAME "ArrayFire")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "ci.arrayfire.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=ArrayFire")
 set(CTEST_DROP_SITE_CDASH TRUE)
