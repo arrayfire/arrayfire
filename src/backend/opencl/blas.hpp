2aa9349487a2f914f84c768dc02667b95d6465f6
Umar Arshad <umar@arrayfire.com>
2019-07-22 11:44:51 -0400

    Add matmul helper function in internal API

--- src/backend/opencl/blas.hpp
+++ src/backend/opencl/blas.hpp
@@ -20,9 +20,21 @@ void initBlas();
 void deInitBlas();
 
 template<typename T>
-void gemm(Array<T> &out, af_mat_prop optLhs, af_mat_prop optRhs,
-          const T *alpha, const Array<T> &lhs, const Array<T> &rhs,
-          const T *beta);
+void gemm(Array<T> &out, af_mat_prop optLhs, af_mat_prop optRhs, const T *alpha,
+          const Array<T> &lhs, const Array<T> &rhs, const T *beta);
+
+template<typename T>
+Array<T> matmul(const Array<T> &lhs, const Array<T> &rhs, af_mat_prop optLhs,
+                af_mat_prop optRhs) {
+    int Mdim     = optLhs == AF_MAT_NONE ? 0 : 1;
+    int Ndim     = optRhs == AF_MAT_NONE ? 1 : 0;
+    Array<T> res = createEmptyArray<T>(
+        dim4(lhs.dims()[Mdim], rhs.dims()[Ndim], lhs.dims()[2], lhs.dims()[3]));
+    constexpr T alpha = 1.0;
+    constexpr T beta  = 0.0;
+    gemm(res, optLhs, optRhs, &alpha, lhs, rhs, &beta);
+    return res;
+}
 
 template<typename T>
 Array<T> dot(const Array<T> &lhs, const Array<T> &rhs, af_mat_prop optLhs,
