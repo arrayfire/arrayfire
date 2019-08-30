2709c377a1870c8a4d9aa15a01cbcd923b7801d1
Mark Poscablo <mark@arrayfire.com>
2019-08-29 23:39:41 -0400

    Clarify and correct nearest neighbour documentation

--- test/nearest_neighbour.cpp
+++ test/nearest_neighbour.cpp
@@ -503,3 +503,80 @@ TEST(KNearestNeighbours, InvalidLargeK) {
     ASSERT_THROW(nearestNeighbour(indices, distances, q, t, 0, k, AF_SSD),
                  af::exception);
 }
+
+TEST(NearestNeighbour, DocSnippet1) {
+    //! [ex_nearest_1]
+    float h_pts[6] = {1.f, 2.f, 3.f, 8.f, 9.f, 10.f};
+    array pts(dim4(1, 6), h_pts);
+    //  1.   2.   3.   8.   9.   10.
+
+    float h_query = 1.25f;
+    array query(dim4(1), &h_query);
+    //  1.5
+
+    array idx;
+    array dist;
+    nearestNeighbour(idx, dist, query, pts, 0, 3);
+    // idx
+    //  0.
+    //  1.
+    //  2.
+    //
+    // dist
+    //  0.0625
+    //  0.5625
+    //  3.0625
+
+    //! [ex_nearest_1]
+
+    unsigned int h_gold_idx[3] = {0, 1, 2};
+    float h_gold_dist[3] = {0.0625f, 0.5625f, 3.0625f};
+    array gold_idx(dim4(3), h_gold_idx);
+    array gold_dist(dim4(3), h_gold_dist);
+    ASSERT_ARRAYS_EQ(gold_idx, idx);
+    ASSERT_ARRAYS_EQ(gold_dist, dist);
+}
+
+TEST(NearestNeighbour, DocSnippet2) {
+    //! [ex_nearest_2]
+    float h_pts[18] = {0.f, 0.f, 0.f,
+                       1.f, 0.f, 0.f,
+                       0.f, 1.f, 0.f,
+                       8.f, 9.f, 1.f,
+                       9.f, 8.f, 1.f,
+                       9.f, 9.f, 1.f};
+    array pts(dim4(3, 6), h_pts);
+    //  0.    1.    0.    8.    9.    9.
+    //  0.    0.    1.    9.    8.    9.
+    //  0.    0.    0.    1.    1.    1.
+
+    float h_query[6] = {1.5f, 0.f, 0.f,
+                        7.5f, 9.f, 1.f};
+    array query(dim4(3, 2), h_query);
+    //  1.5   7.5
+    //  0.    9.
+    //  0.    1.
+
+    array idx;
+    array dist;
+    nearestNeighbour(idx, dist, query, pts, 0, 3);
+    // idx
+    //  1     3
+    //  0     5
+    //  2     4
+    //
+    // dist
+    //  0.25  0.25
+    //  2.25  2.25
+    //  3.25  3.25
+    //! [ex_nearest_2]
+
+    unsigned int h_gold_idx[6] = {1, 0, 2,
+                                  3, 5, 4};
+    float h_gold_dist[6] = {0.25f, 2.25f, 3.25f,
+                            0.25f, 2.25f, 3.25f};
+    array gold_idx(dim4(3, 2), h_gold_idx);
+    array gold_dist(dim4(3, 2), h_gold_dist);
+    ASSERT_ARRAYS_EQ(gold_idx, idx);
+    ASSERT_ARRAYS_EQ(gold_dist, dist);
+}
