9f7b24dd586bd8c0ee7048f137069bc3dd59e516
pradeep <pradeep@arrayfire.com>
2020-01-10 20:10:19 +0530

    Anisotropic smoothing perf improvements in OpenCL

--- src/backend/opencl/kernel/anisotropic_diffusion.hpp
+++ src/backend/opencl/kernel/anisotropic_diffusion.hpp
@@ -20,8 +20,9 @@
 
 namespace opencl {
 namespace kernel {
-static const int THREADS_X = 16;
-static const int THREADS_Y = 16;
+constexpr int THREADS_X = 32;
+constexpr int THREADS_Y = 8;
+constexpr int YDIM_LOAD = 2 * THREADS_X / THREADS_Y;
 
 template<typename T, bool isMCDE>
 void anisotropicDiffusion(Param inout, const float dt, const float mct,
@@ -35,7 +36,8 @@ void anisotropicDiffusion(Param inout, const float dt, const float mct,
 
     std::string kerKeyStr = std::string("anisotropic_diffusion_") +
                             std::string(dtype_traits<T>::getName()) + "_" +
-                            std::to_string(isMCDE);
+                            std::to_string(isMCDE) + "_" +
+                            std::to_string(fluxFnCode);
 
     int device       = getActiveDeviceId();
     kc_entry_t entry = kernelCache(device, kerKeyStr);
@@ -43,9 +45,10 @@ void anisotropicDiffusion(Param inout, const float dt, const float mct,
     if (entry.prog == 0 && entry.ker == 0) {
         std::ostringstream options;
         options << " -D T=" << dtype_traits<T>::getName()
-                << " -D SHRD_MEM_HEIGHT=" << (THREADS_X + 2)
-                << " -D SHRD_MEM_WIDTH=" << (THREADS_Y + 2)
-                << " -D IS_MCDE=" << isMCDE;
+                << " -D SHRD_MEM_HEIGHT=" << (THREADS_Y * YDIM_LOAD + 2)
+                << " -D SHRD_MEM_WIDTH=" << (THREADS_X + 2)
+                << " -D IS_MCDE=" << isMCDE << " -D FLUX_FN=" << fluxFnCode
+                << " -D YDIM_LOAD=" << YDIM_LOAD;
         if (std::is_same<T, double>::value) options << " -D USE_DOUBLE";
 
         const char *ker_strs[] = {anisotropic_diffusion_cl};
@@ -58,20 +61,19 @@ void anisotropicDiffusion(Param inout, const float dt, const float mct,
     }
 
     auto diffUpdateOp =
-        KernelFunctor<Buffer, KParam, float, float, int, unsigned, unsigned>(
+        KernelFunctor<Buffer, KParam, float, float, unsigned, unsigned>(
             *entry.ker);
 
     NDRange threads(THREADS_X, THREADS_Y, 1);
 
     int blkX = divup(inout.info.dims[0], threads[0]);
-    int blkY = divup(inout.info.dims[1], threads[1]);
+    int blkY = divup(inout.info.dims[1], threads[1] * YDIM_LOAD);
 
     NDRange global(threads[0] * blkX * inout.info.dims[2],
                    threads[1] * blkY * inout.info.dims[3], 1);
 
     diffUpdateOp(EnqueueArgs(getQueue(), global, threads), *inout.data,
-                 inout.info, dt, mct, fluxFnCode, blkX, blkY);
-
+                 inout.info, dt, mct, blkX, blkY);
     CL_DEBUG_FINISH(getQueue());
 }
 }  // namespace kernel
