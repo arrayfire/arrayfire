e63f32606a75e772bd74978000b4347323b2f60e
Umar Arshad <umar@arrayfire.com>
2020-03-25 01:04:00 -0400

    Create a thrust policy to intercept tmp buffer allocations (#2806)
    * Create a thrust policy to intercept tmp buffer allocations
    
    Thrust uses policies to perform certain operations in the backend. This
    commit creates an ArrayFire policy for thrust which intercepts temporary
    buffer allocations and frees to the memory manager. It also allows
    you to specify the stream of the operation so the older approach to
    specify the stream has been updated.

---
+++ src/backend/cuda/ThrustArrayFirePolicy.cpp
@@ -0,0 +1,20 @@
+/*******************************************************
+ * Copyright (c) 2020, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#include <ThrustArrayFirePolicy.hpp>
+
+namespace cuda {
+
+cudaStream_t get_stream(ThrustArrayFirePolicy) { return getActiveStream(); }
+
+cudaError_t synchronize_stream(ThrustArrayFirePolicy) {
+    return cudaStreamSynchronize(getActiveStream());
+}
+
+}  // namespace cuda
