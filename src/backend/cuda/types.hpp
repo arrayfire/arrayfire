08296d6f06d7eef2ef692542a0e9f284d902a055
pradeep <pradeep@arrayfire.com>
2020-03-26 11:45:40 +0530

    Use static cufft,cublas,cusolver and cusolver on Unix
    thrust::stable_sort_by_key has known issue with device linking. The code
    crashes with cudaInvalidValueError. It works as expected without any
    changes with or without separable compilation otherwise.
    
    https://github.com/thrust/thrust/wiki/Debugging#known-issues
    https://github.com/thrust/thrust/blob/master/doc/changelog.md#known-issues-2
    
    The above documents mention a known issue with device linking and thrust.
    Although the documents say it happens in debug mode(with -G flag), I noticed
    similar crashes in release configuration too in ArrayFire.
    
    Due to the above issue, I have separated out the relevant source files
    (fft,blas,sparse and solver) which require device linking into separate
    static library. Once separated into a separate static library, sort_by_key
    and all the other unit tests that use it are running as expected without
    any crashes.

--- src/backend/cuda/types.hpp
+++ src/backend/cuda/types.hpp
@@ -148,13 +148,17 @@ struct kernel_type<common::half> {
     using data = common::half;
 
 #ifdef __CUDA_ARCH__
+
     // These are the types within a kernel
 #if __CUDA_ARCH__ >= 530 && __CUDA_ARCH__ != 610
     using compute = __half;
 #else
     using compute = float;
 #endif
-#else
+    using native = compute;
+
+#else  // __CUDA_ARCH__
+
     // outside of a cuda kernel use float
     using compute = float;
 
@@ -163,6 +167,7 @@ struct kernel_type<common::half> {
 #else
     using native = common::half;
 #endif
-#endif
+
+#endif  // __CUDA_ARCH__
 };
 }  // namespace common
