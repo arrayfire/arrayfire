e2ad39d68504582d5ebf575e1cc42c146aa43f9a
Corentin Schreiber <54102755+cschreib-ibex@users.noreply.github.com>
2020-04-24 11:34:13 +0100

    Auto cache compiled CUDA kernels on disk to speed up compilation (#2848)
    * Adds CMake variable AF_CACHE_KERNELS_TO_DISK to enable kernel caching. It is turned ON by default.
    * cuda::buildKernel() now dumps cubin to disk for reuse
    * Adds cuda::loadKernel() for loading cached cubin files
    * cuda::loadKernel() returns empty kernel on failure
    * Uses XDG_CACHE_HOME as cache directory for Linux
    * Adds common::deterministicHash() - This uses the FNV-1a hashing algorithm for fast and reproducible hashing of string or binary data. This is meant to replace the use of std::hash in some place, since std::hash does not guarantee its return value will be the same in subsequence executions of the program.
    * Write cached kernel to temporary file before moving into final file. This prevents data races where two threads or two processes might write to the same file.
    * Uses deterministicHash() for hashing kernel names and kernel binary data.
    * Adds kernel binary data file integrity check upon loading from disk

--- src/backend/common/util.cpp
+++ src/backend/common/util.cpp
@@ -10,17 +10,28 @@
 /// This file contains platform independent utility functions
 #if defined(OS_WIN)
 #include <Windows.h>
+#else
+#include <pwd.h>
+#include <unistd.h>
 #endif
 
 #include <common/defines.hpp>
 #include <common/util.hpp>
 #include <af/defines.h>
 
+#include <sys/stat.h>
+#include <algorithm>
+#include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
+#include <numeric>
 #include <string>
+#include <thread>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 string getEnvVar(const std::string& key) {
 #if defined(OS_WIN)
@@ -90,4 +101,115 @@ void saveKernel(const std::string& funcName, const std::string& jit_ker,
 std::string int_version_to_string(int version) {
     return std::to_string(version / 1000) + "." +
            std::to_string((int)((version % 1000) / 10.));
+}
+
+#if defined(OS_WIN)
+string getTemporaryDirectory() {
+    DWORD bufSize = 261;  // limit according to GetTempPath documentation
+    string retVal;
+    retVal.resize(bufSize);
+    bufSize = GetTempPathA(bufSize, &retVal[0]);
+    retVal.resize(bufSize);
+    return retVal;
+}
+#else
+string getHomeDirectory() {
+    string home = getEnvVar("XDG_CACHE_HOME");
+    if (!home.empty()) return home;
+
+    home = getEnvVar("HOME");
+    if (!home.empty()) return home;
+
+    return getpwuid(getuid())->pw_dir;
+}
+#endif
+
+bool directoryExists(const string& path) {
+#if defined(OS_WIN)
+    struct _stat status;
+    return _stat(path.c_str(), &status) == 0 && (status.st_mode & S_IFDIR) != 0;
+#else
+    struct stat status;
+    return stat(path.c_str(), &status) == 0 && (status.st_mode & S_IFDIR) != 0;
+#endif
+}
+
+bool createDirectory(const string& path) {
+#if defined(OS_WIN)
+    return CreateDirectoryA(path.c_str(), NULL) != 0;
+#else
+    return mkdir(path.c_str(), 0777) == 0;
+#endif
+}
+
+bool removeFile(const string& path) {
+#if defined(OS_WIN)
+    return DeleteFileA(path.c_str()) != 0;
+#else
+    return unlink(path.c_str()) == 0;
+#endif
+}
+
+bool renameFile(const string& sourcePath, const string& destPath) {
+    return std::rename(sourcePath.c_str(), destPath.c_str()) == 0;
+}
+
+bool isDirectoryWritable(const string& path) {
+    if (!directoryExists(path) && !createDirectory(path)) return false;
+
+    const string testPath = path + AF_PATH_SEPARATOR + "test";
+    if (!std::ofstream(testPath).is_open()) return false;
+    removeFile(testPath);
+
+    return true;
+}
+
+const string& getCacheDirectory() {
+    static std::once_flag flag;
+    static string cacheDirectory;
+
+    std::call_once(flag, []() {
+        const vector<string> pathList = {
+#if defined(OS_WIN)
+            getTemporaryDirectory() + "\\ArrayFire"
+#else
+            getHomeDirectory() + "/.arrayfire",
+            "/tmp/arrayfire"
+#endif
+        };
+
+        auto iterDir =
+            std::find_if(pathList.begin(), pathList.end(), isDirectoryWritable);
+
+        cacheDirectory = iterDir != pathList.end() ? *iterDir : "";
+    });
+
+    return cacheDirectory;
+}
+
+string makeTempFilename() {
+    thread_local std::size_t fileCount = 0u;
+
+    ++fileCount;
+    const std::size_t threadID =
+        std::hash<std::thread::id>{}(std::this_thread::get_id());
+
+    return std::to_string(std::hash<string>{}(std::to_string(threadID) + "_" +
+                                              std::to_string(fileCount)));
+}
+
+std::size_t deterministicHash(const void* data, std::size_t byteSize) {
+    // Fowler-Noll-Vo "1a" 32 bit hash
+    // https://en.wikipedia.org/wiki/Fowler-Noll-Vo_hash_function
+    constexpr std::size_t seed   = 0x811C9DC5;
+    constexpr std::size_t prime  = 0x01000193;
+    const std::uint8_t* byteData = static_cast<const std::uint8_t*>(data);
+    return std::accumulate(byteData, byteData + byteSize, seed,
+                           [&](std::size_t hash, std::uint8_t data) {
+                               return (hash ^ data) * prime;
+                           });
+}
+
+std::size_t deterministicHash(const std::string& data) {
+    return deterministicHash(data.data(), data.size());
 }
