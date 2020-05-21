3f50abe6ad8a9f90f78f201f7e2a0e5db4330b33
pradeep <pradeep@arrayfire.com>
2020-05-21 14:27:31 +0530

    Protect kernel caching using shared_mutex
    Earlier it was thread_local was duplicating caches and compilation
    of those jit kernels.

--- src/backend/common/kernel_cache.cpp
+++ src/backend/common/kernel_cache.cpp
@@ -17,6 +17,7 @@
 
 #include <algorithm>
 #include <map>
+#include <shared_mutex>
 #include <string>
 #include <vector>
 
@@ -24,44 +25,33 @@ using detail::Kernel;
 
 using std::back_inserter;
 using std::map;
+using std::shared_lock;
+using std::shared_timed_mutex;
 using std::string;
 using std::transform;
+using std::unique_lock;
 using std::vector;
 
 namespace common {
 
 using KernelMap = map<string, Kernel>;
 
+shared_timed_mutex& getCacheMutex(const int device) {
+    static shared_timed_mutex jitMutexes[detail::DeviceManager::MAX_DEVICES];
+    return jitMutexes[device];
+}
+
 KernelMap& getCache(const int device) {
-    thread_local KernelMap caches[detail::DeviceManager::MAX_DEVICES];
+    static KernelMap caches[detail::DeviceManager::MAX_DEVICES];
     return caches[device];
 }
 
-void cacheKernel(const int device, const string& nameExpr, const Kernel entry) {
-    getCache(device).emplace(nameExpr, entry);
-}
-
-Kernel findKernel(const int device, const string& nameExpr,
-                  const vector<string>& sources) {
-    auto& cache = getCache(device);
-    auto iter   = cache.find(nameExpr);
-
-    if (iter != cache.end()) return iter->second;
-
-    if (sources.size() > 0) {
-#if defined(AF_CUDA) && defined(AF_CACHE_KERNELS_TO_DISK)
-        Kernel kernel = loadKernelFromDisk(device, nameExpr, sources);
-        if (kernel.getModule() != nullptr && kernel.getKernel() != nullptr) {
-            cacheKernel(device, nameExpr, kernel);
-            return kernel;
-        }
-#endif
-    }
-    return Kernel{nullptr, nullptr};
-}
-
 Kernel findKernel(const int device, const string& key) {
-    return findKernel(device, key, {});
+    shared_lock<shared_timed_mutex> readLock(getCacheMutex(device));
+    auto& cache = getCache(device);
+    auto iter   = cache.find(key);
+    if (iter != cache.end()) { return iter->second; }
+    return Kernel{nullptr, nullptr};
 }
 
 Kernel getKernel(const string& kernelName, const vector<string>& sources,
@@ -83,12 +73,31 @@ Kernel getKernel(const string& kernelName, const vector<string>& sources,
     }
 
     int device    = detail::getActiveDeviceId();
-    Kernel kernel = findKernel(device, tInstance, sources);
+    Kernel kernel = findKernel(device, tInstance);
 
     if (kernel.getModule() == nullptr || kernel.getKernel() == nullptr) {
+#if defined(AF_CUDA) && defined(AF_CACHE_KERNELS_TO_DISK)
+        kernel = loadKernelFromDisk(device, tInstance, sources);
+        if (kernel.getModule() == nullptr || kernel.getKernel() == nullptr) {
+            kernel = compileKernel(kernelName, tInstance, sources, compileOpts,
+                                   isKernelJIT);
+        }
+#else
         kernel = compileKernel(kernelName, tInstance, sources, compileOpts,
                                isKernelJIT);
-        cacheKernel(device, tInstance, kernel);
+#endif
+        unique_lock<shared_timed_mutex> writeLock(getCacheMutex(device));
+
+        auto& cache = getCache(device);
+
+        // Lookup in case another thread finished adding the same kernel
+        // to this device's cache during it's life time with write access
+        auto iter = cache.find(tInstance);
+        if (iter != cache.end()) {
+            kernel = iter->second;
+        } else {
+            cache.emplace(tInstance, kernel);
+        }
     }
     return kernel;
 }
