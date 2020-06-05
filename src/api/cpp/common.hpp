9b90c471af827470835aa2b7b28e3fe55642cc12
Umar Arshad <umar@arrayfire.com>
2020-06-05 01:16:28 -0400

    Fix several error messages when compiling against CUDA 9.0
    * Address casts from double to __half which are missing in 9.0
    * Thrust return_temporary_buffer function can accept void* pointers
      in older versions of Thrust. Use raw_pointer_cast to pass the
      pointer to memFree
    * cublasGemmEx doesn't exist in CUDA 9.0. Add ifdefs to guard
      against older builds
    * __float2half is not a host function so it needs to be removed
      from mean
    * Add template instantiation for memFree to accept void* pointers

--- src/api/cpp/common.hpp
+++ src/api/cpp/common.hpp
@@ -15,6 +15,10 @@
 #include "half.hpp"
 #pragma GCC diagnostic pop
 
+#ifdef AF_CUDA
+#include <cuda_fp16.h>
+#endif
+
 #include <cstring>
 
 namespace af {
@@ -36,9 +40,19 @@ static inline dim_t getFNSD(const int dim, af::dim4 dims) {
 namespace {
 // casts from one type to another. Needed for af_half conversions specialization
 template<typename To, typename T>
-To cast(T in) {
+inline To cast(T in) {
     return static_cast<To>(in);
 }
+
+#if defined(AF_CUDA) && CUDA_VERSION < 10000
+template<>
+inline __half cast<__half, double>(double in) {
+    __half_raw out;
+    half_float::half h(in);
+    memcpy(&out, &h, sizeof(__half_raw));
+    return out;
+}
+#endif
 
 template<>
 [[gnu::unused]] af_half cast<af_half, double>(double in) {
