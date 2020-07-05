d9f672eab93378a521da52f21986858a889577ba
pradeep <pradeep@arrayfire.com>
2020-07-05 16:51:52 +0530

    Add support for cuDNN verion 8
    Adds runtime support for cuDNN 8

--- src/backend/cuda/cudnnModule.hpp
+++ src/backend/cuda/cudnnModule.hpp
@@ -31,6 +31,36 @@ cudnnStatus_t cudnnSetFilter4dDescriptor_v4(
 size_t cudnnGetCudartVersion(void);
 #endif
 
+#if CUDNN_VERSION >= 8000
+typedef enum {
+    CUDNN_CONVOLUTION_FWD_NO_WORKSPACE            = 0,
+    CUDNN_CONVOLUTION_FWD_PREFER_FASTEST          = 1,
+    CUDNN_CONVOLUTION_FWD_SPECIFY_WORKSPACE_LIMIT = 2,
+} cudnnConvolutionFwdPreference_t;
+
+typedef enum {
+    CUDNN_CONVOLUTION_BWD_FILTER_NO_WORKSPACE            = 0,
+    CUDNN_CONVOLUTION_BWD_FILTER_PREFER_FASTEST          = 1,
+    CUDNN_CONVOLUTION_BWD_FILTER_SPECIFY_WORKSPACE_LIMIT = 2,
+} cudnnConvolutionBwdFilterPreference_t;
+
+cudnnStatus_t cudnnGetConvolutionForwardAlgorithm(
+    cudnnHandle_t handle, const cudnnTensorDescriptor_t xDesc,
+    const cudnnFilterDescriptor_t wDesc,
+    const cudnnConvolutionDescriptor_t convDesc,
+    const cudnnTensorDescriptor_t yDesc,
+    cudnnConvolutionFwdPreference_t preference, size_t memoryLimitInBytes,
+    cudnnConvolutionFwdAlgo_t* algo);
+
+cudnnStatus_t cudnnGetConvolutionBackwardFilterAlgorithm(
+    cudnnHandle_t handle, const cudnnTensorDescriptor_t xDesc,
+    const cudnnTensorDescriptor_t dyDesc,
+    const cudnnConvolutionDescriptor_t convDesc,
+    const cudnnFilterDescriptor_t dwDesc,
+    cudnnConvolutionBwdFilterPreference_t preference, size_t memoryLimitInBytes,
+    cudnnConvolutionBwdFilterAlgo_t* algo);
+#endif
+
 namespace cuda {
 
 class cudnnModule {
@@ -51,10 +81,14 @@ class cudnnModule {
     MODULE_MEMBER(cudnnDestroyFilterDescriptor);
     MODULE_MEMBER(cudnnDestroyTensorDescriptor);
     MODULE_MEMBER(cudnnGetConvolutionBackwardDataWorkspaceSize);
-    MODULE_MEMBER(cudnnGetConvolutionBackwardFilterAlgorithm);
+    MODULE_MEMBER(cudnnGetConvolutionForwardAlgorithmMaxCount);
+    MODULE_MEMBER(cudnnGetConvolutionBackwardFilterAlgorithmMaxCount);
+    MODULE_MEMBER(cudnnFindConvolutionForwardAlgorithm);
+    MODULE_MEMBER(cudnnFindConvolutionBackwardFilterAlgorithm);
+    MODULE_MEMBER(cudnnGetConvolutionForwardWorkspaceSize);
     MODULE_MEMBER(cudnnGetConvolutionBackwardFilterWorkspaceSize);
     MODULE_MEMBER(cudnnGetConvolutionForwardAlgorithm);
-    MODULE_MEMBER(cudnnGetConvolutionForwardWorkspaceSize);
+    MODULE_MEMBER(cudnnGetConvolutionBackwardFilterAlgorithm);
     MODULE_MEMBER(cudnnGetConvolutionNdForwardOutputDim);
     MODULE_MEMBER(cudnnSetConvolution2dDescriptor);
     MODULE_MEMBER(cudnnSetFilter4dDescriptor);
