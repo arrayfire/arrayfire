1d08272dacc227bd6c87da20bddc6087ab19389b
pradeep <pradeep@arrayfire.com>
2020-07-09 12:34:29 +0530

    Update confidence connected components to use brain scan image

--- examples/image_processing/confidence_connected_components.cpp
+++ examples/image_processing/confidence_connected_components.cpp
@@ -15,40 +15,52 @@
 
 using namespace af;
 
+array normalize01(const array& in) {
+    float min = af::min<float>(in);
+    float max = af::max<float>(in);
+    return (in - min) / (max - min);
+}
+
+void markCrossHair(array& in, const unsigned x, const unsigned y,
+                   const float val) {
+    const int draw_len = 5;
+    for (int i = -1; i < 2; i++) {
+        in(x + i, seq(y - draw_len, y + draw_len), 0) = val;
+        in(x + i, seq(y - draw_len, y + draw_len), 1) = 0.f;
+        in(x + i, seq(y - draw_len, y + draw_len), 2) = 0.f;
+
+        in(seq(x - draw_len, x + draw_len), y + i, 0) = val;
+        in(seq(x - draw_len, x + draw_len), y + i, 1) = 0.f;
+        in(seq(x - draw_len, x + draw_len), y + i, 2) = 0.f;
+    }
+}
+
 int main(int argc, char* argv[]) {
     try {
         unsigned radius     = 3;
-        unsigned multiplier = 3;
-        int iter            = 5;
+        unsigned multiplier = 2;
+        int iter            = 3;
 
-        array A = loadImage(ASSETS_DIR "/examples/images/donut.png", false);
+        array input =
+            loadImage(ASSETS_DIR "/examples/images/depression.jpg", false);
+        array normIn = normalize01(input);
 
-        unsigned seedx = 132;
-        unsigned seedy = 132;
-        array ring =
-            confidenceCC(A, 1, &seedx, &seedy, radius, multiplier, iter, 255);
+        unsigned seedx = 162;
+        unsigned seedy = 126;
+        array blob = confidenceCC(input, 1, &seedx, &seedy, radius, multiplier,
+                                  iter, 255);
 
-        seedx = 152;
-        seedy = 152;
-        array sxArr(dim4(1), &seedx);
-        array syArr(dim4(1), &seedy);
-        array core =
-            confidenceCC(A, sxArr, syArr, radius, multiplier, iter, 255);
+        array colorIn  = colorSpace(normIn, AF_RGB, AF_GRAY);
+        array colorOut = colorSpace(blob, AF_RGB, AF_GRAY);
 
-        seedx                 = 15;
-        seedy                 = 15;
-        unsigned seedcoords[] = {15, 15};
-        array seeds(dim4(1, 2), seedcoords);
-        array background =
-            confidenceCC(A, seeds, radius, multiplier, iter, 255);
+        markCrossHair(colorIn, seedx, seedy, 1);
+        markCrossHair(colorOut, seedx, seedy, 255);
 
-        af::Window wnd("Confidence Connected Components demo");
+        af::Window wnd("Confidence Connected Components Demo");
         while (!wnd.close()) {
-            wnd.grid(2, 2);
-            wnd(0, 0).image(A, "Input");
-            wnd(0, 1).image(ring, "Ring Component - Seed(132, 132)");
-            wnd(1, 0).image(core, "Center Black Hole - Seed(152, 152)");
-            wnd(1, 1).image(background, "Background - Seed(15, 15)");
+            wnd.grid(1, 2);
+            wnd(0, 0).image(colorIn, "Input Brain Scan");
+            wnd(0, 1).image(colorOut, "Region connected to Seed(162, 126)");
             wnd.show();
         }
     } catch (af::exception& e) {
