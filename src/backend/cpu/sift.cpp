1af2d82681f80d5b71c89816740fcbf6a95b97ba
pradeep <pradeep@arrayfire.com>
2020-12-29 22:06:47 +0530

    Remove non-free guards for SIFT/GLOH algorithms
    SIFT patent expired recently and these algorithms can be
    provided as part of open source binaries that are distributed from our
    website.

--- src/backend/cpu/sift.cpp
+++ src/backend/cpu/sift.cpp
@@ -7,21 +7,9 @@
  * http://arrayfire.com/licenses/BSD-3-Clause
  ********************************************************/
 
-#include <Array.hpp>
-#include <convolve.hpp>
-#include <err_cpu.hpp>
-#include <math.hpp>
-#include <memory.hpp>
-#include <resize.hpp>
-#include <sort_index.hpp>
-#include <af/dim4.hpp>
-#include <cfloat>
-#include <cstring>
-#include <vector>
+#include <sift.hpp>
 
-#ifdef AF_WITH_NONFREE_SIFT
-#include <kernel/sift_nonfree.hpp>
-#endif
+#include <kernel/sift.hpp>
 
 using af::dim4;
 
@@ -35,35 +23,9 @@ unsigned sift(Array<float>& x, Array<float>& y, Array<float>& score,
               const float init_sigma, const bool double_input,
               const float img_scale, const float feature_ratio,
               const bool compute_GLOH) {
-#ifdef AF_WITH_NONFREE_SIFT
     return sift_impl<T, convAccT>(
         x, y, score, ori, size, desc, in, n_layers, contrast_thr, edge_thr,
         init_sigma, double_input, img_scale, feature_ratio, compute_GLOH);
-#else
-    UNUSED(x);
-    UNUSED(y);
-    UNUSED(score);
-    UNUSED(ori);
-    UNUSED(size);
-    UNUSED(desc);
-    UNUSED(in);
-    UNUSED(n_layers);
-    UNUSED(contrast_thr);
-    UNUSED(edge_thr);
-    UNUSED(init_sigma);
-    UNUSED(double_input);
-    UNUSED(img_scale);
-    UNUSED(feature_ratio);
-    if (compute_GLOH) {
-        AF_ERROR(
-            "ArrayFire was not built with nonfree support, GLOH disabled\n",
-            AF_ERR_NONFREE);
-    } else {
-        AF_ERROR(
-            "ArrayFire was not built with nonfree support, SIFT disabled\n",
-            AF_ERR_NONFREE);
-    }
-#endif
 }
 
 #define INSTANTIATE(T, convAccT)                                               \
