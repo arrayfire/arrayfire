30db56d3b0ff8f3f30629650d0c3d6a4018220ef
Umar Arshad <umar@arrayfire.com>
2021-08-09 17:41:34 -0400

    Move createBinaryNode to common
    (cherry picked from commit 92dd704efac7d1990bb9c0aa8b179aba803c788a)

---
+++ src/backend/common/jit/BinaryNode.cpp
@@ -0,0 +1,149 @@
+
+#include <Array.hpp>
+#include <binary.hpp>
+#include <common/jit/BinaryNode.hpp>
+#include <complex.hpp>
+#include <types.hpp>
+
+using af::dim4;
+using af::dtype_traits;
+using detail::Array;
+using detail::BinOp;
+using detail::cdouble;
+using detail::cfloat;
+using detail::createNodeArray;
+
+namespace common {
+#ifdef AF_CPU
+template<typename To, typename Ti, af_op_t op>
+Array<To> createBinaryNode(const Array<Ti> &lhs, const Array<Ti> &rhs,
+                           const af::dim4 &odims) {
+    common::Node_ptr lhs_node = lhs.getNode();
+    common::Node_ptr rhs_node = rhs.getNode();
+
+    detail::jit::BinaryNode<To, Ti, op> *node =
+        new detail::jit::BinaryNode<To, Ti, op>(lhs_node, rhs_node);
+
+    return createNodeArray<To>(odims, common::Node_ptr(node));
+}
+
+#else
+
+template<typename To, typename Ti, af_op_t op>
+Array<To> createBinaryNode(const Array<Ti> &lhs, const Array<Ti> &rhs,
+                           const af::dim4 &odims) {
+    auto createBinary = [](std::array<Node_ptr, 2> &operands) -> Node_ptr {
+        BinOp<To, Ti, op> bop;
+        return Node_ptr(
+            new BinaryNode(static_cast<af::dtype>(dtype_traits<To>::af_type),
+                           bop.name(), operands[0], operands[1], (int)(op)));
+    };
+
+    Node_ptr out =
+        common::createNaryNode<Ti, 2>(odims, createBinary, {&lhs, &rhs});
+    return createNodeArray<To>(odims, out);
+}
+
+#endif
+
+#define INSTANTIATE(To, Ti, op)                      \
+    template Array<To> createBinaryNode<To, Ti, op>( \
+        const Array<Ti> &lhs, const Array<Ti> &rhs, const dim4 &odims)
+
+INSTANTIATE(cfloat, float, af_cplx2_t);
+INSTANTIATE(cdouble, double, af_cplx2_t);
+
+#define INSTANTIATE_ARITH(op)                                \
+    INSTANTIATE(float, float, op);                           \
+    INSTANTIATE(cfloat, cfloat, op);                         \
+    INSTANTIATE(double, double, op);                         \
+    INSTANTIATE(cdouble, cdouble, op);                       \
+    INSTANTIATE(unsigned, unsigned, op);                     \
+    INSTANTIATE(short, short, op);                           \
+    INSTANTIATE(unsigned short, unsigned short, op);         \
+    INSTANTIATE(unsigned long long, unsigned long long, op); \
+    INSTANTIATE(long long, long long, op);                   \
+    INSTANTIATE(unsigned char, unsigned char, op);           \
+    INSTANTIATE(char, char, op);                             \
+    INSTANTIATE(common::half, common::half, op);             \
+    INSTANTIATE(int, int, op)
+
+INSTANTIATE_ARITH(af_add_t);
+INSTANTIATE_ARITH(af_sub_t);
+INSTANTIATE_ARITH(af_mul_t);
+INSTANTIATE_ARITH(af_div_t);
+INSTANTIATE_ARITH(af_min_t);
+INSTANTIATE_ARITH(af_max_t);
+
+#undef INSTANTIATE_ARITH
+
+#define INSTANTIATE_ARITH_REAL(op)                           \
+    INSTANTIATE(float, float, op);                           \
+    INSTANTIATE(double, double, op);                         \
+    INSTANTIATE(unsigned, unsigned, op);                     \
+    INSTANTIATE(short, short, op);                           \
+    INSTANTIATE(unsigned short, unsigned short, op);         \
+    INSTANTIATE(unsigned long long, unsigned long long, op); \
+    INSTANTIATE(long long, long long, op);                   \
+    INSTANTIATE(unsigned char, unsigned char, op);           \
+    INSTANTIATE(char, char, op);                             \
+    INSTANTIATE(common::half, common::half, op);             \
+    INSTANTIATE(int, int, op)
+
+INSTANTIATE_ARITH_REAL(af_rem_t);
+INSTANTIATE_ARITH_REAL(af_pow_t);
+INSTANTIATE_ARITH_REAL(af_mod_t);
+
+#define INSTANTIATE_FLOATOPS(op)     \
+    INSTANTIATE(float, float, op);   \
+    INSTANTIATE(double, double, op); \
+    INSTANTIATE(common::half, common::half, op)
+
+INSTANTIATE_FLOATOPS(af_hypot_t);
+INSTANTIATE_FLOATOPS(af_atan2_t);
+
+#define INSTANTIATE_BITOP(op)                                \
+    INSTANTIATE(unsigned, unsigned, op);                     \
+    INSTANTIATE(short, short, op);                           \
+    INSTANTIATE(unsigned short, unsigned short, op);         \
+    INSTANTIATE(unsigned long long, unsigned long long, op); \
+    INSTANTIATE(long long, long long, op);                   \
+    INSTANTIATE(unsigned char, unsigned char, op);           \
+    INSTANTIATE(char, char, op);                             \
+    INSTANTIATE(int, int, op)
+
+INSTANTIATE_BITOP(af_bitshiftl_t);
+INSTANTIATE_BITOP(af_bitshiftr_t);
+INSTANTIATE_BITOP(af_bitor_t);
+INSTANTIATE_BITOP(af_bitand_t);
+INSTANTIATE_BITOP(af_bitxor_t);
+#undef INSTANTIATE_BITOP
+
+#define INSTANTIATE_LOGIC(op)                  \
+    INSTANTIATE(char, float, op);              \
+    INSTANTIATE(char, double, op);             \
+    INSTANTIATE(char, cfloat, op);             \
+    INSTANTIATE(char, cdouble, op);            \
+    INSTANTIATE(char, common::half, op);       \
+    INSTANTIATE(char, unsigned, op);           \
+    INSTANTIATE(char, short, op);              \
+    INSTANTIATE(char, unsigned short, op);     \
+    INSTANTIATE(char, unsigned long long, op); \
+    INSTANTIATE(char, long long, op);          \
+    INSTANTIATE(char, unsigned char, op);      \
+    INSTANTIATE(char, char, op);               \
+    INSTANTIATE(char, int, op)
+
+INSTANTIATE_LOGIC(af_and_t);
+INSTANTIATE_LOGIC(af_or_t);
+INSTANTIATE_LOGIC(af_eq_t);
+INSTANTIATE_LOGIC(af_neq_t);
+INSTANTIATE_LOGIC(af_lt_t);
+INSTANTIATE_LOGIC(af_le_t);
+INSTANTIATE_LOGIC(af_gt_t);
+INSTANTIATE_LOGIC(af_ge_t);
+
+#undef INSTANTIATE_LOGIC
+#undef INSTANTIATE
+
+}  // namespace common
---
+++ src/backend/cpu/binary.hpp
@@ -0,0 +1,152 @@
+/*******************************************************
+ * Copyright (c) 2021, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+#pragma once
+
+#include <math.hpp>
+#include <optypes.hpp>
+#include <types.hpp>
+#include <cmath>
+
+namespace cpu {
+
+template<typename To, typename Ti, af_op_t op>
+struct BinOp;
+
+#define ARITH_FN(OP, op)                                                 \
+    template<typename T>                                                 \
+    struct BinOp<T, T, OP> {                                             \
+        void eval(jit::array<compute_t<T>> &out,                         \
+                  const jit::array<compute_t<T>> &lhs,                   \
+                  const jit::array<compute_t<T>> &rhs, int lim) const {  \
+            for (int i = 0; i < lim; i++) { out[i] = lhs[i] op rhs[i]; } \
+        }                                                                \
+    };
+
+ARITH_FN(af_add_t, +)
+ARITH_FN(af_sub_t, -)
+ARITH_FN(af_mul_t, *)
+ARITH_FN(af_div_t, /)
+
+#undef ARITH_FN
+
+#define LOGIC_FN(OP, op)                                                      \
+    template<typename T>                                                      \
+    struct BinOp<char, T, OP> {                                               \
+        void eval(jit::array<char> &out, const jit::array<compute_t<T>> &lhs, \
+                  const jit::array<compute_t<T>> &rhs, int lim) {             \
+            for (int i = 0; i < lim; i++) { out[i] = lhs[i] op rhs[i]; }      \
+        }                                                                     \
+    };
+
+LOGIC_FN(af_eq_t, ==)
+LOGIC_FN(af_neq_t, !=)
+LOGIC_FN(af_lt_t, <)
+LOGIC_FN(af_gt_t, >)
+LOGIC_FN(af_le_t, <=)
+LOGIC_FN(af_ge_t, >=)
+LOGIC_FN(af_and_t, &&)
+LOGIC_FN(af_or_t, ||)
+
+#undef LOGIC_FN
+
+#define LOGIC_CPLX_FN(T, OP, op)                                               \
+    template<>                                                                 \
+    struct BinOp<char, std::complex<T>, OP> {                                  \
+        typedef std::complex<T> Ti;                                            \
+        void eval(jit::array<char> &out, const jit::array<compute_t<Ti>> &lhs, \
+                  const jit::array<compute_t<Ti>> &rhs, int lim) {             \
+            for (int i = 0; i < lim; i++) {                                    \
+                T lhs_mag = std::abs(lhs[i]);                                  \
+                T rhs_mag = std::abs(rhs[i]);                                  \
+                out[i]    = lhs_mag op rhs_mag;                                \
+            }                                                                  \
+        }                                                                      \
+    };
+
+LOGIC_CPLX_FN(float, af_lt_t, <)
+LOGIC_CPLX_FN(float, af_le_t, <=)
+LOGIC_CPLX_FN(float, af_gt_t, >)
+LOGIC_CPLX_FN(float, af_ge_t, >=)
+LOGIC_CPLX_FN(float, af_and_t, &&)
+LOGIC_CPLX_FN(float, af_or_t, ||)
+
+LOGIC_CPLX_FN(double, af_lt_t, <)
+LOGIC_CPLX_FN(double, af_le_t, <=)
+LOGIC_CPLX_FN(double, af_gt_t, >)
+LOGIC_CPLX_FN(double, af_ge_t, >=)
+LOGIC_CPLX_FN(double, af_and_t, &&)
+LOGIC_CPLX_FN(double, af_or_t, ||)
+
+#undef LOGIC_CPLX_FN
+
+template<typename T>
+static T __mod(T lhs, T rhs) {
+    T res = lhs % rhs;
+    return (res < 0) ? abs(rhs - res) : res;
+}
+
+template<typename T>
+static T __rem(T lhs, T rhs) {
+    return lhs % rhs;
+}
+
+template<>
+STATIC_ float __mod<float>(float lhs, float rhs) {
+    return fmod(lhs, rhs);
+}
+template<>
+STATIC_ double __mod<double>(double lhs, double rhs) {
+    return fmod(lhs, rhs);
+}
+template<>
+STATIC_ float __rem<float>(float lhs, float rhs) {
+    return remainder(lhs, rhs);
+}
+template<>
+STATIC_ double __rem<double>(double lhs, double rhs) {
+    return remainder(lhs, rhs);
+}
+
+#define BITWISE_FN(OP, op)                                               \
+    template<typename T>                                                 \
+    struct BinOp<T, T, OP> {                                             \
+        void eval(jit::array<compute_t<T>> &out,                         \
+                  const jit::array<compute_t<T>> &lhs,                   \
+                  const jit::array<compute_t<T>> &rhs, int lim) {        \
+            for (int i = 0; i < lim; i++) { out[i] = lhs[i] op rhs[i]; } \
+        }                                                                \
+    };
+
+BITWISE_FN(af_bitor_t, |)
+BITWISE_FN(af_bitand_t, &)
+BITWISE_FN(af_bitxor_t, ^)
+BITWISE_FN(af_bitshiftl_t, <<)
+BITWISE_FN(af_bitshiftr_t, >>)
+
+#undef BITWISE_FN
+
+#define NUMERIC_FN(OP, FN)                                                 \
+    template<typename T>                                                   \
+    struct BinOp<T, T, OP> {                                               \
+        void eval(jit::array<compute_t<T>> &out,                           \
+                  const jit::array<compute_t<T>> &lhs,                     \
+                  const jit::array<compute_t<T>> &rhs, int lim) {          \
+            for (int i = 0; i < lim; i++) { out[i] = FN(lhs[i], rhs[i]); } \
+        }                                                                  \
+    };
+
+NUMERIC_FN(af_max_t, max)
+NUMERIC_FN(af_min_t, min)
+NUMERIC_FN(af_mod_t, __mod)
+NUMERIC_FN(af_pow_t, pow)
+NUMERIC_FN(af_rem_t, __rem)
+NUMERIC_FN(af_atan2_t, atan2)
+NUMERIC_FN(af_hypot_t, hypot)
+
+}  // namespace cpu
