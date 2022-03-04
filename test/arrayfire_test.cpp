2fd4557cee5ee3fcb46da3a15a87f67141aa09a6
syurkevi <stefan@arrayfire.com>
2022-03-04 12:35:52 -0500

    change imageio names to include backend to avoid collisions

--- test/arrayfire_test.cpp
+++ test/arrayfire_test.cpp
@@ -29,7 +29,6 @@
 #include <iterator>
 #include <limits>
 #include <numeric>
-#include <random>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -1690,25 +1689,6 @@ INSTANTIATE(std::complex<float>);
 INSTANTIATE(std::complex<double>);
 INSTANTIATE(af_half);
 #undef INSTANTIATE
-
-unsigned int random_char() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 255);
-    return dis(gen);
-}
-
-std::string generate_hex(const unsigned int len) {
-    std::stringstream ss;
-    for (auto i = 0; i < len; i++) {
-        const auto rc = random_char();
-        std::stringstream hexstream;
-        hexstream << std::hex << rc;
-        auto hex = hexstream.str();
-        ss << (hex.length() < 2 ? '0' + hex : hex);
-    }
-    return ss.str();
-}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
