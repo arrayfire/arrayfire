368c6162fcca0cc38f63ae2e019bb38dcb790555
Umar Arshad <umar@arrayfire.com>
2022-03-11 18:34:45 -0500

    Add some debugging macros

---
+++ src/backend/common/debug.hpp
@@ -0,0 +1,62 @@
+/*******************************************************
+ * Copyright (c) 2022, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#pragma once
+
+#define FMT_HEADER_ONLY
+#include <boost/stacktrace.hpp>
+#include <common/ArrayFireTypesIO.hpp>
+#include <common/jit/NodeIO.hpp>
+#include <spdlog/fmt/bundled/format.h>
+#include <iostream>
+
+#define DBGTRACE(msg)                                              \
+    fmt::print(std::cout, __FILE__ ":{}:{}\n{}\n", __LINE__, #msg, \
+               boost::stacktrace::stacktrace())
+
+namespace debugging {
+
+template<typename first>
+void print(const char *F, const first &FF) {
+    fmt::print(std::cout, "{} = {}", F, FF);
+}
+
+template<typename first, typename... ARGS>
+void print(const char *F, const first &FF, ARGS... args) {
+    fmt::print(std::cout, "{} = {} | ", F, FF);
+    print(args...);
+}
+}  // namespace debugging
+
+#define SHOW1(val1) debugging::print(#val1, val1)
+#define SHOW2(val1, val2) debugging::print(#val1, val1, #val2, val2)
+#define SHOW3(val1, val2, val3) \
+    debugging::print(#val1, val1, #val2, val2, #val3, val3)
+
+#define SHOW4(val1, val2, val3, val4) \
+    debugging::print(#val1, val1, #val2, val2, #val3, val3, #val4, val4)
+#define SHOW5(val1, val2, val3, val4, val5)                              \
+    debugging::print(#val1, val1, #val2, val2, #val3, val3, #val4, val4, \
+                     #val5, val5)
+
+#define GET_MACRO(_1, _2, _3, _4, _5, NAME, ...) NAME
+
+#define SHOW(...)                                                 \
+    do {                                                          \
+        fmt::print(std::cout, "{}:({}): ", __FILE__, __LINE__);   \
+        GET_MACRO(__VA_ARGS__, SHOW5, SHOW4, SHOW3, SHOW2, SHOW1) \
+        (__VA_ARGS__);                                            \
+        fmt::print(std::cout, "\n");                              \
+    } while (0)
+
+#define PRINTVEC(val)                                                        \
+    do {                                                                     \
+        fmt::print(std::cout, "{}:({}):{} [{}]\n", __FILE__, __LINE__, #val, \
+                   fmt::join(val, ", "));                                    \
+    } while (0)
---
+++ src/backend/common/jit/NodeIO.hpp
@@ -0,0 +1,93 @@
+/*******************************************************
+ * Copyright (c) 2021, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#pragma once
+#include <common/jit/Node.hpp>
+#include <common/util.hpp>
+#include <spdlog/fmt/bundled/format.h>
+
+#include <common/TemplateArg.hpp>
+
+template<>
+struct fmt::formatter<af::dtype> : fmt::formatter<char> {
+    template<typename FormatContext>
+    auto format(const af::dtype& p, FormatContext& ctx) -> decltype(ctx.out()) {
+        format_to(ctx.out(), "{}", getName(p));
+        return ctx.out();
+    }
+};
+
+template<>
+struct fmt::formatter<common::Node> {
+    // Presentation format: 'p' - pointer, 't' - type.
+    // char presentation;
+    bool pointer;
+    bool type;
+    bool children;
+    bool op;
+
+    // Parses format specifications of the form ['f' | 'e'].
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        auto it = ctx.begin(), end = ctx.end();
+
+        if (it == end || *it == '}') {
+            pointer = type = children = op = true;
+            return it;
+        }
+
+        while (it != end && *it != '}') {
+            switch (*it) {
+                case 'p': pointer = true; break;
+                case 't': type = true; break;
+                case 'c': children = true; break;
+                case 'o': op = true; break;
+                default: throw format_error("invalid format");
+            }
+            ++it;
+        }
+
+        // Return an iterator past the end of the parsed range:
+        return it;
+    }
+
+    // Formats the point p using the parsed format specification (presentation)
+    // stored in this formatter.
+    template<typename FormatContext>
+    auto format(const common::Node& node, FormatContext& ctx)
+        -> decltype(ctx.out()) {
+        // ctx.out() is an output iterator to write to.
+
+        format_to(ctx.out(), "{{");
+        if (pointer) format_to(ctx.out(), "{} ", (void*)&node);
+        if (op) {
+            if (node.isBuffer()) {
+                format_to(ctx.out(), "buffer ");
+            } else {
+                format_to(ctx.out(), "{} ", getOpEnumStr(node.getOp()));
+            }
+        }
+        if (type) format_to(ctx.out(), "{} ", node.getType());
+        if (children) {
+            int count;
+            for (count = 0; count < common::Node::kMaxChildren &&
+                            node.m_children[count].get() != nullptr;
+                 count++) {}
+            if (count > 0) {
+                format_to(ctx.out(), "children: {{ ");
+                for (int i = 0; i < count; i++) {
+                    format_to(ctx.out(), "{} ", *(node.m_children[i].get()));
+                }
+                format_to(ctx.out(), "\b}} ");
+            }
+        }
+        format_to(ctx.out(), "\b}}");
+
+        return ctx.out();
+    }
+};
