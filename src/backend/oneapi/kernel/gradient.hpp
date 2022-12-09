00eaba7d6870a29f997984c91f6d2aa761a3f308
Gallagher Donovan Pryor <gallagher@arrayfire.com>
2022-12-09 05:22:07 -0500

    formatting

--- src/backend/oneapi/kernel/gradient.hpp
+++ src/backend/oneapi/kernel/gradient.hpp
@@ -15,7 +15,6 @@
 #include <debug_oneapi.hpp>
 #include <kernel/default_config.hpp>
 
-
 namespace oneapi {
 namespace kernel {
 
@@ -82,10 +81,11 @@ class gradientCreateKernel {
                    idy * grad1_.strides[1] + idx;
 
         // Multipliers - 0.5 for interior, 1 for edge cases
-        using Txy = std::conditional<std::is_same<T, std::complex<double>>::value,
-                                  double, float>::type;
-         Txy  xf = 0.5 * (1 + (idx == 0 || idx >= (in_.dims[0] - 1)));
-          Txy yf = 0.5 * (1 + (idy == 0 || idy >= (in_.dims[1] - 1)));
+        using Txy =
+            std::conditional<std::is_same<T, std::complex<double>>::value,
+                             double, float>::type;
+        Txy xf = 0.5 * (1 + (idx == 0 || idx >= (in_.dims[0] - 1)));
+        Txy yf = 0.5 * (1 + (idy == 0 || idy >= (in_.dims[1] - 1)));
 
         // Copy data to scratch space
         T zero = (T)(0);
