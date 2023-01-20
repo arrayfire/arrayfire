5dc2ae2c675ddd9a0b9c6a2521c6cad469be1250
Gallagher Donovan Pryor <gallagher@arrayfire.com>
2023-01-20 10:29:17 -0500

    transform

--- src/backend/oneapi/transform.cpp
+++ src/backend/oneapi/transform.cpp
@@ -9,7 +9,7 @@
 
 #include <transform.hpp>
 
-// #include <kernel/transform.hpp>
+#include <kernel/transform.hpp>
 #include <err_oneapi.hpp>
 
 namespace oneapi {
@@ -18,22 +18,30 @@ template<typename T>
 void transform(Array<T> &out, const Array<T> &in, const Array<float> &tf,
                const af_interp_type method, const bool inverse,
                const bool perspective) {
-    ONEAPI_NOT_SUPPORTED("transform Not supported");
     switch (method) {
         case AF_INTERP_NEAREST:
         case AF_INTERP_LOWER:
-            // kernel::transform<T>(out, in, tf, inverse, perspective, method,
-            // 1);
+            if constexpr (!(std::is_same_v<T, double> ||
+                            std::is_same_v<T, cdouble>)) {
+                kernel::transform<T>(out, in, tf, inverse, perspective, method,
+                                     1);
+            }
             break;
         case AF_INTERP_BILINEAR:
         case AF_INTERP_BILINEAR_COSINE:
-            // kernel::transform<T>(out, in, tf, inverse, perspective, method,
-            // 2);
+            if constexpr (!(std::is_same_v<T, double> ||
+                            std::is_same_v<T, cdouble>)) {
+                kernel::transform<T>(out, in, tf, inverse, perspective, method,
+                                     2);
+            }
             break;
         case AF_INTERP_BICUBIC:
         case AF_INTERP_BICUBIC_SPLINE:
-            // kernel::transform<T>(out, in, tf, inverse, perspective, method,
-            // 3);
+            if constexpr (!(std::is_same_v<T, double> ||
+                            std::is_same_v<T, cdouble>)) {
+                kernel::transform<T>(out, in, tf, inverse, perspective, method,
+                                     3);
+            }
             break;
         default: AF_ERROR("Unsupported interpolation type", AF_ERR_ARG);
     }
