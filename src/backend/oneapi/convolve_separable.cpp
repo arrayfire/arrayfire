e0f042342baa4abb84268951a9a526691253d266
pv-pterab-s <75991366+pv-pterab-s@users.noreply.github.com>
2023-04-24 15:42:36 -0400

    convolve2 and convolve_separable oneapi ports (#3409)
    * convolve2 (not separable) stubs filled in. half is not supported 
    
    ---------
    
    Co-authored-by: Gallagher Donovan Pryor <gallagher@arrayfire.com>

--- src/backend/oneapi/convolve_separable.cpp
+++ src/backend/oneapi/convolve_separable.cpp
@@ -1,5 +1,5 @@
 /*******************************************************
- * Copyright (c) 2022, ArrayFire
+ * Copyright (c) 2023, ArrayFire
  * All rights reserved.
  *
  * This file is distributed under 3-clause BSD license.
@@ -11,6 +11,7 @@
 
 #include <Array.hpp>
 #include <err_oneapi.hpp>
+#include <kernel/convolve_separable.hpp>
 #include <af/dim4.hpp>
 
 using af::dim4;
@@ -21,8 +22,36 @@ namespace oneapi {
 template<typename T, typename accT>
 Array<T> convolve2(Array<T> const& signal, Array<accT> const& c_filter,
                    Array<accT> const& r_filter, const bool expand) {
-    ONEAPI_NOT_SUPPORTED("");
-    Array<T> out = createEmptyArray<T>(dim4(1));
+    const auto cflen = c_filter.elements();
+    const auto rflen = r_filter.elements();
+
+    if ((cflen > kernel::MAX_SCONV_FILTER_LEN) ||
+        (rflen > kernel::MAX_SCONV_FILTER_LEN)) {
+        // TODO call upon fft
+        char errMessage[256];
+        snprintf(errMessage, sizeof(errMessage),
+                 "\noneAPI Separable convolution doesn't support %llu(coloumn) "
+                 "%llu(row) filters\n",
+                 cflen, rflen);
+        ONEAPI_NOT_SUPPORTED(errMessage);
+    }
+
+    const dim4& sDims = signal.dims();
+    dim4 tDims        = sDims;
+    dim4 oDims        = sDims;
+
+    if (expand) {
+        tDims[0] += cflen - 1;
+        oDims[0] += cflen - 1;
+        oDims[1] += rflen - 1;
+    }
+
+    Array<T> temp = createEmptyArray<T>(tDims);
+    Array<T> out  = createEmptyArray<T>(oDims);
+
+    kernel::convSep<T, accT>(temp, signal, c_filter, 0, expand);
+    kernel::convSep<T, accT>(out, temp, r_filter, 1, expand);
+
     return out;
 }
 
