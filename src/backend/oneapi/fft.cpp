dd96f53088e92d8b76bc74af051401d3cc4a2774
Gallagher Donovan Pryor <gallagher@arrayfire.com>
2023-06-16 13:27:53 -0400

    fix: fft_r2c and fft_c2r not inverses. fft_real should now pass

--- src/backend/oneapi/fft.cpp
+++ src/backend/oneapi/fft.cpp
@@ -90,12 +90,25 @@ Array<Tc> fft_r2c(const Array<Tr> &in, const int rank) {
         ::oneapi::mkl::dft::descriptor<precision,
                                        ::oneapi::mkl::dft::domain::REAL>;
 
+    // this section defines reversed dimension decl and output
+    // strides: onemkl is row-major and arrayfire it column
+    // major. since the TRANSPOSE option on the onemkl descriptor is
+    // broken, we're forced to shuffle data like this
     auto desc = [rank, &idims]() {
         if (rank == 1) return desc_ty(idims[0]);
         if (rank == 2) return desc_ty({idims[1], idims[0]});
         if (rank == 3) return desc_ty({idims[2], idims[1], idims[0]});
         return desc_ty({idims[3], idims[2], idims[1], idims[0]});
     }();
+    if (rank > 1) {
+      std::int64_t fft_output_strides[5];
+      fft_output_strides[0] = 0;
+      fft_output_strides[1] = ostrides[1];
+      fft_output_strides[2] = ostrides[0];
+      fft_output_strides[3] = ostrides[2];
+      fft_output_strides[4] = ostrides[3];
+      desc.set_value(::oneapi::mkl::dft::config_param::OUTPUT_STRIDES, fft_output_strides);
+    }
 
     desc.set_value(::oneapi::mkl::dft::config_param::PLACEMENT,
                    DFTI_NOT_INPLACE);
@@ -109,12 +122,6 @@ Array<Tc> fft_r2c(const Array<Tr> &in, const int rank) {
                    ostrides[rank]);
     desc.set_value(::oneapi::mkl::dft::config_param::FWD_DISTANCE,
                    istrides[rank]);
-
-    const std::int64_t fft_output_strides[5] = {
-        0, ostrides[(rank == 2) ? 1 : 0], ostrides[(rank == 2) ? 0 : 1],
-        ostrides[2], ostrides[3]};
-    desc.set_value(::oneapi::mkl::dft::config_param::OUTPUT_STRIDES,
-                   fft_output_strides, rank);
 
     desc.commit(getQueue());
     ::oneapi::mkl::dft::compute_forward(desc, *in.get(), *out.get());
@@ -137,12 +144,25 @@ Array<Tr> fft_c2r(const Array<Tc> &in, const dim4 &odims, const int rank) {
         ::oneapi::mkl::dft::descriptor<precision,
                                        ::oneapi::mkl::dft::domain::REAL>;
 
+    // this section defines reversed dimension decl and input strides:
+    // onemkl is row-major and arrayfire it column major. since the
+    // TRANSPOSE option on the onemkl descriptor is broken, we're
+    // forced to shuffle data like this
     auto desc = [rank, &odims]() {
         if (rank == 1) return desc_ty(odims[0]);
         if (rank == 2) return desc_ty({odims[1], odims[0]});
         if (rank == 3) return desc_ty({odims[2], odims[1], odims[0]});
         return desc_ty({odims[3], odims[2], odims[1], odims[0]});
     }();
+    if (rank > 1) {
+      std::int64_t fft_input_strides[5];
+      fft_input_strides[0] = 0;
+      fft_input_strides[1] = istrides[1];
+      fft_input_strides[2] = istrides[0];
+      fft_input_strides[3] = istrides[2];
+      fft_input_strides[4] = istrides[3];
+      desc.set_value(::oneapi::mkl::dft::config_param::INPUT_STRIDES, fft_input_strides);
+    }
 
     desc.set_value(::oneapi::mkl::dft::config_param::PLACEMENT,
                    DFTI_NOT_INPLACE);
@@ -156,12 +176,6 @@ Array<Tr> fft_c2r(const Array<Tc> &in, const dim4 &odims, const int rank) {
                    istrides[rank]);
     desc.set_value(::oneapi::mkl::dft::config_param::FWD_DISTANCE,
                    ostrides[rank]);
-
-    const std::int64_t fft_output_strides[5] = {
-        0, ostrides[(rank == 2) ? 1 : 0], ostrides[(rank == 2) ? 0 : 1],
-        ostrides[2], ostrides[3]};
-    desc.set_value(::oneapi::mkl::dft::config_param::OUTPUT_STRIDES,
-                   fft_output_strides, rank);
 
     desc.commit(getQueue());
     ::oneapi::mkl::dft::compute_backward(desc, *in.get(), *out.get());
