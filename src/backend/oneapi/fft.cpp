6bcad0636a7f99b550b0829a28be340188d08c73
Gallagher Donovan Pryor <gallagher@arrayfire.com>
2023-06-23 09:26:48 -0400

    recorrect fft_r2c and fft_c2r transposed dims, strides

--- src/backend/oneapi/fft.cpp
+++ src/backend/oneapi/fft.cpp
@@ -101,30 +101,26 @@ Array<Tc> fft_r2c(const Array<Tr> &in, const int rank) {
         ::oneapi::mkl::dft::descriptor<precision,
                                        ::oneapi::mkl::dft::domain::REAL>;
 
+    // TODO: This section defines a descriptor with flipped dimensions
+    // and output strides. oneMKL seems to insist on row-major
+    // computation while arrayfire is column major. Since the
+    // TRANSPOSE option on the oneMKL descriptor causes an immediate
+    // BAD_DESCRIPTOR error, we are forced to shuffle data like this.
+    // This ensures that this function is the inverse of fft_c2r.
     auto desc = [rank, &idims]() {
         if (rank == 1) return desc_ty(idims[0]);
-        if (rank == 2) return desc_ty({idims[0], idims[1]});
-        if (rank == 3) return desc_ty({idims[0], idims[1], idims[2]});
-        return desc_ty({idims[0], idims[1], idims[2], idims[3]});
+        if (rank == 2) return desc_ty({idims[1], idims[0]});
+        if (rank == 3) return desc_ty({idims[1], idims[0], idims[2]});
+        return desc_ty({idims[1], idims[0], idims[2], idims[3]});
     }();
     if (rank > 1) {
-        std::int64_t fft_input_strides[5];
-        fft_input_strides[0] = in.getOffset();
-        fft_input_strides[1] = istrides[0];
-        fft_input_strides[2] = istrides[1];
-        fft_input_strides[3] = istrides[2];
-        fft_input_strides[4] = istrides[3];
-        desc.set_value(::oneapi::mkl::dft::config_param::INPUT_STRIDES,
-                       fft_input_strides);
-
-        std::int64_t fft_output_strides[5];
-        fft_output_strides[0] = out.getOffset();
-        fft_output_strides[1] = ostrides[0];
-        fft_output_strides[2] = ostrides[1];
-        fft_output_strides[3] = ostrides[2];
-        fft_output_strides[4] = ostrides[3];
-        desc.set_value(::oneapi::mkl::dft::config_param::OUTPUT_STRIDES,
-                       fft_output_strides);
+      std::int64_t fft_output_strides[5];
+      fft_output_strides[0] = 0;
+      fft_output_strides[1] = ostrides[1];
+      fft_output_strides[2] = ostrides[0];
+      fft_output_strides[3] = ostrides[2];
+      fft_output_strides[4] = ostrides[3];
+      desc.set_value(::oneapi::mkl::dft::config_param::OUTPUT_STRIDES, fft_output_strides);
     }
 
     desc.set_value(::oneapi::mkl::dft::config_param::PLACEMENT,
@@ -161,30 +157,26 @@ Array<Tr> fft_c2r(const Array<Tc> &in, const dim4 &odims, const int rank) {
         ::oneapi::mkl::dft::descriptor<precision,
                                        ::oneapi::mkl::dft::domain::REAL>;
 
+    // TODO: This section defines a descriptor with flipped dimensions
+    // and output strides. oneMKL seems to insist on row-major
+    // computation while arrayfire is column major. Since the
+    // TRANSPOSE option on the oneMKL descriptor causes an immediate
+    // BAD_DESCRIPTOR error, we are forced to shuffle data like this.
+    // This ensures that this function is the inverse of fft_r2c.
     auto desc = [rank, &odims]() {
         if (rank == 1) return desc_ty(odims[0]);
-        if (rank == 2) return desc_ty({odims[0], odims[1]});
-        if (rank == 3) return desc_ty({odims[0], odims[1], odims[2]});
-        return desc_ty({odims[0], odims[1], odims[2], odims[3]});
+        if (rank == 2) return desc_ty({odims[1], odims[0]});
+        if (rank == 3) return desc_ty({odims[1], odims[0], odims[2]});
+        return desc_ty({odims[1], odims[0], odims[2], odims[3]});
     }();
     if (rank > 1) {
-        std::int64_t fft_input_strides[5];
-        fft_input_strides[0] = in.getOffset();
-        fft_input_strides[1] = istrides[0];
-        fft_input_strides[2] = istrides[1];
-        fft_input_strides[3] = istrides[2];
-        fft_input_strides[4] = istrides[3];
-        desc.set_value(::oneapi::mkl::dft::config_param::INPUT_STRIDES,
-                       fft_input_strides);
-
-        std::int64_t fft_output_strides[5];
-        fft_output_strides[0] = out.getOffset();
-        fft_output_strides[1] = ostrides[0];
-        fft_output_strides[2] = ostrides[1];
-        fft_output_strides[3] = ostrides[2];
-        fft_output_strides[4] = ostrides[3];
-        desc.set_value(::oneapi::mkl::dft::config_param::OUTPUT_STRIDES,
-                       fft_output_strides);
+      std::int64_t fft_input_strides[5];
+      fft_input_strides[0] = 0;
+      fft_input_strides[1] = istrides[1];
+      fft_input_strides[2] = istrides[0];
+      fft_input_strides[3] = istrides[2];
+      fft_input_strides[4] = istrides[3];
+      desc.set_value(::oneapi::mkl::dft::config_param::INPUT_STRIDES, fft_input_strides);
     }
 
     desc.set_value(::oneapi::mkl::dft::config_param::PLACEMENT,
