ccac73e86ac6f761770ed594255c04a01087732d
verstatx <ayer.3d@gmail.com>
2023-10-04 05:50:04 -0400

    Add int8 matmul support to the CUDA backend
    changes to gemm account for differing input/output types

--- src/backend/opencl/blas.hpp
+++ src/backend/opencl/blas.hpp
@@ -20,9 +20,10 @@ namespace opencl {
 void initBlas();
 void deInitBlas();
 
-template<typename T>
-void gemm(Array<T> &out, af_mat_prop optLhs, af_mat_prop optRhs, const T *alpha,
-          const Array<T> &lhs, const Array<T> &rhs, const T *beta);
+template<typename Ti, typename To = Ti>
+void gemm(Array<To> &out, af_mat_prop optLhs, af_mat_prop optRhs,
+          const To *alpha, const Array<Ti> &lhs, const Array<Ti> &rhs,
+          const To *beta);
 
 template<typename T>
 Array<T> matmul(const Array<T> &lhs, const Array<T> &rhs, af_mat_prop optLhs,
