1a2f610d654a64b25b864b0832a1b0f01f16f8e2
willyborn <sabine.willy.born@gmail.com>
2024-09-27 12:12:48 +0200

    Enforced argument checking for direct backend calls on join

--- src/backend/cpu/join.cpp
+++ src/backend/cpu/join.cpp
@@ -15,47 +15,77 @@
 #include <queue.hpp>
 
 #include <algorithm>
+#include <cassert>
+#include <numeric>
 
+using af::dim4;
 using arrayfire::common::half;
 
 namespace arrayfire {
 namespace cpu {
 
 template<typename T>
-Array<T> join(const int dim, const Array<T> &first, const Array<T> &second) {
-    // All dimensions except join dimension must be equal
+Array<T> join(const int jdim, const Array<T> &first, const Array<T> &second) {
     // Compute output dims
-    af::dim4 odims;
-    af::dim4 fdims = first.dims();
-    af::dim4 sdims = second.dims();
+    const dim4 &fdims = first.dims();
+    const dim4 &sdims = second.dims();
+    // All dimensions except join dimension must be equal
+    assert((jdim == 0 ? true : fdims.dims[0] == sdims.dims[0]) &&
+           (jdim == 1 ? true : fdims.dims[1] == sdims.dims[1]) &&
+           (jdim == 2 ? true : fdims.dims[2] == sdims.dims[2]) &&
+           (jdim == 3 ? true : fdims.dims[3] == sdims.dims[3]));
 
-    for (int i = 0; i < 4; i++) {
-        if (i == dim) {
-            odims[i] = fdims[i] + sdims[i];
-        } else {
-            odims[i] = fdims[i];
-        }
-    }
-
+    // compute output dms
+    dim4 odims(fdims);
+    odims.dims[jdim] += sdims.dims[jdim];
     Array<T> out = createEmptyArray<T>(odims);
     std::vector<CParam<T>> v{first, second};
-    getQueue().enqueue(kernel::join<T>, dim, out, v, 2);
+    getQueue().enqueue(kernel::join<T>, jdim, out, v, 2);
 
     return out;
 }
 
 template<typename T>
-void join(Array<T> &out, const int dim, const std::vector<Array<T>> &inputs) {
+void join(Array<T> &out, const int jdim, const std::vector<Array<T>> &inputs) {
     const dim_t n_arrays = inputs.size();
+    if (n_arrays == 0) return;
 
-    std::vector<Array<T> *> input_ptrs(inputs.size());
+    // avoid buffer overflow
+    const dim4 &odims{out.dims()};
+    const dim4 &fdims{inputs[0].dims()};
+    // All dimensions of inputs needs to be equal except for the join
+    // dimension
+    assert(std::all_of(inputs.begin(), inputs.end(),
+                       [jdim, &fdims](const Array<T> &in) {
+                           bool eq{true};
+                           for (int i = 0; i < 4; ++i) {
+                               if (i != jdim) {
+                                   eq &= fdims.dims[i] == in.dims().dims[i];
+                               };
+                           };
+                           return eq;
+                       }));
+    // All dimensions of out needs to cover all input dimensions
+    assert(
+        (odims.dims[0] >= fdims.dims[0]) && (odims.dims[1] >= fdims.dims[1]) &&
+        (odims.dims[2] >= fdims.dims[2]) && (odims.dims[3] >= fdims.dims[3]));
+    // The join dimension of out needs to be larger than the
+    // sum of all input join dimensions
+    assert(odims.dims[jdim] >=
+           std::accumulate(inputs.begin(), inputs.end(), 0,
+                           [jdim](dim_t dim, const Array<T> &in) {
+                               return dim += in.dims()[jdim];
+                           }));
+    assert(out.strides().dims[0] == 1);
+
+    std::vector<Array<T> *> input_ptrs(n_arrays);
     std::transform(
         begin(inputs), end(inputs), begin(input_ptrs),
         [](const Array<T> &input) { return const_cast<Array<T> *>(&input); });
     evalMultiple(input_ptrs);
     std::vector<CParam<T>> inputParams(inputs.begin(), inputs.end());
 
-    getQueue().enqueue(kernel::join<T>, dim, out, inputParams, n_arrays);
+    getQueue().enqueue(kernel::join<T>, jdim, out, inputParams, n_arrays);
 }
 
 #define INSTANTIATE(T)                                              \
